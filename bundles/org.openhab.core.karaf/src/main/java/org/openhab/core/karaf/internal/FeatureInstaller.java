07e75b7e9692993e6c60606fa3e911bb5c5a1ab7
Kai Kreuzer <kai@openhab.org>
2016-04-25 18:16:14 +0200

    enhanced feature installer to also handle remote repos for legacy and experimental features (#39)
    Signed-off-by: Kai Kreuzer <kai@openhab.org>

--- bundles/org.openhab.core.karaf/src/main/java/org/openhab/core/karaf/internal/FeatureInstaller.java
+++ bundles/org.openhab.core.karaf/src/main/java/org/openhab/core/karaf/internal/FeatureInstaller.java
@@ -8,6 +8,13 @@
  */
 package org.openhab.core.karaf.internal;
 
+import java.io.IOException;
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Dictionary;
+import java.util.Hashtable;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -15,15 +22,45 @@ import java.util.concurrent.Executors;
 import org.apache.commons.lang.StringUtils;
 import org.apache.karaf.features.Feature;
 import org.apache.karaf.features.FeaturesService;
+import org.osgi.service.cm.Configuration;
+import org.osgi.service.cm.ConfigurationAdmin;
+import org.osgi.service.cm.ConfigurationEvent;
+import org.osgi.service.cm.ConfigurationListener;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
  * This service reads addons.cfg and installs listed addons (= Karaf features) and the selected package.
+ * It furthermore allows configuration of the base package through the Paper UI as well as administrating Karaf to
+ * access remote repos and certain feature repos like for legacy or experimental features.
  *
- * @author Kai Kreuzer
+ * @author Kai Kreuzer - Initial contribution and API
  */
-public class FeatureInstaller {
+public class FeatureInstaller implements ConfigurationListener {
+
+    private static final String CFG_EXPERIMENTAL = "experimental";
+
+    private static final String CFG_LEGACY = "legacy";
+
+    private static final String ADDONS_SERVICE_PID = "org.openhab.addons";
+
+    private static final String CFG_REMOTE = "remote";
+
+    private static final String PAX_URL_PID = "org.ops4j.pax.url.mvn";
+
+    private static final String PROPERTY_MVN_REPOS = "org.ops4j.pax.url.mvn.repositories";
+
+    private static final String OH_SNAPSHOT_REPO = "http://oss.jfrog.org/libs-snapshot@id=oh-snapshot-repo@snapshots@noreleases";
+
+    private static final String OH_RELEASES_REPO = "https://jcenter.bintray.com@id=oh-releases-repo";
+
+    private static final URI LEGACY_FEATURES_URI = URI
+            .create("mvn:org.openhab.addons/openhab-addons-legacy/LATEST/xml/features");
+
+    private static final URI EXPERIMENTAL_FEATURES_URI = URI
+            .create("mvn:org.openhab.addons/openhab-addons-experimental/LATEST/xml/features");
+
+    public static final String STANDARD_PACKAGE = "standard";
 
     public static final String PREFIX = "openhab-";
 
@@ -35,6 +72,10 @@ public class FeatureInstaller {
     private static final Logger logger = LoggerFactory.getLogger(FeatureInstaller.class);
 
     private FeaturesService featuresService;
+    private ConfigurationAdmin configurationAdmin;
+
+    private boolean paxCfgUpdated = true; // a flag used to check whether CM has already successfully updated the pax
+                                          // configuration as this must be waited for before trying to add feature repos
 
     protected void setFeaturesService(FeaturesService featuresService) {
         this.featuresService = featuresService;
@@ -44,28 +85,178 @@ public class FeatureInstaller {
         this.featuresService = null;
     }
 
+    protected void setConfigurationAdmin(ConfigurationAdmin configurationAdmin) {
+        this.configurationAdmin = configurationAdmin;
+    }
+
+    protected void unsetConfigurationAdmin(ConfigurationAdmin configurationAdmin) {
+        this.configurationAdmin = null;
+    }
+
     protected void activate(final Map<String, Object> config) {
         modified(config);
     }
 
     protected void modified(final Map<String, Object> config) {
+        if (config.get(CFG_REMOTE) == null && getOnlineStatus() == true) {
+            // we seem to have an online distro and no configuration set
+            updateOnlineConfigFlag(true);
+            return;
+        } else {
+            boolean online = config.get(CFG_REMOTE) != null && "true".equals(config.get(CFG_REMOTE).toString());
+            if (getOnlineStatus() != online) {
+                setOnlineStatus(online);
+            }
+        }
+
         final FeaturesService service = featuresService;
         ExecutorService scheduler = Executors.newSingleThreadExecutor();
+
         scheduler.execute(new Runnable() {
             @Override
             public void run() {
-                installPackage(service, config);
-
-                // install addons
-                for (String type : addonTypes) {
-                    Object install = config.get(type);
-                    if (install instanceof String) {
-                        installFeatures(service, type, (String) install);
+                int counter = 0;
+                // wait up to 5 seconds for the config update event
+                while (!paxCfgUpdated && counter++ < 50) {
+                    try {
+                        Thread.sleep(100);
+                    } catch (InterruptedException e) {
                     }
                 }
+                setLegacyExtensions(service, config);
+                setExperimentalExtensions(service, config);
+                installPackage(service, config);
+                installAddons(service, config);
             }
-
         });
+    }
+
+    private void setLegacyExtensions(FeaturesService service, Map<String, Object> config) {
+        if (config.get(CFG_LEGACY) != null && "true".equals(config.get(CFG_LEGACY).toString())) {
+            try {
+                service.addRepository(LEGACY_FEATURES_URI);
+            } catch (Exception e) {
+                logger.debug("Failed adding feature repo for legacy features - we might be offline: {}",
+                        e.getMessage());
+            }
+        } else {
+            try {
+                service.removeRepository(LEGACY_FEATURES_URI);
+            } catch (Exception e) {
+                logger.error("Failed removing feature repo of legacy features: {}", e.getMessage());
+            }
+        }
+    }
+
+    private void setExperimentalExtensions(FeaturesService service, Map<String, Object> config) {
+        if (config.get(CFG_EXPERIMENTAL) != null && "true".equals(config.get(CFG_EXPERIMENTAL).toString())) {
+            try {
+                service.addRepository(EXPERIMENTAL_FEATURES_URI);
+            } catch (Exception e) {
+                logger.debug("Failed adding feature repo for experimental features - we might be offline: {}",
+                        e.getMessage());
+            }
+        } else {
+            try {
+                service.removeRepository(EXPERIMENTAL_FEATURES_URI);
+            } catch (Exception e) {
+                logger.error("Failed removing feature repo of experimental features: {}", e.getMessage());
+            }
+        }
+    }
+
+    private boolean getOnlineStatus() {
+        try {
+            Configuration paxCfg = configurationAdmin.getConfiguration(PAX_URL_PID);
+            Dictionary<String, Object> properties = paxCfg.getProperties();
+            if (properties == null) {
+                return false;
+            }
+            Object repos = properties.get(PROPERTY_MVN_REPOS);
+            List<String> repoCfg;
+            if (repos instanceof String) {
+                repoCfg = Arrays.asList(((String) repos).split(","));
+                if (repoCfg.contains(OH_RELEASES_REPO) && repoCfg.contains(OH_SNAPSHOT_REPO)) {
+                    return true;
+                }
+            }
+        } catch (IOException e) {
+            logger.error("Failed setting the extension management online/offline mode: {}", e.toString());
+        }
+        return false;
+
+    }
+
+    private void setOnlineStatus(boolean status) {
+        try {
+            Configuration paxCfg = configurationAdmin.getConfiguration(PAX_URL_PID);
+            paxCfg.setBundleLocation("?");
+            Dictionary<String, Object> properties = paxCfg.getProperties();
+            if (properties == null) {
+                properties = new Hashtable<>();
+            }
+            List<String> repoCfg = new ArrayList<>();
+            Object repos = properties.get(PROPERTY_MVN_REPOS);
+            if (repos instanceof String) {
+                repoCfg = new ArrayList<>(Arrays.asList(((String) repos).split(",")));
+                repoCfg.remove("");
+            }
+            if (status) {
+                if (!(repoCfg.contains(OH_RELEASES_REPO) && repoCfg.contains(OH_SNAPSHOT_REPO))) {
+                    repoCfg.add(OH_RELEASES_REPO);
+                    repoCfg.add(OH_SNAPSHOT_REPO);
+                    properties.put(PROPERTY_MVN_REPOS, StringUtils.join(repoCfg.toArray(), ","));
+                    paxCfgUpdated = false;
+                    paxCfg.update(properties);
+                }
+            } else {
+                if (repoCfg.contains(OH_RELEASES_REPO) || repoCfg.contains(OH_SNAPSHOT_REPO)) {
+                    repoCfg.remove(OH_RELEASES_REPO);
+                    repoCfg.remove(OH_SNAPSHOT_REPO);
+                    properties.put(PROPERTY_MVN_REPOS, StringUtils.join(repoCfg.toArray(), ","));
+                    paxCfgUpdated = false;
+                    paxCfg.update(properties);
+                }
+            }
+        } catch (IOException e) {
+            logger.error("Failed setting the extension management online/offline mode: {}", e.toString());
+        }
+    }
+
+    private void updateOnlineConfigFlag(final Boolean online) {
+        // let's do this asynchronous to not block the current config dispatching
+        ExecutorService scheduler = Executors.newSingleThreadExecutor();
+        final ConfigurationAdmin service = configurationAdmin;
+
+        scheduler.execute(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Configuration cfg = service.getConfiguration(ADDONS_SERVICE_PID);
+                    Dictionary<String, Object> properties = cfg.getProperties();
+                    if (properties == null) {
+                        properties = new Hashtable<>();
+                    }
+                    if (properties.get(CFG_REMOTE) == null
+                            || !online.toString().equals(properties.get(CFG_REMOTE).toString())) {
+                        // configuration is out of sync, so let's update it
+                        properties.put(CFG_REMOTE, online);
+                        cfg.update(properties);
+                    }
+                } catch (IOException e) {
+                    logger.error("Failed updating the remote configuration: {}", e.toString());
+                }
+            }
+        });
+    }
+
+    private void installAddons(final FeaturesService service, final Map<String, Object> config) {
+        for (String type : addonTypes) {
+            Object install = config.get(type);
+            if (install instanceof String) {
+                installFeatures(service, type, (String) install);
+            }
+        }
     }
 
     private void installFeatures(FeaturesService featuresService, String type, String install) {
@@ -89,7 +280,7 @@ public class FeatureInstaller {
     }
 
     private static void installPackage(FeaturesService featuresService, final Map<String, Object> config) {
-        Object packageName = config.get("package");
+        Object packageName = config.get("package") != null ? config.get("package") : STANDARD_PACKAGE;
         if (packageName instanceof String) {
             String name = PREFIX + PREFIX_PACKAGE + ((String) packageName).trim();
             installFeature(featuresService, name);
@@ -123,5 +314,12 @@ public class FeatureInstaller {
             logger.error("Failed retrieving features: {}", e.getMessage());
         }
         return false;
+    }
+
+    @Override
+    public void configurationEvent(ConfigurationEvent event) {
+        if (event.getPid().equals(PAX_URL_PID) && event.getType() == ConfigurationEvent.CM_UPDATED) {
+            paxCfgUpdated = true;
+        }
     }
 }
