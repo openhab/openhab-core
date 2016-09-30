a45e865068c1811550328036e8a8ca47730ee4f6
Kai Kreuzer <kai@openhab.org>
2016-09-30 19:34:01 +0200

    Added new enhanced sound fragment, which brings mp3 support on top of ESH. Remove the old audio action as all features should now be covered through ESH itself.
    Signed-off-by: Kai Kreuzer <kai@openhab.org>

---
+++ bundles/org.openhab.io.sound/src/main/java/org/openhab/io/sound/internal/EnhancedJavaSoundAudioSink.java
@@ -0,0 +1,117 @@
+/**
+ * Copyright (c) 2014-2016 by the respective copyright holders.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.openhab.io.sound.internal;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashSet;
+import java.util.Locale;
+import java.util.Set;
+
+import org.eclipse.smarthome.core.audio.AudioFormat;
+import org.eclipse.smarthome.core.audio.AudioStream;
+import org.eclipse.smarthome.core.audio.URLAudioStream;
+import org.eclipse.smarthome.core.audio.UnsupportedAudioFormatException;
+import org.eclipse.smarthome.io.javasound.internal.JavaSoundAudioSink;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javazoom.jl.decoder.JavaLayerException;
+import javazoom.jl.player.Player;
+
+/**
+ * This is an audio sink that is registered as a service, which can play mp3 files to the hosts outputs (e.g. speaker,
+ * line-out).
+ *
+ * @author Karel Goderis - Initial contribution and API
+ * @author Kai Kreuzer - Refactored and moved to openHAB
+ */
+public class EnhancedJavaSoundAudioSink extends JavaSoundAudioSink {
+
+    private final Logger logger = LoggerFactory.getLogger(EnhancedJavaSoundAudioSink.class);
+
+    private static AudioFormat mp3 = new AudioFormat(AudioFormat.CONTAINER_NONE, AudioFormat.CODEC_MP3, null, null,
+            null, null);
+    private static AudioFormat wav = new AudioFormat(AudioFormat.CONTAINER_WAVE, AudioFormat.CODEC_PCM_SIGNED, null,
+            null, null, null);
+    private static HashSet<AudioFormat> supportedFormats = new HashSet<>();
+
+    static {
+        supportedFormats.add(wav);
+        supportedFormats.add(mp3);
+    }
+
+    private static Player streamPlayer = null;
+
+    @Override
+    public String getId() {
+        return "enhancedjavasound";
+    }
+
+    @Override
+    public String getLabel(Locale locale) {
+        return "System Speaker";
+    }
+
+    @Override
+    public void process(final AudioStream audioStream) throws UnsupportedAudioFormatException {
+        if (audioStream != null && audioStream.getFormat().getCodec() != AudioFormat.CODEC_MP3) {
+            // we can only deal with mp3, so delegate the rest
+            super.process(audioStream);
+        } else {
+            if (audioStream == null || audioStream instanceof URLAudioStream) {
+                // we are dealing with an infinite stream here
+                if (streamPlayer != null) {
+                    // if we are already playing a stream, stop it first
+                    streamPlayer.close();
+                    streamPlayer = null;
+                }
+                if (audioStream == null) {
+                    // the call was only for stopping the currently playing stream
+                    return;
+                }
+            }
+            try (InputStream stream = audioStream) {
+                Player player = new Player(stream);
+                streamPlayer = player;
+                playInThread(player);
+            } catch (JavaLayerException | IOException e) {
+                logger.error("An exception occurred while playing audio : '{}'", e.getMessage());
+            }
+        }
+    }
+
+    @Override
+    public Set<AudioFormat> getSupportedFormats() {
+        return supportedFormats;
+    }
+
+    static private void playInThread(final Player player) {
+        // run in new thread
+        new Thread() {
+            @Override
+            public void run() {
+                try {
+                    player.play();
+                } catch (Exception e) {
+                    throw new RuntimeException(e.getMessage());
+                } finally {
+                    player.close();
+                }
+            }
+        }.start();
+    }
+
+    protected void deactivate() {
+        if (streamPlayer != null) {
+            // stop playing streams on shutdown
+            streamPlayer.close();
+            streamPlayer = null;
+        }
+    }
+}
