45adcc0aac3dce1ee5570e934768349e753f28f5
Kai Kreuzer <kai@openhab.org>
2016-11-10 11:48:43 +0100

    removed ESHCallType and added correct mapping to new CallItem and StringListType in ESH
    Signed-off-by: Kai Kreuzer <kai@openhab.org>

--- bundles/org.openhab.core.compat1x.test/src/test/java/org/openhab/core/compat1x/internal/ItemMapperTest.java
+++ bundles/org.openhab.core.compat1x.test/src/test/java/org/openhab/core/compat1x/internal/ItemMapperTest.java
@@ -11,6 +11,7 @@ package org.openhab.core.compat1x.internal;
 import static org.junit.Assert.*;
 
 import org.eclipse.smarthome.core.items.GroupItem;
+import org.eclipse.smarthome.core.library.items.CallItem;
 import org.eclipse.smarthome.core.library.items.ColorItem;
 import org.eclipse.smarthome.core.library.items.ContactItem;
 import org.eclipse.smarthome.core.library.items.DateTimeItem;
@@ -26,11 +27,10 @@ import org.openhab.core.items.Item;
 
 public class ItemMapperTest {
 
-
     @Test
     public void testESH2OHItemMapping() {
         Item item = null;
-        
+
         item = ItemMapper.mapToOpenHABItem(new StringItem("test1"));
         assertEquals(org.openhab.core.library.items.StringItem.class, item.getClass());
         assertEquals("test1", item.getName());
@@ -62,37 +62,44 @@ public class ItemMapperTest {
         item = ItemMapper.mapToOpenHABItem(new SwitchItem("test8"));
         assertEquals(org.openhab.core.library.items.SwitchItem.class, item.getClass());
         assertEquals("test8", item.getName());
+
+        item = ItemMapper.mapToOpenHABItem(new CallItem("test9"));
+        assertEquals(org.openhab.library.tel.items.CallItem.class, item.getClass());
+        assertEquals("test9", item.getName());
     }
-    
+
     @Test
     public void testGroupItemMapping() {
-        org.openhab.core.items.GroupItem group = (org.openhab.core.items.GroupItem) ItemMapper.mapToOpenHABItem(new GroupItem("group1"));
+        org.openhab.core.items.GroupItem group = (org.openhab.core.items.GroupItem) ItemMapper
+                .mapToOpenHABItem(new GroupItem("group1"));
         assertEquals("group1", group.getName());
         assertNull(group.getBaseItem());
-        
-        group = (org.openhab.core.items.GroupItem) ItemMapper.mapToOpenHABItem(new GroupItem("group2", new NumberItem("baseItem")));
+
+        group = (org.openhab.core.items.GroupItem) ItemMapper
+                .mapToOpenHABItem(new GroupItem("group2", new NumberItem("baseItem")));
         assertEquals("group2", group.getName());
         assertEquals(org.openhab.core.library.items.NumberItem.class, group.getBaseItem().getClass());
 
         GroupItem eshGroup = new GroupItem("group3");
-        for(int i = 1; i <= 10; i++) {
+        for (int i = 1; i <= 10; i++) {
             eshGroup.addMember(new StringItem(Integer.toString(i)));
         }
         group = (org.openhab.core.items.GroupItem) ItemMapper.mapToOpenHABItem(eshGroup);
         assertEquals("group3", group.getName());
         assertEquals(10, group.getMembers().size());
-        assertEquals(org.openhab.core.library.items.StringItem.class, group.getMembers().iterator().next().getClass());        
+        assertEquals(org.openhab.core.library.items.StringItem.class, group.getMembers().iterator().next().getClass());
     }
-    
+
     @Test
     public void testItemStateMapping() {
         StringItem item = new StringItem("test");
-        item.setState(UnDefType.NULL);        
+        item.setState(UnDefType.NULL);
         assertEquals(org.openhab.core.types.UnDefType.NULL, ItemMapper.mapToOpenHABItem(item).getState());
-        item.setState(UnDefType.UNDEF);        
+        item.setState(UnDefType.UNDEF);
         assertEquals(org.openhab.core.types.UnDefType.UNDEF, ItemMapper.mapToOpenHABItem(item).getState());
-        item.setState(new StringType("ABC"));        
-        assertEquals(new org.openhab.core.library.types.StringType("ABC"), ItemMapper.mapToOpenHABItem(item).getState());
+        item.setState(new StringType("ABC"));
+        assertEquals(new org.openhab.core.library.types.StringType("ABC"),
+                ItemMapper.mapToOpenHABItem(item).getState());
     }
 
 }
