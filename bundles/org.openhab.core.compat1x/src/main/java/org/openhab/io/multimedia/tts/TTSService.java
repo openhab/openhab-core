6bb1ba10576332ce998d2caf35983653a2477b9a
Martin van Wingerden <martinvw@users.noreply.github.com>
2017-08-20 09:56:41 +0200

    Updated outdated copyrights (#197)
    Signed-off-by: Martin van Wingerden <martinvw@mtin.nl>

--- bundles/org.openhab.core.compat1x/src/main/java/org/openhab/io/multimedia/tts/TTSService.java
+++ bundles/org.openhab.core.compat1x/src/main/java/org/openhab/io/multimedia/tts/TTSService.java
@@ -1,3 +1,11 @@
+/**
+ * Copyright (c) 2015-2017 by the respective copyright holders.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
 package org.openhab.io.multimedia.tts;
 
 // NOTE: This interface is only kept in order to allow openHAB 1.x TTS services to correctly compile.
