e70f55646f2940095f375e582e19854aad6e1e04
Martin van Wingerden <martin@martinvw.nl>
2018-11-13 17:29:00 +0100

    Add no-save and corrected some js warnings
    Signed-off-by: Martin van Wingerden <martin@martinvw.nl>

--- bundles/org.openhab.ui.homebuilder/web/webpack.config.js
+++ bundles/org.openhab.ui.homebuilder/web/webpack.config.js
@@ -1,5 +1,5 @@
-var path = require('path')
-var webpack = require('webpack')
+var path = require('path');
+var webpack = require('webpack');
 
 module.exports = {
   entry: './src/main.js',
@@ -59,10 +59,10 @@ module.exports = {
     hints: false
   },
   devtool: '#eval-source-map'
-}
+};
 
 if (process.env.NODE_ENV === 'production') {
-  module.exports.devtool = '#source-map'
+  module.exports.devtool = '#source-map';
   // http://vue-loader.vuejs.org/en/workflow/production.html
   module.exports.plugins = (module.exports.plugins || []).concat([
     new webpack.DefinePlugin({
