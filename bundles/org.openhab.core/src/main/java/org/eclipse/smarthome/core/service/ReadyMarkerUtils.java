7073266aa6c10aff7eade4bcd282c9bb10572ec2
Markus Rathgeb <maggu2810@gmail.com>
2019-02-06 14:03:38 +0100

    centralize the bundle identifier construction
    The ready marker logic is created to hide the real usage. The ready
    marker uses an "identifier" that is not specific or limited to bundles.
    
    Currently the bundle symbolic name is used as bundle identifier.
    That "convention" needs to be known at several different places.
    
    There should be one method that created an identifier for a bundle and
    "no one" needs to care about the implementation details.
    
    Another point is that the bundle symbolic name that has been used is
    optional. It may be null e.g. for bundles that has been installed by the
    synthetic bundle installer mechanism etc.
    
    The runtime assigns a bundle ID to an installed bundle that remains the
    same for the bundle (see JavaDoc). The bundle ID is present all the
    time.
    
    The implementation of the "get identifier for bundle" has been choosen
    to use the BSN -- if available -- (as before) but fallback to a custom
    one using also the bundle ID (to be unique).
    So we can provide always a non null identifier for a bundle.
    It should be easily to change the identifier creation now if there is
    every any need for.
    
    Signed-off-by: Markus Rathgeb <maggu2810@gmail.com>

---
+++ bundles/org.openhab.core/src/main/java/org/eclipse/smarthome/core/service/ReadyMarkerUtils.java
@@ -0,0 +1,56 @@
+/**
+ * Copyright (c) 2014,2019 Contributors to the Eclipse Foundation
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.eclipse.smarthome.core.service;
+
+import java.util.Arrays;
+
+import org.osgi.framework.Bundle;
+
+/**
+ * Utilities for the ready maker usage.
+ *
+ * @author Markus Rathgeb - Initial contribution and API
+ */
+public class ReadyMarkerUtils {
+
+    /**
+     * Gets an identifier for a bundle.
+     *
+     * @param bundle the bundle
+     * @return an identifier
+     */
+    public static String getIdentifier(final Bundle bundle) {
+        final String bsn = bundle.getSymbolicName();
+        if (bsn != null) {
+            return bsn;
+        } else {
+            return String.format("@bundleId@0x%x", bundle.getBundleId());
+        }
+    }
+
+    /**
+     * Provides a string to debug bundle information.
+     *
+     * @param bundle the bundle
+     * @return a debug string
+     */
+    public static String debugString(final Bundle bundle) {
+        return "Bundle [getState()=" + bundle.getState() + ", getHeaders()=" + bundle.getHeaders() + ", getBundleId()="
+                + bundle.getBundleId() + ", getLocation()=" + bundle.getLocation() + ", getRegisteredServices()="
+                + Arrays.toString(bundle.getRegisteredServices()) + ", getServicesInUse()="
+                + Arrays.toString(bundle.getServicesInUse()) + ", getSymbolicName()=" + bundle.getSymbolicName()
+                + ", getLastModified()=" + bundle.getLastModified() + ", getBundleContext()="
+                + bundle.getBundleContext() + ", getVersion()=" + bundle.getVersion() + "]";
+    }
+
+}
