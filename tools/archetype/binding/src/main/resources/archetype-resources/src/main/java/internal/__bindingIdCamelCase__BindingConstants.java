ff57db83966107d7a22febf0e782cc92df00bf0b
Hilbrand Bouwkamp <hilbrand@h72.nl>
2019-04-26 13:03:28 +0200

    Added maven archetype project to generate binding skeletons (#589)
    This archetype generates bindings for the new bnd based build system.
    It also updates specific bundle files that need information about the new binding.
    
    This doesn't yet include generation of test projects.
    
    Signed-off-by: Hilbrand Bouwkamp <hilbrand@h72.nl>

---
+++ tools/archetype/binding/src/main/resources/archetype-resources/src/main/java/internal/__bindingIdCamelCase__BindingConstants.java
@@ -0,0 +1,37 @@
+#set( $dt = $package.getClass().forName("java.util.Date").newInstance() )
+#set( $year = $dt.getYear() + 1900 )
+#set( $copyright = "Contributors to the ${vendorName} project" )
+/**
+ * Copyright (c) ${startYear},${year} ${copyright}
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package ${package}.internal;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.eclipse.smarthome.core.thing.ThingTypeUID;
+
+/**
+ * The {@link ${bindingIdCamelCase}BindingConstants} class defines common constants, which are
+ * used across the whole binding.
+ *
+ * @author ${author} - Initial contribution
+ */
+@NonNullByDefault
+public class ${bindingIdCamelCase}BindingConstants {
+
+    private static final String BINDING_ID = "${bindingId}";
+
+    // List of all Thing Type UIDs
+    public static final ThingTypeUID THING_TYPE_SAMPLE = new ThingTypeUID(BINDING_ID, "sample");
+
+    // List of all Channel ids
+    public static final String CHANNEL_1 = "channel1";
+}
---
+++ tools/archetype/binding/src/main/resources/archetype-resources/src/main/java/internal/__bindingIdCamelCase__HandlerFactory.java
@@ -0,0 +1,60 @@
+#set( $dt = $package.getClass().forName("java.util.Date").newInstance() )
+#set( $year = $dt.getYear() + 1900 )
+#set( $copyright = "Contributors to the ${vendorName} project" )
+/**
+ * Copyright (c) ${startYear},${year} ${copyright}
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package ${package}.internal;
+
+import static ${package}.internal.${bindingIdCamelCase}BindingConstants.*;
+
+import java.util.Collections;
+import java.util.Set;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.eclipse.jdt.annotation.Nullable;
+import ${package}.internal.${bindingIdCamelCase}Handler;
+import org.eclipse.smarthome.core.thing.Thing;
+import org.eclipse.smarthome.core.thing.ThingTypeUID;
+import org.eclipse.smarthome.core.thing.binding.BaseThingHandlerFactory;
+import org.eclipse.smarthome.core.thing.binding.ThingHandler;
+import org.eclipse.smarthome.core.thing.binding.ThingHandlerFactory;
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * The {@link ${bindingIdCamelCase}HandlerFactory} is responsible for creating things and thing
+ * handlers.
+ *
+ * @author ${author} - Initial contribution
+ */
+@NonNullByDefault
+@Component(configurationPid = "binding.${bindingId}", service = ThingHandlerFactory.class)
+public class ${bindingIdCamelCase}HandlerFactory extends BaseThingHandlerFactory {
+
+    private static final Set<ThingTypeUID> SUPPORTED_THING_TYPES_UIDS = Collections.singleton(THING_TYPE_SAMPLE);
+
+    @Override
+    public boolean supportsThingType(ThingTypeUID thingTypeUID) {
+        return SUPPORTED_THING_TYPES_UIDS.contains(thingTypeUID);
+    }
+
+    @Override
+    protected @Nullable ThingHandler createHandler(Thing thing) {
+        ThingTypeUID thingTypeUID = thing.getThingTypeUID();
+
+        if (THING_TYPE_SAMPLE.equals(thingTypeUID)) {
+            return new ${bindingIdCamelCase}Handler(thing);
+        }
+
+        return null;
+    }
+}
