f881ef87f19e94bcf9902b91da02feb06ffbc21b
Markus Rathgeb <maggu2810@gmail.com>
2019-05-01 23:19:07 +0200

    model: prevent simultan access of singleton (#773)
    The singleton instance of "Diagnostician" is used without
    synchronization.
    The singleton "Diasnostician" instance is using the singleton
    "EValidator.Registry" instance (without synchronization).
    In very rare high load situations there has been CME detected.
    
    My first "solution" has been to synchronize the access of
    Diagnostician's instance method by using
    
    ```java
    synchronized (Diagnostician.INSTANCE) {
        ...
    ```
    
    But after realize that EMF is using internally other singletons I tried
    to find any information about EMF and thread safety.
    
    I found this one: https://javahacks.net/2016/07/13/emf-thread-safety/
    
      EMF models are not thread-safe by default and writing multithreaded
      applications is not that simple.
      The more complex our application became, the more often we got
      concurrent modification exceptions and had problems with filtering and
      sorting operations.
    
    So, I assume instead of adding synchronizations to our code that is
    using EMF (IIRC this has been already done on the ESH hosted code base
    long time ago) we should try to execute EMF code in a safe manner.
    
    This implementation adds a "SafeEMF" OSGi service that should be used to
    execute EMF code to ensure none code of EMF (or at least the ones that
    calls has been migrated to the SafeEMF usage) is accessed by separate
    threads at the same time.
    
    Related to: https://github.com/openhab/openhab-core/issues/772
    
    Signed-off-by: Markus Rathgeb <maggu2810@gmail.com>

---
+++ bundles/org.openhab.core.model.core/src/main/java/org/eclipse/smarthome/model/core/internal/SafeEMFImpl.java
@@ -0,0 +1,38 @@
+/**
+ * Copyright (c) 2010-2019 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.eclipse.smarthome.model.core.internal;
+
+import java.util.function.Supplier;
+
+import org.eclipse.smarthome.model.core.SafeEMF;
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * Implementation of a safe EMF caller..
+ *
+ * @author Markus Rathgeb - Initial contribution
+ */
+@Component
+public class SafeEMFImpl implements SafeEMF {
+
+    @Override
+    public synchronized <T> T call(Supplier<T> func) {
+        return func.get();
+    }
+
+    @Override
+    public synchronized void call(Runnable func) {
+        func.run();
+    }
+
+}
