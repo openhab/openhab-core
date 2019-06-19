fefbef50f5c63d690215b61df99c41df4be38435
Markus Rathgeb <maggu2810@gmail.com>
2019-06-19 22:42:27 +0200

    UID: allow to encode arbitrary string (+decode) (#864)
    Signed-off-by: Markus Rathgeb <maggu2810@gmail.com>

---
+++ bundles/org.openhab.core/src/test/java/org/eclipse/smarthome/core/common/UIDUtilsTest.java
@@ -0,0 +1,45 @@
+/**
+ * Copyright (c) 2010-2019 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.eclipse.smarthome.core.common;
+
+import java.util.function.Consumer;
+
+import org.eclipse.smarthome.core.util.UIDUtils;
+import org.hamcrest.core.IsEqual;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Tests for {@link AbstractUID}.
+ *
+ * @author Markus Rathgeb - Initial contribution
+ */
+public class UIDUtilsTest {
+
+    @Test
+    public void encodeDecode() {
+        Consumer<String> test = in -> {
+            final String encoded = UIDUtils.encode(in);
+            final String decoded = UIDUtils.decode(encoded);
+            System.out.printf("in: %s%n encoded: %s%n decoded: %s%n equals: %b%n", in, encoded, decoded,
+                    in.equals(decoded));
+            Assert.assertThat(decoded, IsEqual.equalTo(in));
+        };
+        test.accept("test");
+        test.accept("TEST");
+        test.accept("test123TEST");
+        test.accept("test_test-test%test");
+        test.accept("äöø€");
+    }
+
+}
