5b10f8d7ed3d30431b0a2ccac2b74ee0c98c600c
Markus Rathgeb <maggu2810@gmail.com>
2019-06-28 12:08:46 +0200

    modify and use modified publisher
    Signed-off-by: Markus Rathgeb <maggu2810@gmail.com>

--- bundles/org.openhab.core.io.rest.publisher/src/main/java/org/openhab/core/thirdparty/com/eclipsesource/jaxrs/publisher/internal/Activator.java
+++ bundles/org.openhab.core.io.rest.publisher/src/main/java/org/openhab/core/thirdparty/com/eclipsesource/jaxrs/publisher/internal/Activator.java
@@ -16,10 +16,15 @@ import java.util.Dictionary;
 import java.util.Hashtable;
 
 import javax.ws.rs.Path;
+import javax.ws.rs.ext.MessageBodyReader;
+import javax.ws.rs.ext.MessageBodyWriter;
 
-import org.openhab.core.thirdparty.com.eclipsesource.jaxrs.publisher.ResourceFilter;
+import org.eclipse.smarthome.io.rest.RESTResource;
+import org.eclipse.smarthome.io.rest.sse.SseResource;
+import org.glassfish.jersey.media.sse.SseFeature;
+import org.glassfish.jersey.server.monitoring.ApplicationEventListener;
+import org.openhab.core.io.rest.publisher.internal.ResourceFilterImpl;
 import org.osgi.framework.Bundle;
-import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.BundleException;
 import org.osgi.framework.Constants;
@@ -28,35 +33,113 @@ import org.osgi.framework.InvalidSyntaxException;
 import org.osgi.framework.ServiceReference;
 import org.osgi.framework.ServiceRegistration;
 import org.osgi.service.cm.ManagedService;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
+import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.component.annotations.ReferenceCardinality;
+import org.osgi.service.component.annotations.ReferencePolicy;
 
-public class Activator implements BundleActivator {
+@Component(immediate = true)
+public class Activator {
 
-    private ServiceRegistration connectorRegistration;
+    private ServiceRegistration<JAXRSConnector> connectorRegistration;
     private JAXRSConnector jaxRsConnector;
     private HttpTracker httpTracker;
-    private ResourceTracker allTracker;
     private ServletConfigurationTracker servletConfigurationTracker;
     private ApplicationConfigurationTracker applicationConfigurationTracker;
-    private ServiceRegistration configRegistration;
+    private ServiceRegistration<ManagedService> configRegistration;
 
-    @Override
+    private ResourceFilterImpl ohResourceFilter;
+    private ResourceTracker ohTracker;
+
+    @Activate
+    public Activator(final BundleContext context) {
+        ohResourceFilter = new ResourceFilterImpl();
+        jaxRsConnector = new JAXRSConnector(context);
+    }
+
+    @Activate
     public void start(BundleContext context) throws Exception {
         System.setProperty("javax.ws.rs.ext.RuntimeDelegate",
                 "org.glassfish.jersey.server.internal.RuntimeDelegateImpl");
         startJerseyServer();
-        jaxRsConnector = new JAXRSConnector(context);
         registerConfiguration(context);
-        connectorRegistration = context.registerService(JAXRSConnector.class.getName(), jaxRsConnector, null);
+        connectorRegistration = context.registerService(JAXRSConnector.class, jaxRsConnector, null);
         openHttpServiceTracker(context);
         openServletConfigurationTracker(context);
         openApplicationConfigurationTracker(context);
-        openAllServiceTracker(context);
+        openOhServiceTracker(context);
+    }
+
+    @Deactivate
+    public void stop(BundleContext context) throws Exception {
+        httpTracker.close();
+        servletConfigurationTracker.close();
+        applicationConfigurationTracker.close();
+        ohTracker.close();
+        connectorRegistration.unregister();
+        configRegistration.unregister();
+    }
+
+    @Reference(cardinality = ReferenceCardinality.MULTIPLE, policy = ReferencePolicy.DYNAMIC)
+    public void addJaxRsMessageBodyReader(final ServiceReference<MessageBodyReader<?>> resource) {
+        jaxRsConnector.addResource(resource);
+    }
+
+    public void removeJaxRsMessageBodyReader(final ServiceReference<MessageBodyReader<?>> resource) {
+        jaxRsConnector.removeResource(resource);
+    }
+
+    @Reference(cardinality = ReferenceCardinality.MULTIPLE, policy = ReferencePolicy.DYNAMIC)
+    public void addJaxRsMessageBodyWriter(final ServiceReference<MessageBodyWriter<?>> resource) {
+        jaxRsConnector.addResource(resource);
+    }
+
+    public void removeJaxRsMessageBodyWriter(final ServiceReference<MessageBodyWriter<?>> resource) {
+        jaxRsConnector.removeResource(resource);
+    }
+
+    @Reference(cardinality = ReferenceCardinality.MULTIPLE, policy = ReferencePolicy.DYNAMIC)
+    public void addOhRESTResource(final ServiceReference<RESTResource> resource) {
+        jaxRsConnector.addResource(resource);
+    }
+
+    public void removeOhRESTResource(final ServiceReference<RESTResource> resource) {
+        jaxRsConnector.removeResource(resource);
+    }
+
+    @Reference(cardinality = ReferenceCardinality.MULTIPLE, policy = ReferencePolicy.DYNAMIC)
+    public void addOhSSEResource(final ServiceReference<SseResource> resource) {
+        jaxRsConnector.addResource(resource);
+    }
+
+    public void removeOhSSEResource(final ServiceReference<SseResource> resource) {
+        jaxRsConnector.removeResource(resource);
+    }
+
+    @Reference(cardinality = ReferenceCardinality.MULTIPLE, policy = ReferencePolicy.DYNAMIC)
+    public void addJerseySseFeature(final ServiceReference<SseFeature> resource) {
+        jaxRsConnector.addResource(resource);
+    }
+
+    public void removeJerseySseFeature(final ServiceReference<SseFeature> resource) {
+        jaxRsConnector.removeResource(resource);
+    }
+
+    @Reference(cardinality = ReferenceCardinality.MULTIPLE, policy = ReferencePolicy.DYNAMIC)
+    public void addJerseyApplicationEventListener(final ServiceReference<ApplicationEventListener> resource) {
+        jaxRsConnector.addResource(resource);
+    }
+
+    public void removeJerseyApplicationEventListener(final ServiceReference<ApplicationEventListener> resource) {
+        jaxRsConnector.removeResource(resource);
     }
 
     private void registerConfiguration(BundleContext context) {
         Dictionary<String, String> properties = new Hashtable<>();
         properties.put(Constants.SERVICE_PID, Configuration.CONFIG_SERVICE_PID);
-        configRegistration = context.registerService(ManagedService.class.getName(), new Configuration(jaxRsConnector),
+        configRegistration = context.registerService(ManagedService.class, new Configuration(jaxRsConnector),
                 properties);
     }
 
@@ -82,28 +165,9 @@ public class Activator implements BundleActivator {
         applicationConfigurationTracker.open();
     }
 
-    private void openAllServiceTracker(BundleContext context) throws InvalidSyntaxException {
-        ResourceFilter allResourceFilter = getResourceFilter(context);
-        allTracker = new ResourceTracker(context, allResourceFilter.getFilter(), jaxRsConnector);
-        allTracker.open();
-    }
-
-    private ResourceFilter getResourceFilter(BundleContext context) {
-        ServiceReference filterReference = context.getServiceReference(ResourceFilter.class.getName());
-        if (filterReference != null) {
-            return (ResourceFilter) context.getService(filterReference);
-        }
-        return new AllResourceFilter(context);
-    }
-
-    @Override
-    public void stop(BundleContext context) throws Exception {
-        httpTracker.close();
-        servletConfigurationTracker.close();
-        applicationConfigurationTracker.close();
-        allTracker.close();
-        connectorRegistration.unregister();
-        configRegistration.unregister();
+    private void openOhServiceTracker(BundleContext context) throws InvalidSyntaxException {
+        ohTracker = new ResourceTracker(context, ohResourceFilter.getFilter(), jaxRsConnector);
+        ohTracker.open();
     }
 
     // For testing purpose
