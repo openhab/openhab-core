9409f641c4a49091a49ff03d49f9c105d8f5b1a8
lolodomo <lg.hc@free.fr>
2019-09-01 13:43:25 +0200

    New Audio REST API
    Signed-off-by: Laurent Garnier <lg.hc@free.fr>

---
+++ bundles/org.openhab.core.io.rest.audio/src/main/java/org/eclipse/smarthome/io/rest/audio/internal/AudioSinkDTO.java
@@ -0,0 +1,35 @@
+/**
+ * Copyright (c) 2010-2019 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.eclipse.smarthome.io.rest.audio.internal;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.eclipse.jdt.annotation.Nullable;
+import org.eclipse.smarthome.core.audio.AudioSink;
+
+/**
+ * A DTO that is used on the REST API to provide infos about {@link AudioSink} to UIs.
+ *
+ * @author Laurent Garnier - Initial contribution
+ *
+ */
+@NonNullByDefault
+public class AudioSinkDTO {
+    public String id;
+    public @Nullable String label;
+
+    public AudioSinkDTO(String id, @Nullable String label) {
+        this.id = id;
+        this.label = label;
+    }
+
+}
