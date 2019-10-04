beba7a7603238c10b77f01d8c6fa42fbc2017b0e
Wouter Born <github@maindrain.net>
2019-10-04 21:19:53 +0200

    Remove notes
    Signed-off-by: Wouter Born <github@maindrain.net>

--- notes/migration.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-find -type d -name "ESH-INF" | grep -v -e '/target/' -e 'src/main/resources/ESH-INF' | while read ESHINF_DIR; do
-    PROJECT_DIR="$(dirname "${ESHINF_DIR}")"
-    mkdir -p "${PROJECT_DIR}/src/main/resources"
-    mv "${ESHINF_DIR}" "${PROJECT_DIR}/src/main/resources"
-done
-
