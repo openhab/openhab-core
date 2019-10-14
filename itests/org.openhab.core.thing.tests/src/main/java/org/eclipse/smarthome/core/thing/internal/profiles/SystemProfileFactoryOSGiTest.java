29ea69c3b2702d33a880dc5c6f743f96395e3192
Christoph Weitkamp <github@christophweitkamp.de>
2019-10-14 17:47:48 +0200

    Added integration test to check if factory creates all available profiles
    Signed-off-by: Christoph Weitkamp <github@christophweitkamp.de>

--- itests/org.openhab.core.thing.tests/src/main/java/org/eclipse/smarthome/core/thing/internal/profiles/SystemProfileFactoryOSGiTest.java
+++ itests/org.openhab.core.thing.tests/src/main/java/org/eclipse/smarthome/core/thing/internal/profiles/SystemProfileFactoryOSGiTest.java
@@ -14,20 +14,29 @@ package org.eclipse.smarthome.core.thing.internal.profiles;
 
 import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
 
+import java.math.BigDecimal;
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
+import org.eclipse.smarthome.config.core.Configuration;
 import org.eclipse.smarthome.core.library.CoreItemFactory;
 import org.eclipse.smarthome.core.thing.Channel;
 import org.eclipse.smarthome.core.thing.ChannelUID;
 import org.eclipse.smarthome.core.thing.binding.builder.ChannelBuilder;
+import org.eclipse.smarthome.core.thing.profiles.ProfileCallback;
+import org.eclipse.smarthome.core.thing.profiles.ProfileContext;
 import org.eclipse.smarthome.core.thing.profiles.ProfileType;
 import org.eclipse.smarthome.core.thing.profiles.ProfileTypeProvider;
+import org.eclipse.smarthome.core.thing.profiles.ProfileTypeUID;
 import org.eclipse.smarthome.core.thing.profiles.SystemProfiles;
 import org.eclipse.smarthome.core.thing.type.ChannelType;
 import org.eclipse.smarthome.test.java.JavaOSGiTest;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
 
 /**
  * Test cases for the {@link SystemProfileFactory} class.
@@ -36,18 +45,46 @@ import org.junit.Test;
  */
 public class SystemProfileFactoryOSGiTest extends JavaOSGiTest {
 
+    private final Map<String, Object> properties = new HashMap<String, Object>() {
+        private static final long serialVersionUID = 1L;
+        {
+            put("offset", BigDecimal.ZERO);
+        }
+    };
+
     private SystemProfileFactory profileFactory;
+
+    @Mock
+    private ProfileCallback mockCallback;
+
+    @Mock
+    private ProfileContext mockContext;
 
     @Before
     public void setUp() {
         profileFactory = getService(ProfileTypeProvider.class, SystemProfileFactory.class);
         assertNotNull(profileFactory);
+
+        mockCallback = mock(ProfileCallback.class);
+
+        mockContext = mock(ProfileContext.class);
+        when(mockContext.getConfiguration()).thenReturn(new Configuration(properties));
     }
 
     @Test
-    public void systemProfileTypesShouldBeAvailable() {
+    public void systemProfileTypesAndUidsShouldBeAvailable() {
+        Collection<ProfileTypeUID> systemProfileTypeUIDs = profileFactory.getSupportedProfileTypeUIDs();
+        assertEquals(15, systemProfileTypeUIDs.size());
+
         Collection<ProfileType> systemProfileTypes = profileFactory.getProfileTypes(null);
-        assertEquals(15, systemProfileTypes.size());
+        assertEquals(systemProfileTypeUIDs.size(), systemProfileTypes.size());
+    }
+
+    @Test
+    public void testFactoryCreatesAvailableProfiles() {
+        for (ProfileTypeUID profileTypeUID : profileFactory.getSupportedProfileTypeUIDs()) {
+            assertThat(profileFactory.createProfile(profileTypeUID, mockCallback, mockContext), is(notNullValue()));
+        }
     }
 
     @Test
