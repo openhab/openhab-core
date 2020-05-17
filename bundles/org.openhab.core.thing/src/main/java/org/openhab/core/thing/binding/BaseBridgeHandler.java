3d78cb6df668ce8bd311270a814a566b800cab0d
Christoph Weitkamp <github@christophweitkamp.de>
2020-05-17 21:47:58 +0200

    [thing] Removed 'getThingByUID()' method from 'BaseBridgeHandler' (#1465)
    Signed-off-by: Christoph Weitkamp <github@christophweitkamp.de>

--- bundles/org.openhab.core.thing/src/main/java/org/openhab/core/thing/binding/BaseBridgeHandler.java
+++ bundles/org.openhab.core.thing/src/main/java/org/openhab/core/thing/binding/BaseBridgeHandler.java
@@ -12,13 +12,9 @@
  */
 package org.openhab.core.thing.binding;
 
-import java.util.List;
-
 import org.eclipse.jdt.annotation.NonNullByDefault;
-import org.eclipse.jdt.annotation.Nullable;
 import org.openhab.core.thing.Bridge;
 import org.openhab.core.thing.Thing;
-import org.openhab.core.thing.ThingUID;
 import org.openhab.core.thing.binding.builder.BridgeBuilder;
 
 /**
@@ -41,26 +37,6 @@ public abstract class BaseBridgeHandler extends BaseThingHandler implements Brid
         super(bridge);
     }
 
-    /**
-     * Finds and returns a child thing for a given UID of this bridge.
-     *
-     * @param uid uid of the child thing
-     * @return child thing with the given uid or null if thing was not found
-     */
-    public @Nullable Thing getThingByUID(ThingUID uid) {
-        Bridge bridge = getThing();
-
-        List<Thing> things = bridge.getThings();
-
-        for (Thing thing : things) {
-            if (thing.getUID().equals(uid)) {
-                return thing;
-            }
-        }
-
-        return null;
-    }
-
     @Override
     public Bridge getThing() {
         return (Bridge) super.getThing();
@@ -70,14 +46,13 @@ public abstract class BaseBridgeHandler extends BaseThingHandler implements Brid
      * Creates a bridge builder, which allows to modify the bridge. The method
      * {@link BaseThingHandler#updateThing(Thing)} must be called to persist the changes.
      *
-     * @return {@link BridgeBuilder} which builds an exact copy of the bridge (not null)
+     * @return {@link BridgeBuilder} which builds an exact copy of the bridge
      */
     @Override
     protected BridgeBuilder editThing() {
-        return BridgeBuilder.create(this.thing.getThingTypeUID(), this.thing.getUID())
-                .withBridge(this.thing.getBridgeUID()).withChannels(this.thing.getChannels())
-                .withConfiguration(this.thing.getConfiguration()).withLabel(this.thing.getLabel())
-                .withLocation(this.thing.getLocation()).withProperties(this.thing.getProperties());
+        return BridgeBuilder.create(thing.getThingTypeUID(), thing.getUID()).withBridge(thing.getBridgeUID())
+                .withChannels(thing.getChannels()).withConfiguration(thing.getConfiguration())
+                .withLabel(thing.getLabel()).withLocation(thing.getLocation()).withProperties(thing.getProperties());
     }
 
     @Override
