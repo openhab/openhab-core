56a016fdbfc6808c40c0f38e31a637b3e4a40b1b
Yannick Schaus <github@schaus.net>
2020-05-19 00:30:17 +0200

    Fix header & Javadoc
    Signed-off-by: Yannick Schaus <github@schaus.net>

--- bundles/org.openhab.core.io.rest.swagger/src/main/java/org/openhab/core/io/rest/swagger/impl/SwaggerGenerator.java
+++ bundles/org.openhab.core.io.rest.swagger/src/main/java/org/openhab/core/io/rest/swagger/impl/SwaggerGenerator.java
@@ -1,3 +1,15 @@
+/**
+ * Copyright (c) 2010-2020 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
 package org.openhab.core.io.rest.swagger.impl;
 
 import java.io.IOException;
@@ -29,8 +41,7 @@ import io.swagger.models.parameters.Parameter;
 import io.swagger.util.Json;
 
 /**
- * An JAX-RS Whiteboard Swagger 1 generator implementation.
- *
+ * A JAX-RS Whiteboard Swagger 1 generator implementation.
  *
  * @author Markus Rathgeb - Initial contribution
  * @author Yannick Schaus - adapt to provide OAuth2 security definitions
