3d5acaca6da0d0ffeaa08503983276ec33b3612c
Yannick Schaus <github@schaus.net>
2020-05-18 20:37:38 +0200

    Remove configurationPid
    Signed-off-by: Yannick Schaus <github@schaus.net>

--- bundles/org.openhab.core.io.rest.auth/src/main/java/org/openhab/core/io/rest/auth/internal/AuthFilter.java
+++ bundles/org.openhab.core.io.rest.auth/src/main/java/org/openhab/core/io/rest/auth/internal/AuthFilter.java
@@ -40,7 +40,7 @@ import org.osgi.service.jaxrs.whiteboard.propertytypes.JaxrsExtension;
  * @author Yannick Schaus - initial contribution
  */
 @PreMatching
-@Component(configurationPid = "org.openhab.restauth")
+@Component
 @JaxrsExtension
 @JaxrsApplicationSelect("(" + JaxrsWhiteboardConstants.JAX_RS_NAME + "=" + RESTConstants.JAX_RS_NAME + ")")
 @Priority(Priorities.AUTHENTICATION)
