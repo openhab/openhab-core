openhab/openhab-core

commit f201b83c0937c876bda8dff1917880f7bcc344cc

Fix "Unable to watch transformation directory" warnings (#1650)
Fixes #1636

Signed-off-by: Wouter Born <github@maindrain.net>

bundles/org.openhab.core.transform/src/main/java/org/openhab/core/transform/AbstractFileTransformationService.java
@@ -247,10 +247,10 @@ public abstract class AbstractFileTransformationService<T> implements Transforma
         }
     }
 
-    private @Nullable String getFileExtension(String fileName) {
+    private String getFileExtension(String fileName) {
         int extensionPos = fileName.lastIndexOf(EXTENSION_SEPARATOR);
         int lastSeparatorPos = Math.max(fileName.lastIndexOf(UNIX_SEPARATOR), fileName.lastIndexOf(WINDOWS_SEPARATOR));
-        return lastSeparatorPos > extensionPos ? null : fileName.substring(extensionPos + 1);
+        return lastSeparatorPos > extensionPos ? "" : fileName.substring(extensionPos + 1);
     }
 
     /**
@@ -262,31 +262,21 @@ public abstract class AbstractFileTransformationService<T> implements Transforma
      */
     protected String getLocalizedProposedFilename(String filename, final WatchService watchService) {
         final File file = new File(filename);
+        if (file.getParent() != null) {
+            watchSubDirectory(file.getParent(), watchService);
+        }
+
         String extension = getFileExtension(filename);
-        String prefix = file.getPath();
-        String result = filename;
-
-        if (extension == null) {
-            extension = "";
-        }
-        if (!prefix.isEmpty()) {
-            watchSubDirectory(prefix, watchService);
-        }
-
         // the filename may already contain locale information
         if (!filename.matches(".*_[a-z]{2}." + extension + "$")) {
-            String basename = file.getName();
-            String alternateName = prefix + basename + "_" + getLocale().getLanguage() + "." + extension;
+            String alternateName = filename + "_" + getLocale().getLanguage() + "." + extension;
             String alternatePath = getSourcePath() + alternateName;
-
-            File f = new File(alternatePath);
-            if (f.exists()) {
-                result = alternateName;
+            if (new File(alternatePath).exists()) {
+                return alternatePath;
             }
         }
 
-        result = getSourcePath() + result;
-        return result;
+        return getSourcePath() + filename;
     }
 
     /**

commit 3f70625a0326edba946f660a4c9e008034f77dea

Fix transformation file path with locale specific files
The path is incorrect because a bug was introduced in #1650.
This results in the MapTransformationServiceTest failing so it was disabled in openhab/openhab-addons#8519.

Signed-off-by: Wouter Born <github@maindrain.net>

bundles/org.openhab.core.transform/src/main/java/org/openhab/core/transform/AbstractFileTransformationService.java
@@ -266,17 +266,19 @@ public abstract class AbstractFileTransformationService<T> implements Transforma
             watchSubDirectory(file.getParent(), watchService);
         }
 
+        String sourcePath = getSourcePath();
         String extension = getFileExtension(filename);
+
         // the filename may already contain locale information
         if (!filename.matches(".*_[a-z]{2}." + extension + "$")) {
-            String alternateName = filename + "_" + getLocale().getLanguage() + "." + extension;
-            String alternatePath = getSourcePath() + alternateName;
+            String alternatePath = sourcePath + filename.substring(0, filename.length() - extension.length() - 1) + "_"
+                    + getLocale().getLanguage() + "." + extension;
             if (new File(alternatePath).exists()) {
                 return alternatePath;
             }
         }
 
-        return getSourcePath() + filename;
+        return sourcePath + filename;
     }
 
     /**