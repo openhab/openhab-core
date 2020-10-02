3b16f2ce7c9ef73377b0094e1673c1360bc5e45a
Wouter Born <github@maindrain.net>
2020-10-02 14:25:52 +0200

    Filter empty i18n config parameter options (#1680)
    * Filter empty i18n config parameter options
    
    There are many locales that do not have a country resulting in these empty parameter options.
    
    Related to #1662
    
    Signed-off-by: Wouter Born <github@maindrain.net>

--- bundles/org.openhab.core.config.core/src/test/java/org/openhab/core/config/core/internal/i18n/I18nConfigOptionsProviderTest.java
+++ bundles/org.openhab.core.config.core/src/test/java/org/openhab/core/config/core/internal/i18n/I18nConfigOptionsProviderTest.java
@@ -19,7 +19,6 @@ import java.net.URI;
 import java.util.Locale;
 
 import org.hamcrest.collection.IsEmptyCollection;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openhab.core.config.core.ParameterOption;
 
@@ -30,37 +29,41 @@ import org.openhab.core.config.core.ParameterOption;
  */
 public class I18nConfigOptionsProviderTest {
 
-    private I18nConfigOptionsProvider provider;
+    private final I18nConfigOptionsProvider provider = new I18nConfigOptionsProvider();
+    private final URI uriI18N = URI.create("system:i18n");
+
+    private final ParameterOption empty = new ParameterOption("", "");
     private final ParameterOption expectedLangEN = new ParameterOption("en", "English");
     private final ParameterOption expectedLangFR = new ParameterOption("en", "anglais");
     private final ParameterOption expectedCntryEN = new ParameterOption("US", "United States");
     private final ParameterOption expectedCntryFRJava8 = new ParameterOption("US", "Etats-Unis");
     private final ParameterOption expectedCntryFRJava9 = new ParameterOption("US", "États-Unis");
-    private URI uriI18N;
-
-    @BeforeEach
-    public void setup() throws Exception {
-        provider = new I18nConfigOptionsProvider();
-        uriI18N = new URI("system:i18n");
-    }
 
     @Test
-    public void testLanguage() throws Exception {
+    public void testLanguage() {
         assertThat(provider.getParameterOptions(uriI18N, "language", null, Locale.US), hasItem(expectedLangEN));
+        assertThat(provider.getParameterOptions(uriI18N, "language", null, Locale.US), not(hasItem(empty)));
+
         assertThat(provider.getParameterOptions(uriI18N, "language", null, Locale.FRENCH), hasItem(expectedLangFR));
+        assertThat(provider.getParameterOptions(uriI18N, "language", null, Locale.FRENCH), not(hasItem(empty)));
+
         assertThat(provider.getParameterOptions(uriI18N, "language", null, null), not(IsEmptyCollection.empty()));
     }
 
     @Test
-    public void testRegion() throws Exception {
+    public void testRegion() {
         assertThat(provider.getParameterOptions(uriI18N, "region", null, Locale.US), hasItem(expectedCntryEN));
+        assertThat(provider.getParameterOptions(uriI18N, "region", null, Locale.US), not(hasItem(empty)));
+
         assertThat(provider.getParameterOptions(uriI18N, "region", null, Locale.FRENCH),
                 anyOf(hasItem(expectedCntryFRJava8), hasItem(expectedCntryFRJava9)));
+        assertThat(provider.getParameterOptions(uriI18N, "region", null, Locale.FRENCH), not(hasItem(empty)));
+
         assertThat(provider.getParameterOptions(uriI18N, "region", null, null), not(IsEmptyCollection.empty()));
     }
 
     @Test
-    public void testUnknownParameter() throws Exception {
+    public void testUnknownParameter() {
         assertThat(provider.getParameterOptions(uriI18N, "unknown", null, Locale.US), nullValue());
     }
 }
