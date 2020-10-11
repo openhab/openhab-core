46b5c2b3c0bb014befc6792af98e033537c87e20
Yannick Schaus <github@schaus.net>
2020-10-11 11:54:25 +0200

    Allow basic authentication to authorize API access
    Closes #1699.
    
    Note, this opens a minor security issue that allows an attacker
    to brute force passwords by making calls to the API - contrary to
    the authorization page, the credentials parsing for the REST API
    is stateless & doesn't have a lock mechanism to lock user accounts
    after too many failed login attempts.
    
    Signed-off-by: Yannick Schaus <github@schaus.net>

--- bundles/org.openhab.core.io.rest.auth/src/main/java/org/openhab/core/io/rest/auth/internal/AuthFilter.java
+++ bundles/org.openhab.core.io.rest.auth/src/main/java/org/openhab/core/io/rest/auth/internal/AuthFilter.java
@@ -13,6 +13,7 @@
 package org.openhab.core.io.rest.auth.internal;
 
 import java.io.IOException;
+import java.util.Base64;
 
 import javax.annotation.Priority;
 import javax.security.sasl.AuthenticationException;
@@ -26,6 +27,9 @@ import javax.ws.rs.core.SecurityContext;
 import javax.ws.rs.ext.Provider;
 
 import org.openhab.core.auth.Authentication;
+import org.openhab.core.auth.User;
+import org.openhab.core.auth.UserRegistry;
+import org.openhab.core.auth.UsernamePasswordCredentials;
 import org.openhab.core.io.rest.JSONResponse;
 import org.openhab.core.io.rest.RESTConstants;
 import org.osgi.service.component.annotations.Component;
@@ -35,10 +39,11 @@ import org.osgi.service.jaxrs.whiteboard.propertytypes.JaxrsApplicationSelect;
 import org.osgi.service.jaxrs.whiteboard.propertytypes.JaxrsExtension;
 
 /**
- * This filter is responsible for parsing a token provided with a request, and hydrating a {@link SecurityContext} from
- * the claims contained in the token.
+ * This filter is responsible for parsing credentials provided with a request, and hydrating a {@link SecurityContext}
+ * from these credentials if they are valid.
  *
  * @author Yannick Schaus - initial contribution
+ * @author Yannick Schaus - Allow basic authentication
  */
 @PreMatching
 @Component
@@ -52,6 +57,9 @@ public class AuthFilter implements ContainerRequestFilter {
     @Reference
     private JwtHelper jwtHelper;
 
+    @Reference
+    private UserRegistry userRegistry;
+
     @Override
     public void filter(ContainerRequestContext requestContext) throws IOException {
         try {
@@ -63,6 +71,25 @@ public class AuthFilter implements ContainerRequestFilter {
                         Authentication auth = jwtHelper.verifyAndParseJwtAccessToken(authParts[1]);
                         requestContext.setSecurityContext(new JwtSecurityContext(auth));
                         return;
+                    } else if ("Basic".equals(authParts[0])) {
+                        try {
+                            String[] decodedCredentials = new String(Base64.getDecoder().decode(authParts[1]), "UTF-8")
+                                    .split(":");
+                            if (decodedCredentials.length != 2) {
+                                throw new AuthenticationException("Invalid Basic authentication credential format");
+                            }
+                            UsernamePasswordCredentials credentials = new UsernamePasswordCredentials(
+                                    decodedCredentials[0], decodedCredentials[1]);
+                            Authentication auth = userRegistry.authenticate(credentials);
+                            User user = userRegistry.get(auth.getUsername());
+                            if (user == null) {
+                                throw new org.openhab.core.auth.AuthenticationException("User not found in registry");
+                            }
+                            requestContext.setSecurityContext(new UserSecurityContext(user, "Basic"));
+                            return;
+                        } catch (org.openhab.core.auth.AuthenticationException e) {
+                            throw new AuthenticationException("Invalid user name or password");
+                        }
                     }
                 }
             }
@@ -74,7 +101,7 @@ public class AuthFilter implements ContainerRequestFilter {
                 return;
             }
         } catch (AuthenticationException e) {
-            requestContext.abortWith(JSONResponse.createErrorResponse(Status.UNAUTHORIZED, "Invalid token"));
+            requestContext.abortWith(JSONResponse.createErrorResponse(Status.UNAUTHORIZED, "Invalid credentials"));
         }
     }
 }
