b8dd5e5fa28cb572f6d3a1ca91470c4d706af32b
Kai Kreuzer <kai@openhab.org>
2020-12-12 13:46:52 +0100

    addressed review comment
    Signed-off-by: Kai Kreuzer <kai@openhab.org>

--- bundles/org.openhab.core.automation/src/main/java/org/openhab/core/automation/internal/RuleEngineImpl.java
+++ bundles/org.openhab.core.automation/src/main/java/org/openhab/core/automation/internal/RuleEngineImpl.java
@@ -27,7 +27,6 @@ import java.util.concurrent.CopyOnWriteArraySet;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
 
 import org.eclipse.jdt.annotation.NonNullByDefault;
 import org.eclipse.jdt.annotation.Nullable;
@@ -901,7 +900,7 @@ public class RuleEngineImpl implements RuleManager, RegistryChangeListener<Modul
                     return;
                 }
                 setRule(managedRule);
-            }, TimeUnit.MILLISECONDS));
+            }));
         }
     }
 
