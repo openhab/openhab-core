fd29cd87cc7e1c8ebb9ee2aad4c96680b60eb504
Björn Lange <blange@itemis.com>
2021-04-23 14:15:35 +0200

    Ignore illegal thing status updates from REMOVING
    Ignore illegal thing status transitions from REMOVING instead of throwing IllegalArgumentException
    
    Signed-off-by: Björn Lange <blange@itemis.com>

--- bundles/org.openhab.core.thing/src/main/java/org/openhab/core/thing/internal/ThingManagerImpl.java
+++ bundles/org.openhab.core.thing/src/main/java/org/openhab/core/thing/internal/ThingManagerImpl.java
@@ -120,6 +120,7 @@ import org.slf4j.LoggerFactory;
  * @author Henning Sudbrock - Consider thing type properties when migrating to new thing type
  * @author Christoph Weitkamp - Added preconfigured ChannelGroupBuilder
  * @author Yordan Zhelev - Added thing disabling mechanism
+ * @author Björn Lange - Ignore illegal thing status transitions instead of throwing IllegalArgumentException
  */
 @NonNullByDefault
 @Component(immediate = true, service = { ThingTypeMigrationService.class, ThingManager.class })
@@ -187,10 +188,11 @@ public class ThingManagerImpl
 
             if (ThingStatus.REMOVING.equals(oldStatusInfo.getStatus())
                     && !ThingStatus.REMOVED.equals(statusInfo.getStatus())) {
-                // only allow REMOVING -> REMOVED transition, all others are illegal
-                throw new IllegalArgumentException(MessageFormat.format(
-                        "Illegal status transition from REMOVING to {0}, only REMOVED would have been allowed.",
-                        statusInfo.getStatus()));
+                // only allow REMOVING -> REMOVED transition, all others are ignored because they are illegal
+                logger.warn(
+                        "Ignoring illegal status transition for thing {} from REMOVING to {}, only REMOVED would have been allowed.",
+                        thing.getUID(), statusInfo.getStatus());
+                return;
             }
 
             // update thing status and send event about new status
