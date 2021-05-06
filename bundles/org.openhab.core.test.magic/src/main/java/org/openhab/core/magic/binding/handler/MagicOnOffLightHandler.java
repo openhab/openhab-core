aa48f5990ea70a501c6d45beb7712b18e76a029f
Christoph Weitkamp <github@christophweitkamp.de>
2021-05-06 22:11:30 +0200

    Added Channel of DateTimeType (#2347)
    Signed-off-by: Christoph Weitkamp <github@christophweitkamp.de>

--- bundles/org.openhab.core.test.magic/src/main/java/org/openhab/core/magic/binding/handler/MagicOnOffLightHandler.java
+++ bundles/org.openhab.core.test.magic/src/main/java/org/openhab/core/magic/binding/handler/MagicOnOffLightHandler.java
@@ -13,6 +13,7 @@
 package org.openhab.core.magic.binding.handler;
 
 import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.openhab.core.library.types.DateTimeType;
 import org.openhab.core.thing.ChannelUID;
 import org.openhab.core.thing.Thing;
 import org.openhab.core.thing.ThingStatus;
@@ -35,6 +36,7 @@ public class MagicOnOffLightHandler extends BaseThingHandler {
     @Override
     public void handleCommand(ChannelUID channelUID, Command command) {
         triggerChannel("trigger", command.toString());
+        updateState("timestamp", new DateTimeType());
     }
 
     @Override
