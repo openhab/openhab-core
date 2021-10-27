4e5400f20ab1c5bdf51d0f1f29609ad62f7c4e15
Yannick Schaus <github@schaus.net>
2021-10-27 19:35:30 +0200

    Add a SAT error
    Signed-off-by: Yannick Schaus <github@schaus.net>

--- bundles/org.openhab.core.io.http/src/main/java/org/openhab/core/io/http/internal/BundleHttpContext.java
+++ bundles/org.openhab.core.io.http/src/main/java/org/openhab/core/io/http/internal/BundleHttpContext.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2010-2021 Contributors to the openHAB project
+ * Copyright (c) 2010-2021 Contributors to the openHAB project - INVALID
  *
  * See the NOTICE file(s) distributed with this work for additional
  * information.
