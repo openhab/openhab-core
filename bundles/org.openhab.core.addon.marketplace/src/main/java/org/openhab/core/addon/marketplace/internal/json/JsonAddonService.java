39482bae593442f86762bb70bd4af9719d466ac8
Jan N. Klug <jan.n.klug@rub.de>
2021-12-18 18:20:07 +0100

    fix uninstall, too
    Signed-off-by: Jan N. Klug <jan.n.klug@rub.de>

--- bundles/org.openhab.core.addon.marketplace/src/main/java/org/openhab/core/addon/marketplace/internal/json/JsonAddonService.java
+++ bundles/org.openhab.core.addon.marketplace/src/main/java/org/openhab/core/addon/marketplace/internal/json/JsonAddonService.java
@@ -204,7 +204,6 @@ public class JsonAddonService implements AddonService {
                     }
                     return;
                 }
-                return;
             }
         }
         postFailureEvent(id, "Add-on not known.");
