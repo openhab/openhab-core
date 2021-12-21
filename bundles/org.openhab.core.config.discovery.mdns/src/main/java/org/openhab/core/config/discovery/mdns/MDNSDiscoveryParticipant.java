25cc68b71ac04cfd6e0ff8b071b9f914e8ede27b
Jacob Laursen <jacob-github@vindvejr.dk>
2021-12-21 21:21:40 +0100

    Fix wrong argument name in javadoc.
    Signed-off-by: Jacob Laursen <jacob-github@vindvejr.dk>

--- bundles/org.openhab.core.config.discovery.mdns/src/main/java/org/openhab/core/config/discovery/mdns/MDNSDiscoveryParticipant.java
+++ bundles/org.openhab.core.config.discovery.mdns/src/main/java/org/openhab/core/config/discovery/mdns/MDNSDiscoveryParticipant.java
@@ -50,7 +50,7 @@ public interface MDNSDiscoveryParticipant {
     /**
      * Creates a discovery result for a mDNS service
      *
-     * @param device the mDNS service found on the network
+     * @param service the mDNS service found on the network
      * @return the according discovery result or <code>null</code>, if device is not
      *         supported by this participant
      */
@@ -60,7 +60,7 @@ public interface MDNSDiscoveryParticipant {
     /**
      * Returns the thing UID for a mDNS service
      *
-     * @param device the mDNS service on the network
+     * @param service the mDNS service on the network
      * @return a thing UID or <code>null</code>, if device is not supported
      *         by this participant
      */
