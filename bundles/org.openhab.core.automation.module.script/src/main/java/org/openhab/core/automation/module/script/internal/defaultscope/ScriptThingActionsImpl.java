9a9217eab8d4292d4b905ffdea5a9ed5641bfdf6
J-N-K <github@klug.nrw>
2022-02-13 09:57:22 +0100

    Expose three classes used as bindings in JSR-223 rules as interfaces (#2723)
    Signed-off-by: Jan N. Klug <github@klug.nrw>

--- bundles/org.openhab.core.automation.module.script/src/main/java/org/openhab/core/automation/module/script/internal/defaultscope/ScriptThingActionsImpl.java
+++ bundles/org.openhab.core.automation.module.script/src/main/java/org/openhab/core/automation/module/script/internal/defaultscope/ScriptThingActionsImpl.java
@@ -15,6 +15,9 @@ package org.openhab.core.automation.module.script.internal.defaultscope;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.eclipse.jdt.annotation.Nullable;
+import org.openhab.core.automation.module.script.defaultscope.ScriptThingActions;
 import org.openhab.core.thing.Thing;
 import org.openhab.core.thing.ThingRegistry;
 import org.openhab.core.thing.ThingUID;
@@ -25,16 +28,19 @@ import org.openhab.core.thing.binding.ThingHandler;
 /**
  * The methods of this class are made available as functions in the scripts.
  *
- * Note: This class is a copy from the {@link ThingActions} class, which resides in the model.script bundle.
+ * Note: This class is a copy from the {@link org.openhab.core.model.script.internal.engine.action.ThingActionService}
+ * class
  *
  * @author Kai Kreuzer - Initial contribution
+ * @author Jan N. Klug - Moved implementation to internal class
  */
-public class ScriptThingActions {
+@NonNullByDefault
+public class ScriptThingActionsImpl implements ScriptThingActions {
 
     private static final Map<String, ThingActions> THING_ACTIONS_MAP = new HashMap<>();
-    private ThingRegistry thingRegistry;
+    private @Nullable ThingRegistry thingRegistry;
 
-    ScriptThingActions(ThingRegistry thingRegistry) {
+    ScriptThingActionsImpl(ThingRegistry thingRegistry) {
         this.thingRegistry = thingRegistry;
     }
 
@@ -42,22 +48,17 @@ public class ScriptThingActions {
         this.thingRegistry = null;
     }
 
-    /**
-     * Gets an actions instance of a certain scope for a given thing UID
-     *
-     * @param scope the action scope
-     * @param thingUid the UID of the thing
-     *
-     * @return actions the actions instance or null, if not available
-     */
-    public ThingActions get(String scope, String thingUid) {
-        ThingUID uid = new ThingUID(thingUid);
-        Thing thing = thingRegistry.get(uid);
-        if (thing != null) {
-            ThingHandler handler = thing.getHandler();
-            if (handler != null) {
-                ThingActions thingActions = THING_ACTIONS_MAP.get(getKey(scope, thingUid));
-                return thingActions;
+    @Override
+    public @Nullable ThingActions get(@Nullable String scope, @Nullable String thingUid) {
+        ThingRegistry thingRegistry = this.thingRegistry;
+        if (thingUid != null && scope != null && thingRegistry != null) {
+            ThingUID uid = new ThingUID(thingUid);
+            Thing thing = thingRegistry.get(uid);
+            if (thing != null) {
+                ThingHandler handler = thing.getHandler();
+                if (handler != null) {
+                    return THING_ACTIONS_MAP.get(getKey(scope, thingUid));
+                }
             }
         }
         return null;
@@ -65,7 +66,9 @@ public class ScriptThingActions {
 
     void addThingActions(ThingActions thingActions) {
         String key = getKey(thingActions);
-        THING_ACTIONS_MAP.put(key, thingActions);
+        if (key != null) {
+            THING_ACTIONS_MAP.put(key, thingActions);
+        }
     }
 
     void removeThingActions(ThingActions thingActions) {
@@ -73,18 +76,26 @@ public class ScriptThingActions {
         THING_ACTIONS_MAP.remove(key);
     }
 
-    private static String getKey(ThingActions thingActions) {
+    private static @Nullable String getKey(ThingActions thingActions) {
         String scope = getScope(thingActions);
         String thingUID = getThingUID(thingActions);
-        return getKey(scope, thingUID);
+        if (thingUID == null) {
+            return null;
+        } else {
+            return getKey(scope, thingUID);
+        }
     }
 
     private static String getKey(String scope, String thingUID) {
         return scope + "-" + thingUID;
     }
 
-    private static String getThingUID(ThingActions actions) {
-        return actions.getThingHandler().getThing().getUID().getAsString();
+    private static @Nullable String getThingUID(ThingActions actions) {
+        ThingHandler thingHandler = actions.getThingHandler();
+        if (thingHandler == null) {
+            return null;
+        }
+        return thingHandler.getThing().getUID().getAsString();
     }
 
     private static String getScope(ThingActions actions) {
