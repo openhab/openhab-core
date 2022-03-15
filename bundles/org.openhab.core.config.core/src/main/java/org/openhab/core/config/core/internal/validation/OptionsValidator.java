e0605cdc544b6bfb8ad37b9b1463c558cdd52970
Jan N. Klug <github@klug.nrw>
2022-03-15 19:35:03 +0100

    fix spotless
    Signed-off-by: Jan N. Klug <github@klug.nrw>

--- bundles/org.openhab.core.config.core/src/main/java/org/openhab/core/config/core/internal/validation/OptionsValidator.java
+++ bundles/org.openhab.core.config.core/src/main/java/org/openhab/core/config/core/internal/validation/OptionsValidator.java
@@ -36,7 +36,8 @@ final class OptionsValidator implements ConfigDescriptionParameterValidator {
 
         boolean invalid;
 
-        if (param.getType() == ConfigDescriptionParameter.Type.DECIMAL || param.getType() == ConfigDescriptionParameter.Type.INTEGER) {
+        if (param.getType() == ConfigDescriptionParameter.Type.DECIMAL
+                || param.getType() == ConfigDescriptionParameter.Type.INTEGER) {
             BigDecimal bdValue = new BigDecimal(value.toString());
             invalid = param.getOptions().stream().map(o -> new BigDecimal(o.getValue()))
                     .noneMatch(v -> v.compareTo(bdValue) == 0);
