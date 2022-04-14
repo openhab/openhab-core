dbe0794d45107b7a59fa796f2ab38a5b8588894e
clinique <gael@lhopital.org>
2022-04-14 10:57:55 +0200

    Reverting one change
    Signed-off-by: clinique <gael@lhopital.org>

--- bundles/org.openhab.core.thing/src/main/java/org/openhab/core/thing/binding/builder/ChannelBuilder.java
+++ bundles/org.openhab.core.thing/src/main/java/org/openhab/core/thing/binding/builder/ChannelBuilder.java
@@ -181,6 +181,9 @@ public class ChannelBuilder {
      * @return channel builder
      */
     public ChannelBuilder withKind(ChannelKind kind) {
+        if (kind == null) {
+            throw new IllegalArgumentException("'ChannelKind' must not be null");
+        }
         this.kind = kind;
         return this;
     }
