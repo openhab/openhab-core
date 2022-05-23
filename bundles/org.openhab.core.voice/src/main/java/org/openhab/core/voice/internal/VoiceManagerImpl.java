openhab/openhab-core

commit 5753627c02601310471c53318fa8e7294efd8ab1

[voice] Allow list of HLIs  in dialog function (#2906)
* [Voice] Allow hli list

Signed-off-by: Miguel Álvarez Díez <miguelwork92@gmail.com>

bundles/org.openhab.core.voice/src/main/java/org/openhab/core/voice/internal/VoiceManagerImpl.java
@@ -15,6 +15,7 @@ package org.openhab.core.voice.internal;
 import java.io.IOException;
 import java.net.URI;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Comparator;
@@ -288,20 +289,38 @@ public class VoiceManagerImpl implements VoiceManager, ConfigOptionProvider {
     }
 
     @Override
-    public String interpret(String text, @Nullable String hliId) throws InterpretationException {
-        HumanLanguageInterpreter interpreter;
-        if (hliId == null) {
-            interpreter = getHLI();
+    public String interpret(String text, @Nullable String hliIdList) throws InterpretationException {
+        List<HumanLanguageInterpreter> interpreters;
+        if (hliIdList == null) {
+            HumanLanguageInterpreter interpreter = getHLI();
             if (interpreter == null) {
                 throw new InterpretationException("No human language interpreter available!");
             }
+            interpreters = List.of(interpreter);
         } else {
-            interpreter = getHLI(hliId);
-            if (interpreter == null) {
-                throw new InterpretationException("No human language interpreter can be found for " + hliId);
+            interpreters = getHLIsByIds(hliIdList);
+            if (interpreters.isEmpty()) {
+                throw new InterpretationException("No human language interpreter can be found for " + hliIdList);
             }
         }
-        return interpreter.interpret(localeProvider.getLocale(), text);
+        String answer = null;
+        InterpretationException error = null;
+        Locale locale = localeProvider.getLocale();
+        for (var interpreter : interpreters) {
+            try {
+                answer = interpreter.interpret(locale, text);
+                logger.debug("Interpretation result: {}", answer);
+                error = null;
+                break;
+            } catch (InterpretationException e) {
+                logger.debug("Interpretation exception: {}", e.getMessage());
+                error = e;
+            }
+        }
+        if (error != null) {
+            throw error;
+        }
+        return answer;
     }
 
     private @Nullable Voice getVoice(String id) {
@@ -480,7 +499,7 @@ public class VoiceManagerImpl implements VoiceManager, ConfigOptionProvider {
 
     @Override
     public void startDialog() throws IllegalStateException {
-        startDialog(null, null, null, null, null, null, null, this.keyword, this.listeningItem);
+        startDialog(null, null, null, List.of(), null, null, null, this.keyword, this.listeningItem);
     }
 
     @Override
@@ -488,11 +507,25 @@ public class VoiceManagerImpl implements VoiceManager, ConfigOptionProvider {
             @Nullable HumanLanguageInterpreter hli, @Nullable AudioSource source, @Nullable AudioSink sink,
             @Nullable Locale locale, @Nullable String keyword, @Nullable String listeningItem)
             throws IllegalStateException {
+        startDialog(ks, stt, tts, hli == null ? List.of() : List.of(hli), source, sink, locale, keyword, listeningItem);
+    }
+
+    @Override
+    public void startDialog(@Nullable KSService ks, @Nullable STTService stt, @Nullable TTSService tts,
+            List<HumanLanguageInterpreter> hlis, @Nullable AudioSource source, @Nullable AudioSink sink,
+            @Nullable Locale locale, @Nullable String keyword, @Nullable String listeningItem)
+            throws IllegalStateException {
         // use defaults, if null
         KSService ksService = (ks == null) ? getKS() : ks;
         STTService sttService = (stt == null) ? getSTT() : stt;
         TTSService ttsService = (tts == null) ? getTTS() : tts;
-        HumanLanguageInterpreter interpreter = (hli == null) ? getHLI() : hli;
+        List<HumanLanguageInterpreter> interpreters = hlis;
+        if (interpreters.isEmpty()) {
+            HumanLanguageInterpreter hli = getHLI();
+            if (hli != null) {
+                interpreters = List.of(hli);
+            }
+        }
         AudioSource audioSource = (source == null) ? audioManager.getSource() : source;
         AudioSink audioSink = (sink == null) ? audioManager.getSink() : sink;
         Locale loc = (locale == null) ? localeProvider.getLocale() : locale;
@@ -500,19 +533,19 @@ public class VoiceManagerImpl implements VoiceManager, ConfigOptionProvider {
         String item = (listeningItem == null) ? this.listeningItem : listeningItem;
         Bundle b = bundle;
 
-        if (ksService == null || sttService == null || ttsService == null || interpreter == null || audioSource == null
-                || audioSink == null || b == null) {
+        if (ksService == null || sttService == null || ttsService == null || interpreters.isEmpty()
+                || audioSource == null || audioSink == null || b == null) {
             throw new IllegalStateException("Cannot start dialog as services are missing.");
         } else if (!checkLocales(ksService.getSupportedLocales(), loc)
-                || !checkLocales(sttService.getSupportedLocales(), loc)
-                || !checkLocales(interpreter.getSupportedLocales(), loc)) {
+                || !checkLocales(sttService.getSupportedLocales(), loc) || !interpreters.stream()
+                        .allMatch(interpreter -> checkLocales(interpreter.getSupportedLocales(), loc))) {
             throw new IllegalStateException("Cannot start dialog as provided locale is not supported by all services.");
         } else {
             DialogProcessor processor = dialogProcessors.get(audioSource.getId());
             if (processor == null) {
                 logger.debug("Starting a new dialog for source {} ({})", audioSource.getLabel(null),
                         audioSource.getId());
-                processor = new DialogProcessor(ksService, sttService, ttsService, interpreter, audioSource, audioSink,
+                processor = new DialogProcessor(ksService, sttService, ttsService, interpreters, audioSource, audioSink,
                         loc, kw, item, this.eventPublisher, this.i18nProvider, b);
                 dialogProcessors.put(audioSource.getId(), processor);
                 processor.start();
@@ -551,28 +584,34 @@ public class VoiceManagerImpl implements VoiceManager, ConfigOptionProvider {
 
     @Override
     public void listenAndAnswer() throws IllegalStateException {
-        listenAndAnswer(null, null, null, null, null, null, null);
+        listenAndAnswer(null, null, List.of(), null, null, null, null);
     }
 
     @Override
-    public void listenAndAnswer(@Nullable STTService stt, @Nullable TTSService tts,
-            @Nullable HumanLanguageInterpreter hli, @Nullable AudioSource source, @Nullable AudioSink sink,
-            @Nullable Locale locale, @Nullable String listeningItem) throws IllegalStateException {
+    public void listenAndAnswer(@Nullable STTService stt, @Nullable TTSService tts, List<HumanLanguageInterpreter> hlis,
+            @Nullable AudioSource source, @Nullable AudioSink sink, @Nullable Locale locale,
+            @Nullable String listeningItem) throws IllegalStateException {
         // use defaults, if null
         STTService sttService = (stt == null) ? getSTT() : stt;
         TTSService ttsService = (tts == null) ? getTTS() : tts;
-        HumanLanguageInterpreter interpreter = (hli == null) ? getHLI() : hli;
+        List<HumanLanguageInterpreter> interpreters = hlis;
+        if (interpreters.isEmpty()) {
+            HumanLanguageInterpreter hli = getHLI();
+            if (hli != null) {
+                interpreters = List.of(hli);
+            }
+        }
         AudioSource audioSource = (source == null) ? audioManager.getSource() : source;
         AudioSink audioSink = (sink == null) ? audioManager.getSink() : sink;
         Locale loc = (locale == null) ? localeProvider.getLocale() : locale;
         String item = (listeningItem == null) ? this.listeningItem : listeningItem;
         Bundle b = bundle;
 
-        if (sttService == null || ttsService == null || interpreter == null || audioSource == null || audioSink == null
-                || b == null) {
+        if (sttService == null || ttsService == null || interpreters.isEmpty() || audioSource == null
+                || audioSink == null || b == null) {
             throw new IllegalStateException("Cannot execute a simple dialog as services are missing.");
-        } else if (!checkLocales(sttService.getSupportedLocales(), loc)
-                || !checkLocales(interpreter.getSupportedLocales(), loc)) {
+        } else if (!checkLocales(sttService.getSupportedLocales(), loc) || !interpreters.stream()
+                .allMatch(interpreter -> checkLocales(interpreter.getSupportedLocales(), loc))) {
             throw new IllegalStateException(
                     "Cannot execute a simple dialog as provided locale is not supported by all services.");
         } else {
@@ -580,7 +619,7 @@ public class VoiceManagerImpl implements VoiceManager, ConfigOptionProvider {
             if (processor == null) {
                 logger.debug("Executing a simple dialog for source {} ({})", audioSource.getLabel(null),
                         audioSource.getId());
-                processor = new DialogProcessor(sttService, ttsService, interpreter, audioSource, audioSink, loc, item,
+                processor = new DialogProcessor(sttService, ttsService, interpreters, audioSource, audioSink, loc, item,
                         this.eventPublisher, this.i18nProvider, b);
                 processor.start();
             } else {
@@ -739,6 +778,25 @@ public class VoiceManagerImpl implements VoiceManager, ConfigOptionProvider {
     @Override
     public @Nullable HumanLanguageInterpreter getHLI(String id) {
         return humanLanguageInterpreters.get(id);
+    }
+
+    @Override
+    public List<HumanLanguageInterpreter> getHLIsByIds(String ids) {
+        return getHLIsByIds(Arrays.asList(ids.split(",")));
+    }
+
+    @Override
+    public List<HumanLanguageInterpreter> getHLIsByIds(List<String> ids) {
+        List<HumanLanguageInterpreter> interpreters = new ArrayList<>();
+        for (String id : ids) {
+            HumanLanguageInterpreter hli = humanLanguageInterpreters.get(id);
+            if (hli == null) {
+                logger.warn("HumanLanguageInterpreter '{}' not available!", id);
+            } else {
+                interpreters.add(hli);
+            }
+        }
+        return interpreters;
     }
 
     @Override

bundles/org.openhab.core.io.rest.voice/src/main/java/org/openhab/core/io/rest/voice/internal/VoiceResource.java
@@ -14,6 +14,7 @@ package org.openhab.core.io.rest.voice.internal;
 
 import java.util.List;
 import java.util.Locale;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 import javax.annotation.security.RolesAllowed;
@@ -55,6 +56,8 @@ import org.osgi.service.jaxrs.whiteboard.propertytypes.JSONRequired;
 import org.osgi.service.jaxrs.whiteboard.propertytypes.JaxrsApplicationSelect;
 import org.osgi.service.jaxrs.whiteboard.propertytypes.JaxrsName;
 import org.osgi.service.jaxrs.whiteboard.propertytypes.JaxrsResource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
@@ -86,6 +89,7 @@ public class VoiceResource implements RESTResource {
     /** The URI path to this resource */
     public static final String PATH_VOICE = "voice";
 
+    private final Logger logger = LoggerFactory.getLogger(VoiceResource.class);
     private final LocaleService localeService;
     private final AudioManager audioManager;
     private final VoiceManager voiceManager;
@@ -133,27 +137,38 @@ public class VoiceResource implements RESTResource {
     }
 
     @POST
-    @Path("/interpreters/{id: [a-zA-Z_0-9]+}")
+    @Path("/interpreters/{ids: [a-zA-Z_0-9,]+}")
     @Consumes(MediaType.TEXT_PLAIN)
-    @Operation(operationId = "interpretText", summary = "Sends a text to a given human language interpreter.", responses = {
+    @Operation(operationId = "interpretText", summary = "Sends a text to a given human language interpreter(s).", responses = {
             @ApiResponse(responseCode = "200", description = "OK"),
             @ApiResponse(responseCode = "404", description = "No human language interpreter was found."),
             @ApiResponse(responseCode = "400", description = "interpretation exception occurs") })
     public Response interpret(
             @HeaderParam(HttpHeaders.ACCEPT_LANGUAGE) @Parameter(description = "language") @Nullable String language,
             @Parameter(description = "text to interpret", required = true) String text,
-            @PathParam("id") @Parameter(description = "interpreter id") String id) {
+            @PathParam("ids") @Parameter(description = "comma separated list of interpreter ids") List<String> ids) {
         final Locale locale = localeService.getLocale(language);
-        HumanLanguageInterpreter hli = voiceManager.getHLI(id);
-        if (hli == null) {
+        List<HumanLanguageInterpreter> hlis = voiceManager.getHLIsByIds(ids);
+        if (hlis.isEmpty()) {
             return JSONResponse.createErrorResponse(Status.NOT_FOUND, "No interpreter found");
         }
-
-        try {
-            hli.interpret(locale, text);
-            return Response.ok(null, MediaType.TEXT_PLAIN).build();
-        } catch (InterpretationException e) {
-            return JSONResponse.createErrorResponse(Status.BAD_REQUEST, e.getMessage());
+        String answer = "";
+        String error = null;
+        for (HumanLanguageInterpreter interpreter : hlis) {
+            try {
+                answer = interpreter.interpret(locale, text);
+                logger.debug("Interpretation result: {}", answer);
+                error = null;
+                break;
+            } catch (InterpretationException e) {
+                logger.debug("Interpretation exception: {}", e.getMessage());
+                error = Objects.requireNonNullElse(e.getMessage(), "Unexpected error");
+            }
+        }
+        if (error != null) {
+            return JSONResponse.createErrorResponse(Status.BAD_REQUEST, error);
+        } else {
+            return Response.ok(answer, MediaType.TEXT_PLAIN).build();
         }
     }
 
@@ -232,7 +247,7 @@ public class VoiceResource implements RESTResource {
             @QueryParam("ksId") @Parameter(description = "keywork spotter ID") @Nullable String ksId,
             @QueryParam("sttId") @Parameter(description = "Speech-to-Text ID") @Nullable String sttId,
             @QueryParam("ttsId") @Parameter(description = "Text-to-Speech ID") @Nullable String ttsId,
-            @QueryParam("hliId") @Parameter(description = "interpreter ID") @Nullable String hliId,
+            @QueryParam("hliIds") @Parameter(description = "comma separated list of interpreter IDs") @Nullable String hliIds,
             @QueryParam("sinkId") @Parameter(description = "audio sink ID") @Nullable String sinkId,
             @QueryParam("keyword") @Parameter(description = "keyword") @Nullable String keyword,
             @QueryParam("listeningItem") @Parameter(description = "listening item") @Nullable String listeningItem) {
@@ -264,10 +279,10 @@ public class VoiceResource implements RESTResource {
                 return JSONResponse.createErrorResponse(Status.NOT_FOUND, "Text-to-Speech not found");
             }
         }
-        HumanLanguageInterpreter hli = null;
-        if (hliId != null) {
-            hli = voiceManager.getHLI(hliId);
-            if (hli == null) {
+        List<HumanLanguageInterpreter> interpreters = List.of();
+        if (hliIds != null) {
+            interpreters = voiceManager.getHLIsByIds(hliIds);
+            if (interpreters.isEmpty()) {
                 return JSONResponse.createErrorResponse(Status.NOT_FOUND, "Interpreter not found");
             }
         }
@@ -281,7 +296,7 @@ public class VoiceResource implements RESTResource {
         final Locale locale = localeService.getLocale(language);
 
         try {
-            voiceManager.startDialog(ks, stt, tts, hli, source, sink, locale, keyword, listeningItem);
+            voiceManager.startDialog(ks, stt, tts, interpreters, source, sink, locale, keyword, listeningItem);
             return Response.ok(null, MediaType.TEXT_PLAIN).build();
         } catch (IllegalStateException e) {
             return JSONResponse.createErrorResponse(Status.BAD_REQUEST, e.getMessage());
@@ -325,7 +340,7 @@ public class VoiceResource implements RESTResource {
             @QueryParam("sourceId") @Parameter(description = "source ID") @Nullable String sourceId,
             @QueryParam("sttId") @Parameter(description = "Speech-to-Text ID") @Nullable String sttId,
             @QueryParam("ttsId") @Parameter(description = "Text-to-Speech ID") @Nullable String ttsId,
-            @QueryParam("hliId") @Parameter(description = "interpreter ID") @Nullable String hliId,
+            @QueryParam("hliIds") @Parameter(description = "interpreter IDs") @Nullable List<String> hliIds,
             @QueryParam("sinkId") @Parameter(description = "audio sink ID") @Nullable String sinkId,
             @QueryParam("listeningItem") @Parameter(description = "listening item") @Nullable String listeningItem) {
         AudioSource source = null;
@@ -349,10 +364,10 @@ public class VoiceResource implements RESTResource {
                 return JSONResponse.createErrorResponse(Status.NOT_FOUND, "Text-to-Speech not found");
             }
         }
-        HumanLanguageInterpreter hli = null;
-        if (hliId != null) {
-            hli = voiceManager.getHLI(hliId);
-            if (hli == null) {
+        List<HumanLanguageInterpreter> interpreters = List.of();
+        if (hliIds != null) {
+            interpreters = voiceManager.getHLIsByIds(hliIds);
+            if (interpreters.isEmpty()) {
                 return JSONResponse.createErrorResponse(Status.NOT_FOUND, "Interpreter not found");
             }
         }
@@ -366,7 +381,7 @@ public class VoiceResource implements RESTResource {
         final Locale locale = localeService.getLocale(language);
 
         try {
-            voiceManager.listenAndAnswer(stt, tts, hli, source, sink, locale, listeningItem);
+            voiceManager.listenAndAnswer(stt, tts, interpreters, source, sink, locale, listeningItem);
             return Response.ok(null, MediaType.TEXT_PLAIN).build();
         } catch (IllegalStateException e) {
             return JSONResponse.createErrorResponse(Status.BAD_REQUEST, e.getMessage());

commit 980a7a71a36de72e58fa45313c04de84f9ee1064

Fix null type mismatch errors
This fixes the null type errors in Eclipse which were introduced by #2906.

Signed-off-by: Wouter Born <github@maindrain.net>

bundles/org.openhab.core.voice/src/main/java/org/openhab/core/voice/internal/VoiceManagerImpl.java
@@ -290,37 +290,35 @@ public class VoiceManagerImpl implements VoiceManager, ConfigOptionProvider {
 
     @Override
     public String interpret(String text, @Nullable String hliIdList) throws InterpretationException {
-        List<HumanLanguageInterpreter> interpreters;
+        List<HumanLanguageInterpreter> interpreters = new ArrayList<>();
         if (hliIdList == null) {
             HumanLanguageInterpreter interpreter = getHLI();
-            if (interpreter == null) {
-                throw new InterpretationException("No human language interpreter available!");
+            if (interpreter != null) {
+                interpreters.add(interpreter);
             }
-            interpreters = List.of(interpreter);
         } else {
             interpreters = getHLIsByIds(hliIdList);
-            if (interpreters.isEmpty()) {
-                throw new InterpretationException("No human language interpreter can be found for " + hliIdList);
+        }
+
+        if (!interpreters.isEmpty()) {
+            Locale locale = localeProvider.getLocale();
+            for (var interpreter : interpreters) {
+                try {
+                    String answer = interpreter.interpret(locale, text);
+                    logger.debug("Interpretation result: {}", answer);
+                    return answer;
+                } catch (InterpretationException e) {
+                    logger.debug("Interpretation exception: {}", e.getMessage());
+                    throw e;
+                }
             }
         }
-        String answer = null;
-        InterpretationException error = null;
-        Locale locale = localeProvider.getLocale();
-        for (var interpreter : interpreters) {
-            try {
-                answer = interpreter.interpret(locale, text);
-                logger.debug("Interpretation result: {}", answer);
-                error = null;
-                break;
-            } catch (InterpretationException e) {
-                logger.debug("Interpretation exception: {}", e.getMessage());
-                error = e;
-            }
+
+        if (hliIdList == null) {
+            throw new InterpretationException("No human language interpreter available!");
+        } else {
+            throw new InterpretationException("No human language interpreter can be found for " + hliIdList);
         }
-        if (error != null) {
-            throw error;
-        }
-        return answer;
     }
 
     private @Nullable Voice getVoice(String id) {
@@ -354,27 +352,13 @@ public class VoiceManagerImpl implements VoiceManager, ConfigOptionProvider {
         // Return the first concrete AudioFormat found
         for (AudioFormat currentAudioFormat : audioFormats) {
             // Check if currentAudioFormat is abstract
-            if (null == currentAudioFormat.getCodec()) {
+            if ((null == currentAudioFormat.getCodec()) || (null == currentAudioFormat.getContainer())
+                    || (null == currentAudioFormat.isBigEndian()) || (null == currentAudioFormat.getBitDepth())) {
                 continue;
             }
-            if (null == currentAudioFormat.getContainer()) {
-                continue;
-            }
-            if (null == currentAudioFormat.isBigEndian()) {
-                continue;
-            }
-            if (null == currentAudioFormat.getBitDepth()) {
-                continue;
-            }
-            if (null == currentAudioFormat.getBitRate()) {
-                continue;
-            }
-            if (null == currentAudioFormat.getFrequency()) {
-                continue;
-            }
-
             // Prefer WAVE container
-            if (!AudioFormat.CONTAINER_WAVE.equals(currentAudioFormat.getContainer())) {
+            if ((null == currentAudioFormat.getBitRate()) || (null == currentAudioFormat.getFrequency())
+                    || !AudioFormat.CONTAINER_WAVE.equals(currentAudioFormat.getContainer())) {
                 continue;
             }
 
@@ -388,15 +372,9 @@ public class VoiceManagerImpl implements VoiceManager, ConfigOptionProvider {
             AudioFormat format = currentAudioFormat;
 
             // Not all Codecs and containers can be supported
-            if (null == format.getCodec()) {
-                continue;
-            }
-            if (null == format.getContainer()) {
-                continue;
-            }
-
             // Prefer WAVE container
-            if (!AudioFormat.CONTAINER_WAVE.equals(format.getContainer())) {
+            if ((null == format.getCodec()) || (null == format.getContainer())
+                    || !AudioFormat.CONTAINER_WAVE.equals(format.getContainer())) {
                 continue;
             }
 
@@ -813,7 +791,7 @@ public class VoiceManagerImpl implements VoiceManager, ConfigOptionProvider {
     }
 
     private Set<Voice> getAllVoicesSorted(Locale locale) {
-        return ttsServices.values().stream().map(s -> s.getAvailableVoices()).flatMap(Collection::stream)
+        return ttsServices.values().stream().map(TTSService::getAvailableVoices).flatMap(Collection::stream)
                 .sorted(createVoiceComparator(locale)).collect(Collectors
                         .collectingAndThen(Collectors.toCollection(LinkedHashSet::new), Collections::unmodifiableSet));
     }
@@ -836,9 +814,8 @@ public class VoiceManagerImpl implements VoiceManager, ConfigOptionProvider {
             return (tts1 == null || tts2 == null) ? 0
                     : tts1.getLabel(locale).compareToIgnoreCase(tts2.getLabel(locale));
         };
-        Comparator<Voice> byVoiceLocale = (Voice v1, Voice v2) -> {
-            return v1.getLocale().getDisplayName(locale).compareToIgnoreCase(v2.getLocale().getDisplayName(locale));
-        };
+        Comparator<Voice> byVoiceLocale = (Voice v1, Voice v2) -> v1.getLocale().getDisplayName(locale)
+                .compareToIgnoreCase(v2.getLocale().getDisplayName(locale));
         return byTTSLabel.thenComparing(byVoiceLocale).thenComparing(Voice::getLabel);
     }
 

bundles/org.openhab.core.automation.module.script/src/test/java/org/openhab/core/automation/module/script/ScriptTransformationServiceTest.java
@@ -13,16 +13,13 @@
 package org.openhab.core.automation.module.script;
 
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.instanceOf;
-import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.*;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.verifyNoMoreInteractions;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
+
+import java.util.Objects;
 
 import javax.script.ScriptContext;
 import javax.script.ScriptEngine;
@@ -81,7 +78,7 @@ public class ScriptTransformationServiceTest {
 
     @Test
     public void success() throws TransformationException {
-        String returnValue = service.transform(SCRIPT_TYPE + ":" + SCRIPT_UID, "input");
+        String returnValue = Objects.requireNonNull(service.transform(SCRIPT_TYPE + ":" + SCRIPT_UID, "input"));
 
         assertThat(returnValue, is(SCRIPT_OUTPUT));
     }