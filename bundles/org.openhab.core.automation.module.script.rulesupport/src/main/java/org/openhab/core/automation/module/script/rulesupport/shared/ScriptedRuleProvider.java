ed73b7c5c4b51781a5d65e2f90dcf2f2e7f7a2f1
Cody Cutrer <cody@cutrer.us>
2022-11-21 16:14:05 -0700

    tweak ScriptedRuleProvider slightly
    just extend AbstractProvider so that it doesn't need to re-implement
    managing listeners
    
    Signed-off-by: Cody Cutrer <cody@cutrer.us>

--- bundles/org.openhab.core.automation.module.script.rulesupport/src/main/java/org/openhab/core/automation/module/script/rulesupport/shared/ScriptedRuleProvider.java
+++ bundles/org.openhab.core.automation.module.script.rulesupport/src/main/java/org/openhab/core/automation/module/script/rulesupport/shared/ScriptedRuleProvider.java
@@ -12,7 +12,6 @@
  */
 package org.openhab.core.automation.module.script.rulesupport.shared;
 
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
@@ -20,43 +19,29 @@ import java.util.Map;
 import org.eclipse.jdt.annotation.NonNullByDefault;
 import org.openhab.core.automation.Rule;
 import org.openhab.core.automation.RuleProvider;
-import org.openhab.core.common.registry.ProviderChangeListener;
+import org.openhab.core.common.registry.AbstractProvider;
 import org.osgi.service.component.annotations.Component;
 
 /**
- * This RuleProvider keeps Rules at added by scripts during the runtime. This ensures that Rules are not kept on reboot,
+ * This RuleProvider keeps Rules added by scripts during runtime. This ensures that Rules are not kept on reboot,
  * but have to be added by the scripts again.
  *
  * @author Simon Merschjohann - Initial contribution
  */
 @NonNullByDefault
 @Component(immediate = true, service = { ScriptedRuleProvider.class, RuleProvider.class })
-public class ScriptedRuleProvider implements RuleProvider {
-    private final Collection<ProviderChangeListener<Rule>> listeners = new ArrayList<>();
-
+public class ScriptedRuleProvider extends AbstractProvider<Rule> implements RuleProvider {
     private final Map<String, Rule> rules = new HashMap<>();
-
-    @Override
-    public void addProviderChangeListener(ProviderChangeListener<Rule> listener) {
-        listeners.add(listener);
-    }
 
     @Override
     public Collection<Rule> getAll() {
         return rules.values();
     }
 
-    @Override
-    public void removeProviderChangeListener(ProviderChangeListener<Rule> listener) {
-        listeners.remove(listener);
-    }
-
     public void addRule(Rule rule) {
         rules.put(rule.getUID(), rule);
 
-        for (ProviderChangeListener<Rule> providerChangeListener : listeners) {
-            providerChangeListener.added(this, rule);
-        }
+        notifyListenersAboutAddedElement(rule);
     }
 
     public void removeRule(String ruleUID) {
@@ -68,8 +53,6 @@ public class ScriptedRuleProvider implements RuleProvider {
 
     public void removeRule(Rule rule) {
         rules.remove(rule.getUID());
-        for (ProviderChangeListener<Rule> providerChangeListener : listeners) {
-            providerChangeListener.removed(this, rule);
-        }
+        notifyListenersAboutRemovedElement(rule);
     }
 }
