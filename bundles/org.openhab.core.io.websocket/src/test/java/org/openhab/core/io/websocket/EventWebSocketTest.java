da2d6a8a21e9505d59dc82fec816e177e3d5c560
J-N-K <github@klug.nrw>
2022-12-10 12:28:58 +0100

    Add EventWebSocket (#2891)
    Signed-off-by: Jan N. Klug <github@klug.nrw>

---
+++ bundles/org.openhab.core.io.websocket/src/test/java/org/openhab/core/io/websocket/EventWebSocketTest.java
@@ -0,0 +1,248 @@
+/**
+ * Copyright (c) 2010-2022 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.core.io.websocket;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.openhab.core.io.websocket.EventWebSocket.WEBSOCKET_EVENT_TYPE;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.util.Objects;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.eclipse.jdt.annotation.Nullable;
+import org.eclipse.jetty.websocket.api.RemoteEndpoint;
+import org.eclipse.jetty.websocket.api.Session;
+import org.eclipse.jetty.websocket.api.StatusCode;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.junit.jupiter.MockitoSettings;
+import org.mockito.quality.Strictness;
+import org.openhab.core.events.Event;
+import org.openhab.core.events.EventPublisher;
+import org.openhab.core.items.ItemNotFoundException;
+import org.openhab.core.items.ItemRegistry;
+import org.openhab.core.items.events.ItemEventFactory;
+import org.openhab.core.library.items.NumberItem;
+import org.openhab.core.library.types.DecimalType;
+
+import com.google.gson.Gson;
+
+/**
+ * The {@link EventWebSocketTest} contains tests for the {@link EventWebSocket}
+ *
+ * @author Jan N. Klug - Initial contribution
+ */
+@NonNullByDefault
+@ExtendWith(MockitoExtension.class)
+@MockitoSettings(strictness = Strictness.LENIENT)
+public class EventWebSocketTest {
+    private static final String REMOTE_WEBSOCKET_IMPLEMENTATION = "fooWebsocket";
+
+    private static final String TEST_ITEM_NAME = "testItem";
+    private static final NumberItem TEST_ITEM = new NumberItem(TEST_ITEM_NAME);
+
+    private Gson gson = new Gson();
+
+    private @Mock @NonNullByDefault({}) EventWebSocketServlet servlet;
+    private @Mock @NonNullByDefault({}) ItemRegistry itemRegistry;
+    private @Mock @NonNullByDefault({}) EventPublisher eventPublisher;
+    private @Mock @NonNullByDefault({}) Session session;
+    private @Mock @NonNullByDefault({}) RemoteEndpoint remoteEndpoint;
+
+    private @NonNullByDefault({}) ItemEventUtility itemEventUtility;
+    private @NonNullByDefault({}) EventWebSocket eventWebSocket;
+
+    @BeforeEach
+    public void setup() throws ItemNotFoundException {
+        itemEventUtility = new ItemEventUtility(gson, itemRegistry);
+        eventWebSocket = new EventWebSocket(gson, servlet, itemEventUtility, eventPublisher);
+
+        when(session.getRemote()).thenReturn(remoteEndpoint);
+        when(remoteEndpoint.getInetSocketAddress()).thenReturn(new InetSocketAddress(47115));
+
+        when(itemRegistry.getItem(eq(TEST_ITEM_NAME))).thenReturn(TEST_ITEM);
+
+        eventWebSocket.onConnect(session);
+        verify(servlet).registerListener(eventWebSocket);
+    }
+
+    @Test
+    public void listenerCorrectlyUnregisteredOnClose() {
+        eventWebSocket.onClose(StatusCode.NORMAL, "Normal close.");
+
+        verify(servlet).unregisterListener(eventWebSocket);
+    }
+
+    @Test
+    public void sessionClosesOnErrorAndOnCloseCalled() {
+        eventWebSocket.onError(session, new IllegalStateException());
+
+        verify(session).close();
+        verify(servlet).unregisterListener(eventWebSocket);
+    }
+
+    @Test
+    public void stateEventWithIdFromWebsocketIsPublishedAndConfirmed() throws IOException {
+        Event expectedEvent = ItemEventFactory.createStateEvent(TEST_ITEM_NAME, DecimalType.ZERO,
+                REMOTE_WEBSOCKET_IMPLEMENTATION);
+        EventDTO eventDTO = new EventDTO(expectedEvent);
+        eventDTO.eventId = "id-1";
+        EventDTO expectedResponse = new EventDTO(WEBSOCKET_EVENT_TYPE, "/response/success", "", null, eventDTO.eventId);
+
+        assertEventProcessing(eventDTO, expectedEvent, expectedResponse);
+    }
+
+    @Test
+    public void stateEventWithoutIdFromWebsocketIsPublished() throws IOException {
+        Event expectedEvent = ItemEventFactory.createStateEvent(TEST_ITEM_NAME, DecimalType.ZERO,
+                REMOTE_WEBSOCKET_IMPLEMENTATION);
+        EventDTO eventDTO = new EventDTO(expectedEvent);
+
+        assertEventProcessing(eventDTO, expectedEvent, null);
+    }
+
+    @Test
+    public void commandEventWithIdFromWebsocketIsPublishedAndConfirmed() throws IOException {
+        Event expectedEvent = ItemEventFactory.createCommandEvent(TEST_ITEM_NAME, DecimalType.ZERO,
+                REMOTE_WEBSOCKET_IMPLEMENTATION);
+
+        EventDTO eventDTO = new EventDTO(expectedEvent);
+        eventDTO.eventId = "id-1";
+        EventDTO expectedResponse = new EventDTO(WEBSOCKET_EVENT_TYPE, "/response/success", "", null, eventDTO.eventId);
+
+        assertEventProcessing(eventDTO, expectedEvent, expectedResponse);
+    }
+
+    @Test
+    public void commandEventWithoutIdFromWebsocketIsPublished() throws IOException {
+        Event expectedEvent = ItemEventFactory.createCommandEvent(TEST_ITEM_NAME, DecimalType.ZERO,
+                REMOTE_WEBSOCKET_IMPLEMENTATION);
+
+        EventDTO eventDTO = new EventDTO(expectedEvent);
+        assertEventProcessing(eventDTO, expectedEvent, null);
+    }
+
+    @Test
+    public void illegalStateEventNotPublishedAndResponseSent() throws IOException {
+        Event expectedEvent = ItemEventFactory.createStateEvent(TEST_ITEM_NAME, DecimalType.ZERO,
+                REMOTE_WEBSOCKET_IMPLEMENTATION);
+
+        EventDTO eventDTO = new EventDTO(expectedEvent);
+        eventDTO.payload = "";
+
+        EventDTO expectedResponse = new EventDTO(WEBSOCKET_EVENT_TYPE, "/response/failed",
+                "Processing error: Failed to deserialize payload \u0027\u0027.", null, null);
+
+        assertEventProcessing(eventDTO, null, expectedResponse);
+    }
+
+    @Test
+    public void illegalCommandEventNotPublishedAndResponseSent() throws IOException {
+        Event expectedEvent = ItemEventFactory.createCommandEvent(TEST_ITEM_NAME, DecimalType.ZERO,
+                REMOTE_WEBSOCKET_IMPLEMENTATION);
+
+        EventDTO eventDTO = new EventDTO(expectedEvent);
+        eventDTO.eventId = "id-1";
+        eventDTO.topic = "";
+
+        EventDTO expectedResponse = new EventDTO(WEBSOCKET_EVENT_TYPE, "/response/failed",
+                "Processing error: Topic must follow the format {namespace}/{entityType}/{entity}/{action}.", null,
+                eventDTO.eventId);
+
+        assertEventProcessing(eventDTO, null, expectedResponse);
+    }
+
+    @Test
+    public void heartBeat() throws IOException {
+        EventDTO eventDTO = new EventDTO(WEBSOCKET_EVENT_TYPE, "/heartbeat", "PING", null, null);
+        EventDTO expectedResponse = new EventDTO(WEBSOCKET_EVENT_TYPE, "/heartbeat", "PONG", null, null);
+
+        assertEventProcessing(eventDTO, null, expectedResponse);
+    }
+
+    @Test
+    public void eventFromBusSent() throws IOException {
+        Event event = ItemEventFactory.createCommandEvent(TEST_ITEM_NAME, DecimalType.ZERO,
+                REMOTE_WEBSOCKET_IMPLEMENTATION);
+
+        eventWebSocket.processEvent(event);
+        EventDTO eventDTO = new EventDTO(event);
+
+        verify(remoteEndpoint).sendString(gson.toJson(eventDTO));
+    }
+
+    @Test
+    public void eventFromBusFilterType() throws IOException {
+        EventDTO eventDTO = new EventDTO(WEBSOCKET_EVENT_TYPE, "/filter/type", "[\"ItemCommandEvent\"]", null, null);
+        EventDTO responseEventDTO = new EventDTO(WEBSOCKET_EVENT_TYPE, "/filter/type", eventDTO.payload, null, null);
+        eventWebSocket.onText(gson.toJson(eventDTO));
+        verify(remoteEndpoint).sendString(gson.toJson(responseEventDTO));
+
+        // subscribed type is sent
+        Event event = ItemEventFactory.createCommandEvent(TEST_ITEM_NAME, DecimalType.ZERO,
+                REMOTE_WEBSOCKET_IMPLEMENTATION);
+        eventWebSocket.processEvent(event);
+        verify(remoteEndpoint).sendString(gson.toJson(new EventDTO(event)));
+
+        // not subscribed event not sent
+        event = ItemEventFactory.createStateEvent(TEST_ITEM_NAME, DecimalType.ZERO, REMOTE_WEBSOCKET_IMPLEMENTATION);
+        eventWebSocket.processEvent(event);
+        verify(remoteEndpoint, times(2)).sendString(any());
+    }
+
+    @Test
+    public void eventFromBusFilterSource() throws IOException {
+        EventDTO eventDTO = new EventDTO(WEBSOCKET_EVENT_TYPE, "/filter/source",
+                "[\"" + REMOTE_WEBSOCKET_IMPLEMENTATION + "\"]", null, null);
+        EventDTO responseEventDTO = new EventDTO(WEBSOCKET_EVENT_TYPE, "/filter/source", eventDTO.payload, null, null);
+        eventWebSocket.onText(gson.toJson(eventDTO));
+        verify(remoteEndpoint).sendString(gson.toJson(responseEventDTO));
+
+        // non-matching is sent
+        Event event = ItemEventFactory.createCommandEvent(TEST_ITEM_NAME, DecimalType.ZERO);
+        eventWebSocket.processEvent(event);
+        verify(remoteEndpoint).sendString(gson.toJson(new EventDTO(event)));
+
+        // matching is not sent
+        event = ItemEventFactory.createStateEvent(TEST_ITEM_NAME, DecimalType.ZERO, REMOTE_WEBSOCKET_IMPLEMENTATION);
+        eventWebSocket.processEvent(event);
+        verify(remoteEndpoint, times(2)).sendString(any());
+    }
+
+    private void assertEventProcessing(EventDTO incoming, @Nullable Event expectedEvent,
+            @Nullable EventDTO expectedResponse) throws IOException {
+        eventWebSocket.onText(gson.toJson(incoming));
+
+        if (expectedEvent != null) {
+            verify(eventPublisher).post(eq(Objects.requireNonNull(expectedEvent)));
+        } else {
+            verify(eventPublisher, never()).post(any());
+        }
+
+        if (expectedResponse != null) {
+            String expectedResponseString = gson.toJson(expectedResponse);
+            verify(remoteEndpoint).sendString(eq(expectedResponseString));
+        } else {
+            verify(remoteEndpoint, never()).sendString(any());
+        }
+    }
+}
---
+++ bundles/org.openhab.core.io.websocket/src/test/java/org/openhab/core/io/websocket/ItemEventUtilityTest.java
@@ -0,0 +1,171 @@
+/**
+ * Copyright (c) 2010-2022 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.core.io.websocket;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.when;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.junit.jupiter.MockitoSettings;
+import org.mockito.quality.Strictness;
+import org.openhab.core.events.Event;
+import org.openhab.core.items.ItemNotFoundException;
+import org.openhab.core.items.ItemRegistry;
+import org.openhab.core.items.events.ItemEvent;
+import org.openhab.core.items.events.ItemEventFactory;
+import org.openhab.core.library.items.StringItem;
+import org.openhab.core.library.types.DecimalType;
+import org.openhab.core.library.types.HSBType;
+import org.openhab.core.library.types.StringType;
+
+import com.google.gson.Gson;
+
+/**
+ * The {@link ItemEventUtilityTest} contains tests for the {@link ItemEventUtility} class.
+ *
+ * @author Jan N. Klug - Initial contribution
+ */
+@NonNullByDefault
+@ExtendWith(MockitoExtension.class)
+@MockitoSettings(strictness = Strictness.LENIENT)
+public class ItemEventUtilityTest {
+    private static final String EXISTING_ITEM_NAME = "existingItem";
+    private static final String NON_EXISTING_ITEM_NAME = "nonExistingItem";
+    private static final StringType ITEM_STATE = new StringType("foo");
+
+    private @Mock @NonNullByDefault({}) ItemRegistry itemRegistry;
+
+    private StringItem existingItem = new StringItem(EXISTING_ITEM_NAME);
+    private Gson gson = new Gson();
+
+    private @NonNullByDefault({}) ItemEventUtility itemEventUtility;
+
+    @BeforeEach
+    public void setUp() throws ItemNotFoundException {
+        itemEventUtility = new ItemEventUtility(gson, itemRegistry);
+
+        when(itemRegistry.getItem(eq(EXISTING_ITEM_NAME))).thenReturn(existingItem);
+        when(itemRegistry.getItem(eq(NON_EXISTING_ITEM_NAME)))
+                .thenThrow(new ItemNotFoundException(NON_EXISTING_ITEM_NAME));
+    }
+
+    @Test
+    public void validStateEvent() throws EventProcessingException {
+        ItemEvent event = ItemEventFactory.createStateEvent(EXISTING_ITEM_NAME, ITEM_STATE);
+        EventDTO eventDTO = new EventDTO(event);
+
+        Event itemEvent = itemEventUtility.createStateEvent(eventDTO);
+
+        assertThat(itemEvent, is(event));
+    }
+
+    @Test
+    public void validStateEventWithMissingItem() {
+        ItemEvent event = ItemEventFactory.createStateEvent(NON_EXISTING_ITEM_NAME, ITEM_STATE);
+        EventDTO eventDTO = new EventDTO(event);
+
+        EventProcessingException e = assertThrows(EventProcessingException.class,
+                () -> itemEventUtility.createStateEvent(eventDTO));
+        assertThat(e.getMessage(), is("Could not find item '" + NON_EXISTING_ITEM_NAME + "' in registry."));
+    }
+
+    @Test
+    public void validStateEventWithInvalidState() {
+        ItemEvent event = ItemEventFactory.createStateEvent(EXISTING_ITEM_NAME, DecimalType.ZERO);
+        EventDTO eventDTO = new EventDTO(event);
+
+        EventProcessingException e = assertThrows(EventProcessingException.class,
+                () -> itemEventUtility.createStateEvent(eventDTO));
+        assertThat(e.getMessage(), is("Incompatible datatype, rejected."));
+    }
+
+    @Test
+    public void invalidStateEventTopic() {
+        ItemEvent event = ItemEventFactory.createCommandEvent(EXISTING_ITEM_NAME, HSBType.BLACK);
+        EventDTO eventDTO = new EventDTO(event);
+
+        EventProcessingException e = assertThrows(EventProcessingException.class,
+                () -> itemEventUtility.createStateEvent(eventDTO));
+        assertThat(e.getMessage(), is("Topic does not match event type."));
+    }
+
+    @Test
+    public void invalidStateEventPayload() {
+        ItemEvent event = ItemEventFactory.createStateEvent(EXISTING_ITEM_NAME, HSBType.BLACK);
+        EventDTO eventDTO = new EventDTO(event);
+        eventDTO.payload = "invalidNoJson";
+
+        EventProcessingException e = assertThrows(EventProcessingException.class,
+                () -> itemEventUtility.createStateEvent(eventDTO));
+        assertThat(e.getMessage(), is("Failed to deserialize payload 'invalidNoJson'."));
+    }
+
+    @Test
+    public void validCommandEvent() throws EventProcessingException {
+        ItemEvent event = ItemEventFactory.createCommandEvent(EXISTING_ITEM_NAME, ITEM_STATE);
+        EventDTO eventDTO = new EventDTO(event);
+
+        Event itemEvent = itemEventUtility.createCommandEvent(eventDTO);
+
+        assertThat(itemEvent, is(event));
+    }
+
+    @Test
+    public void validCommandEventWithMissingItem() {
+        ItemEvent event = ItemEventFactory.createStateEvent(NON_EXISTING_ITEM_NAME, ITEM_STATE);
+        EventDTO eventDTO = new EventDTO(event);
+
+        EventProcessingException e = assertThrows(EventProcessingException.class,
+                () -> itemEventUtility.createStateEvent(eventDTO));
+        assertThat(e.getMessage(), is("Could not find item '" + NON_EXISTING_ITEM_NAME + "' in registry."));
+    }
+
+    @Test
+    public void validCommandEventWithInvalidState() {
+        ItemEvent event = ItemEventFactory.createCommandEvent(EXISTING_ITEM_NAME, HSBType.BLACK);
+        EventDTO eventDTO = new EventDTO(event);
+
+        EventProcessingException e = assertThrows(EventProcessingException.class,
+                () -> itemEventUtility.createCommandEvent(eventDTO));
+        assertThat(e.getMessage(), is("Incompatible datatype, rejected."));
+    }
+
+    @Test
+    public void invalidCommandEvent() {
+        ItemEvent event = ItemEventFactory.createStateEvent(EXISTING_ITEM_NAME, HSBType.BLACK);
+        EventDTO eventDTO = new EventDTO(event);
+
+        EventProcessingException e = assertThrows(EventProcessingException.class,
+                () -> itemEventUtility.createCommandEvent(eventDTO));
+        assertThat(e.getMessage(), is("Topic does not match event type."));
+    }
+
+    @Test
+    public void invalidCommandEventPayload() {
+        ItemEvent event = ItemEventFactory.createCommandEvent(EXISTING_ITEM_NAME, HSBType.BLACK);
+        EventDTO eventDTO = new EventDTO(event);
+        eventDTO.payload = "invalidNoJson";
+
+        EventProcessingException e = assertThrows(EventProcessingException.class,
+                () -> itemEventUtility.createCommandEvent(eventDTO));
+        assertThat(e.getMessage(), is("Failed to deserialize payload 'invalidNoJson'."));
+    }
+}
