928233795f9d02c7577cc690d540339d65c7e84e
Andrew Fiddian-Green <software@whitebear.ch>
2023-03-21 20:00:30 +0000

    Fourth attempt at synchronization
    Signed-off-by: Andrew Fiddian-Green <software@whitebear.ch>

--- itests/org.openhab.core.io.net.tests/src/main/java/org/openhab/core/io/net/tests/ClientFactoryTest.java
+++ itests/org.openhab.core.io.net.tests/src/main/java/org/openhab/core/io/net/tests/ClientFactoryTest.java
@@ -76,6 +76,7 @@ public class ClientFactoryTest extends JavaOSGiTest {
     private static final Completable<Boolean> HTTP_TEST_DONE = new Completable<>();
     private static final Completable<Boolean> HTTP2_TEST_DONE = new Completable<>();
     private static final Completable<Boolean> WS_TEST_DONE = new Completable<>();
+    private static final Object THREADED_TEST_PIPELINE = new Object();
 
     @AfterAll
     public static void afterAll() throws Exception {
@@ -105,134 +106,134 @@ public class ClientFactoryTest extends JavaOSGiTest {
     @Test
     public void testHttp1Client() throws Exception {
         SERVER_RUNNING.get(WAIT_SECONDS, TimeUnit.SECONDS);
+        synchronized (THREADED_TEST_PIPELINE) {
+            HttpClientFactory httpClientFactory = getService(HttpClientFactory.class);
+            assertNotNull(httpClientFactory);
 
-        HttpClientFactory httpClientFactory = getService(HttpClientFactory.class);
-        assertNotNull(httpClientFactory);
+            HttpClient client = httpClientFactory.createHttpClient(CONSUMER);
+            // HttpClient client = new HttpClient();
+            assertNotNull(client);
 
-        HttpClient client = httpClientFactory.createHttpClient(CONSUMER);
-        // HttpClient client = new HttpClient();
-        assertNotNull(client);
-
-        try {
-            // start client
-            client.setConnectTimeout(WAIT_SECONDS * 1000);
-            client.start();
-
-            // send request
-            ContentResponse response = client.GET(httpUrl);
-
-            // check response
-            assertEquals(HttpStatus.OK_200, response.getStatus());
-            assertEquals(RESPONSE, response.getContentAsString());
-        } finally {
             try {
-                // stop client
-                client.stop();
-            } catch (Exception e) {
+                // start client
+                client.setConnectTimeout(WAIT_SECONDS * 1000);
+                client.start();
+
+                // send request
+                ContentResponse response = client.GET(httpUrl);
+
+                // check response
+                assertEquals(HttpStatus.OK_200, response.getStatus());
+                assertEquals(RESPONSE, response.getContentAsString());
+            } finally {
+                try {
+                    // stop client
+                    client.stop();
+                } catch (Exception e) {
+                }
             }
         }
-
         HTTP_TEST_DONE.complete(true);
     }
 
     @Test
     public void testHttp2Client() throws Exception {
         SERVER_RUNNING.get(WAIT_SECONDS, TimeUnit.SECONDS);
+        synchronized (THREADED_TEST_PIPELINE) {
+            HttpClientFactory httpClientFactory = getService(HttpClientFactory.class);
+            assertNotNull(httpClientFactory);
 
-        HttpClientFactory httpClientFactory = getService(HttpClientFactory.class);
-        assertNotNull(httpClientFactory);
+            HTTP2Client client = httpClientFactory.createHttp2Client(CONSUMER);
+            // HTTP2Client client = new HTTP2Client();
+            assertNotNull(client);
 
-        HTTP2Client client = httpClientFactory.createHttp2Client(CONSUMER);
-        // HTTP2Client client = new HTTP2Client();
-        assertNotNull(client);
+            // initialize address
+            InetSocketAddress address = new InetSocketAddress(HOST, port);
 
-        // initialize address
-        InetSocketAddress address = new InetSocketAddress(HOST, port);
+            Completable<@Nullable Session> sessionCompletable = new Completable<>();
+            Completable<@Nullable Stream> streamCompletable = new Completable<>();
 
-        Completable<@Nullable Session> sessionCompletable = new Completable<>();
-        Completable<@Nullable Stream> streamCompletable = new Completable<>();
+            TestStreamAdapter streamAdapter = new TestStreamAdapter();
 
-        TestStreamAdapter streamAdapter = new TestStreamAdapter();
+            MetaData.Request request = new MetaData.Request(HttpMethod.GET.toString(), new HttpURI(http2Url),
+                    HttpVersion.HTTP_2, new HttpFields());
 
-        MetaData.Request request = new MetaData.Request(HttpMethod.GET.toString(), new HttpURI(http2Url),
-                HttpVersion.HTTP_2, new HttpFields());
+            HeadersFrame headers = new HeadersFrame(request, null, true);
 
-        HeadersFrame headers = new HeadersFrame(request, null, true);
-
-        try {
-            // start client
-            client.setConnectTimeout(WAIT_SECONDS * 1000);
-            client.start();
-
-            // establish session
-            client.connect(address, new Session.Listener.Adapter(), sessionCompletable);
-            Session session = sessionCompletable.get(WAIT_SECONDS, TimeUnit.SECONDS);
-            assertNotNull(session);
-            assertFalse(session.isClosed());
-
-            // open stream
-            session.newStream(headers, streamCompletable, streamAdapter);
-            Stream stream = streamCompletable.get(WAIT_SECONDS, TimeUnit.SECONDS);
-            assertNotNull(stream);
-
-            // confirm stream is open
-            assertFalse(stream.isClosed());
-            assertFalse(stream.isReset());
-
-            // check response
-            String response = streamAdapter.completable.get(WAIT_SECONDS, TimeUnit.SECONDS);
-            assertEquals(RESPONSE, response);
-        } finally {
             try {
-                // stop client
-                client.stop();
-            } catch (Exception e) {
+                // start client
+                client.setConnectTimeout(WAIT_SECONDS * 1000);
+                client.start();
+
+                // establish session
+                client.connect(address, new Session.Listener.Adapter(), sessionCompletable);
+                Session session = sessionCompletable.get(WAIT_SECONDS, TimeUnit.SECONDS);
+                assertNotNull(session);
+                assertFalse(session.isClosed());
+
+                // open stream
+                session.newStream(headers, streamCompletable, streamAdapter);
+                Stream stream = streamCompletable.get(WAIT_SECONDS, TimeUnit.SECONDS);
+                assertNotNull(stream);
+
+                // confirm stream is open
+                assertFalse(stream.isClosed());
+                assertFalse(stream.isReset());
+
+                // check response
+                String response = streamAdapter.completable.get(WAIT_SECONDS, TimeUnit.SECONDS);
+                assertEquals(RESPONSE, response);
+            } finally {
+                try {
+                    // stop client
+                    client.stop();
+                } catch (Exception e) {
+                }
             }
         }
-
         HTTP2_TEST_DONE.complete(true);
     }
 
     @Test
     public void testWebSocketClient() throws Exception {
         SERVER_RUNNING.get(WAIT_SECONDS, TimeUnit.SECONDS);
+        synchronized (THREADED_TEST_PIPELINE) {
+            WebSocketFactory webSocketFactory = getService(WebSocketFactory.class);
+            assertNotNull(webSocketFactory);
 
-        WebSocketFactory webSocketFactory = getService(WebSocketFactory.class);
-        assertNotNull(webSocketFactory);
+            WebSocketClient client = webSocketFactory.createWebSocketClient(CONSUMER);
+            // WebSocketClient client = new WebSocketClient();
+            assertNotNull(client);
 
-        WebSocketClient client = webSocketFactory.createWebSocketClient(CONSUMER);
-        // WebSocketClient client = new WebSocketClient();
-        assertNotNull(client);
-
-        try {
-            // start client
-            client.setConnectTimeout(WAIT_SECONDS * 1000);
-            client.start();
-
-            // initialize address
-            URI address = new URI(wsUrl);
-
-            // establish session
-            Future<org.eclipse.jetty.websocket.api.Session> session;
-            TestWebSocket webSocket = new TestWebSocket();
-            session = client.connect(webSocket, address);
-            assertNotNull(session);
-
-            // confirm session is open
-            assertFalse(session.isDone());
-            assertFalse(session.isCancelled());
-
-            // check response
-            String response = webSocket.completable.get(WAIT_SECONDS, TimeUnit.SECONDS);
-            assertEquals(RESPONSE, response);
-        } finally {
             try {
-                // stop client
-                client.stop();
-            } catch (Exception e) {
+                // start client
+                client.setConnectTimeout(WAIT_SECONDS * 1000);
+                client.start();
+
+                // initialize address
+                URI address = new URI(wsUrl);
+
+                // establish session
+                Future<org.eclipse.jetty.websocket.api.Session> session;
+                TestWebSocket webSocket = new TestWebSocket();
+                session = client.connect(webSocket, address);
+                assertNotNull(session);
+
+                // confirm session is open
+                assertFalse(session.isDone());
+                assertFalse(session.isCancelled());
+
+                // check response
+                String response = webSocket.completable.get(WAIT_SECONDS, TimeUnit.SECONDS);
+                assertEquals(RESPONSE, response);
+            } finally {
+                try {
+                    // stop client
+                    client.stop();
+                } catch (Exception e) {
+                }
             }
         }
-
         WS_TEST_DONE.complete(true);
     }
 }
