f593128360b3600b950b65df3e29d1e561d650d2
Jimmy Tanagra <jcode@tanagra.id.au>
2023-10-09 10:30:33 +1000

    Remove the duplicate unicode for micro
    Signed-off-by: Jimmy Tanagra <jcode@tanagra.id.au>

--- bundles/org.openhab.core/src/main/java/org/openhab/core/library/types/QuantityType.java
+++ bundles/org.openhab.core/src/main/java/org/openhab/core/library/types/QuantityType.java
@@ -71,10 +71,10 @@ public class QuantityType<T extends Quantity<T>> extends Number
     public static final QuantityType<Dimensionless> ONE = new QuantityType<>(1, AbstractUnit.ONE);
 
     // Regular expression to split unit from value. Split on any blank character, even none (\\s*)
-    // which occurs after a digit (?<=\\d) and before a "unit" character ?=[a-zA-Z°µ\u03BC\u00B5%']
+    // which occurs after a digit (?<=\\d) and before a "unit" character ?=[a-zA-Z°µ\u03BC%']
     // which itself must not be preceded by plus/minus digit (?![\\+\\-]?\\d).
     // The latter would be an exponent from the scalar value.
-    private static final String UNIT_PATTERN = "(?<=\\d)\\s*(?=[a-zA-Z°µ\u03BC\u00B5%'](?![\\+\\-]?\\d))";
+    private static final String UNIT_PATTERN = "(?<=\\d)\\s*(?=[a-zA-Z°µ\u03BC%'](?![\\+\\-]?\\d))";
 
     static {
         UnitInitializer.init();
