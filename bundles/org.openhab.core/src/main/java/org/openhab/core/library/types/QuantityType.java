e9d969c31d87d103cdb2122e1897476d7fea34e4
jimtng <2554958+jimtng@users.noreply.github.com>
2023-10-10 18:08:45 +1000

    Fix QuantityType UNIT_PATTERN regex (#3841)
    Signed-off-by: Jimmy Tanagra <jcode@tanagra.id.au>

--- bundles/org.openhab.core/src/main/java/org/openhab/core/library/types/QuantityType.java
+++ bundles/org.openhab.core/src/main/java/org/openhab/core/library/types/QuantityType.java
@@ -70,11 +70,11 @@ public class QuantityType<T extends Quantity<T>> extends Number
     public static final QuantityType<Dimensionless> ZERO = new QuantityType<>(0, AbstractUnit.ONE);
     public static final QuantityType<Dimensionless> ONE = new QuantityType<>(1, AbstractUnit.ONE);
 
-    // Regular expression to split unit from value. Split on any blank character, even none (\\s*)
-    // which occurs after a digit (?<=\\d) and before a "unit" character ?=[a-zA-Z°µ\u03BC%']
-    // which itself must not be preceded by plus/minus digit (?![\\+\\-]?\\d).
+    // Regular expression to split unit from value. Split on any blank character, or
+    // between a digit (?<=\\d) and a non-digit character (?=\\D)
+    // which must not be preceded by plus/minus digit (?![\\+\\-]?\\d).
     // The latter would be an exponent from the scalar value.
-    private static final String UNIT_PATTERN = "(?<=\\d)\\s*(?=[a-zA-Z°µ\u03BC%'](?![\\+\\-]?\\d))";
+    private static final String UNIT_PATTERN = "\\s+|(?<=\\d)(?=\\D(?![\\+\\-]?\\d))";
 
     static {
         UnitInitializer.init();
