feae11ed5af078f52233abad94272001289082e0
Marco Müller <marco@ms-mueller.ch>
2023-11-01 21:18:12 +0100

    Simplify calculation
    Signed-off-by: Marco Müller <marco@ms-mueller.ch>

--- bundles/org.openhab.core/src/main/java/org/openhab/core/util/ColorUtil.java
+++ bundles/org.openhab.core/src/main/java/org/openhab/core/util/ColorUtil.java
@@ -44,11 +44,11 @@ public class ColorUtil {
     private static final BigDecimal BIG_DECIMAL_120 = BigDecimal.valueOf(120);
     private static final BigDecimal BIG_DECIMAL_100 = BigDecimal.valueOf(100);
     private static final BigDecimal BIG_DECIMAL_60 = BigDecimal.valueOf(60);
+    private static final BigDecimal BIG_DECIMAL_50 = BigDecimal.valueOf(50);
     private static final BigDecimal BIG_DECIMAL_5 = BigDecimal.valueOf(5);
     private static final BigDecimal BIG_DECIMAL_3 = BigDecimal.valueOf(3);
     private static final BigDecimal BIG_DECIMAL_2 = BigDecimal.valueOf(2);
     private static final BigDecimal BIG_DECIMAL_2_POINT_55 = new BigDecimal("2.55");
-    private static final BigDecimal BIG_DECIMAL_0 = BigDecimal.valueOf(0);
     private static final BigDecimal BIG_DECIMAL_127_POINT_5 = BigDecimal.valueOf(127.5);
 
     public static final Gamut DEFAULT_GAMUT = new Gamut(new double[] { 0.9961, 0.0001 }, new double[] { 0, 0.9961 },
@@ -158,10 +158,10 @@ public class ColorUtil {
      * @return array of four {@link PercentType} with the RGBW values in the range 0 to 100 percent.
      */
     public static PercentType[] hsbToRgbwPercent(HSBType hsb) {
-        int[] rgb = hsbToRgb(hsb);
-        final BigDecimal Ri = new BigDecimal(rgb[0]);
-        final BigDecimal Gi = new BigDecimal(rgb[1]);
-        final BigDecimal Bi = new BigDecimal(rgb[2]);
+        PercentType[] rgb = hsbToRgbPercent(hsb);
+        final BigDecimal Ri = rgb[0].toBigDecimal();
+        final BigDecimal Gi = rgb[1].toBigDecimal();
+        final BigDecimal Bi = rgb[2].toBigDecimal();
         // Get the maximum between R, G, and B
         final BigDecimal tM = Ri.max(Gi.max(Bi));
 
@@ -171,7 +171,7 @@ public class ColorUtil {
         }
 
         // This section serves to figure out what the color with 100% hue is
-        final BigDecimal multiplier = BIG_DECIMAL_255.divide(tM, 0, RoundingMode.DOWN);
+        final BigDecimal multiplier = BIG_DECIMAL_100.divide(tM, 0, RoundingMode.DOWN);
         final BigDecimal hR = Ri.multiply(multiplier);
         final BigDecimal hG = Gi.multiply(multiplier);
         final BigDecimal hB = Bi.multiply(multiplier);
@@ -179,23 +179,14 @@ public class ColorUtil {
         // This calculates the Whiteness (not strictly speaking Luminance) of the color
         final BigDecimal M = hR.max(hG.max(hB));
         final BigDecimal m = hR.min(hG.min(hB));
-        final BigDecimal Luminance = ((M.add(m).divide(BIG_DECIMAL_2).subtract(BIG_DECIMAL_127_POINT_5))
-                .multiply(BIG_DECIMAL_255.divide(BIG_DECIMAL_127_POINT_5))).divide(multiplier);
-
-        // Calculate the output values
-        BigDecimal Ro = (Ri.subtract(Luminance).multiply(BIG_DECIMAL_100).divide(BIG_DECIMAL_255, 0,
-                RoundingMode.DOWN));
-        BigDecimal Go = (Gi.subtract(Luminance).multiply(BIG_DECIMAL_100).divide(BIG_DECIMAL_255, 0,
-                RoundingMode.DOWN));
-        BigDecimal Bo = (Bi.subtract(Luminance).multiply(BIG_DECIMAL_100).divide(BIG_DECIMAL_255, 0,
-                RoundingMode.DOWN));
-        BigDecimal Wo = Luminance.multiply(BIG_DECIMAL_100).divide(BIG_DECIMAL_255, 0, RoundingMode.DOWN);
+        final BigDecimal Luminance = ((M.add(m).divide(BIG_DECIMAL_2).subtract(BIG_DECIMAL_50))
+                .multiply(BIG_DECIMAL_100.divide(BIG_DECIMAL_50))).divide(multiplier);
 
         // check range
-        Ro = BIG_DECIMAL_100.min(Ro.max(BIG_DECIMAL_0));
-        Go = BIG_DECIMAL_100.min(Go.max(BIG_DECIMAL_0));
-        Bo = BIG_DECIMAL_100.min(Bo.max(BIG_DECIMAL_0));
-        Wo = BIG_DECIMAL_100.min(Wo.max(BIG_DECIMAL_0));
+        final BigDecimal Ro = Ri.subtract(Luminance).max(BigDecimal.ZERO);
+        final BigDecimal Go = Gi.subtract(Luminance).max(BigDecimal.ZERO);
+        final BigDecimal Bo = Bi.subtract(Luminance).max(BigDecimal.ZERO);
+        final BigDecimal Wo = Luminance.max(BigDecimal.ZERO);
 
         return new PercentType[] { new PercentType(Ro), new PercentType(Go), new PercentType(Bo), new PercentType(Wo) };
     }
@@ -223,7 +214,7 @@ public class ColorUtil {
         BigDecimal Bi = BigDecimal.valueOf(b).add(Luminance);
 
         // Get the maximum between R, G, and B
-        final BigDecimal tM = BIG_DECIMAL_255.min(Ri.max(Gi.max(Bi)).max(BIG_DECIMAL_0));
+        final BigDecimal tM = BIG_DECIMAL_255.min(Ri.max(Gi.max(Bi)).max(BigDecimal.ZERO));
 
         // If the maximum value is 0, immediately return pure black.
         if (tM.floatValue() == 0) {
@@ -232,9 +223,9 @@ public class ColorUtil {
 
         final BigDecimal multiplier = BIG_DECIMAL_255.divide(tM, 0, RoundingMode.DOWN);
 
-        BigDecimal Ro = Ri.divide(multiplier).min(BIG_DECIMAL_255).max(BIG_DECIMAL_0);
-        BigDecimal Go = Gi.divide(multiplier).min(BIG_DECIMAL_255).max(BIG_DECIMAL_0);
-        BigDecimal Bo = Bi.divide(multiplier).min(BIG_DECIMAL_255).max(BIG_DECIMAL_0);
+        BigDecimal Ro = Ri.divide(multiplier).min(BIG_DECIMAL_255).max(BigDecimal.ZERO);
+        BigDecimal Go = Gi.divide(multiplier).min(BIG_DECIMAL_255).max(BigDecimal.ZERO);
+        BigDecimal Bo = Bi.divide(multiplier).min(BIG_DECIMAL_255).max(BigDecimal.ZERO);
 
         return HSBType.fromRGB(Ro.intValue(), Go.intValue(), Bo.intValue());
     }
