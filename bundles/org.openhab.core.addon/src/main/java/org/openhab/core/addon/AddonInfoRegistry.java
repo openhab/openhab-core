be4f73825949b0241b3f3ad20cb5d858675b4c0c
Kai Kreuzer <kai@openhab.org>
2023-12-09 14:38:50 +0100

    Revert "[addon] fix #2204 (#3905)"
    This reverts commit bb30eab3b9b71b6280692d717473901ecd8c10fd.

--- bundles/org.openhab.core.addon/src/main/java/org/openhab/core/addon/AddonInfoRegistry.java
+++ bundles/org.openhab.core.addon/src/main/java/org/openhab/core/addon/AddonInfoRegistry.java
@@ -97,15 +97,8 @@ public class AddonInfoRegistry {
             return a;
         }
         AddonInfo.Builder builder = AddonInfo.builder(a);
-        if (AddonInfo.NA.equals(a.getName())) {
-            builder.withName(b.getName());
-        } else if (AddonInfo.NA.equals(b.getName())) {
-            builder.withName(a.getName());
-        }
-        if (AddonInfo.NA.equals(a.getDescription())) {
+        if (a.getDescription().isEmpty()) {
             builder.withDescription(b.getDescription());
-        } else if (AddonInfo.NA.equals(b.getDescription())) {
-            builder.withDescription(a.getDescription());
         }
         if (a.getConnection() == null && b.getConnection() != null) {
             builder.withConnection(b.getConnection());
