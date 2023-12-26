9974f100c983b56943653b6a2d66332a9f6fee81
Jan N. Klug <github@klug.nrw>
2023-12-26 19:31:49 +0100

    Fix month, week, day not supported
    Due to the way month, week and day are defined in Indriya their symbol is not added as alias. This is a bug in indriya, but their release cycles are quite long and we should provide a fix for our users.
    
    This should be backported to 4.1.x
    
    Signed-off-by: Jan N. Klug <github@klug.nrw>

--- bundles/org.openhab.core/src/main/java/org/openhab/core/library/unit/Units.java
+++ bundles/org.openhab.core/src/main/java/org/openhab/core/library/unit/Units.java
@@ -191,6 +191,7 @@ public final class Units extends CustomUnits {
     public static final Unit<Time> HOUR = addUnit(tech.units.indriya.unit.Units.HOUR);
     public static final Unit<Time> DAY = addUnit(tech.units.indriya.unit.Units.DAY);
     public static final Unit<Time> WEEK = addUnit(tech.units.indriya.unit.Units.WEEK);
+    public static final Unit<Time> MONTH = addUnit(tech.units.indriya.unit.Units.MONTH);
     public static final Unit<Time> YEAR = addUnit(tech.units.indriya.unit.Units.YEAR);
     public static final Unit<VolumetricFlowRate> LITRE_PER_MINUTE = addUnit(new ProductUnit<VolumetricFlowRate>(
             tech.units.indriya.unit.Units.LITRE.divide(tech.units.indriya.unit.Units.MINUTE)));
@@ -309,6 +310,11 @@ public final class Units extends CustomUnits {
         SimpleUnitFormat.getInstance().label(VOLT_AMPERE_HOUR, "VAh");
         SimpleUnitFormat.getInstance().label(WATT_HOUR, "Wh");
         SimpleUnitFormat.getInstance().label(WATT_SECOND, "Ws");
+
+        // workarounds for https://github.com/unitsofmeasurement/indriya/issues/409
+        SimpleUnitFormat.getInstance().alias(MONTH, "mo");
+        SimpleUnitFormat.getInstance().alias(WEEK, "wk");
+        SimpleUnitFormat.getInstance().alias(YEAR, "y");
     }
 
     private Units() {
