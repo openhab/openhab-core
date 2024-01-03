564d6e673e5909a4af4c36c70e3eb341ed65f972
Wouter Born <github@maindrain.net>
2024-01-03 12:15:41 +0100

    Fix `SerialPort.getFlowControlMode()` infinite recursion (#4007)
    An infinite recursion would occur when calling this method.
    I did not find any add-ons using this method but that could be due to this bug.
    
    Signed-off-by: Wouter Born <github@maindrain.net>

--- bundles/org.openhab.core.io.transport.serial.rxtx/src/main/java/org/openhab/core/io/transport/serial/rxtx/RxTxSerialPort.java
+++ bundles/org.openhab.core.io.transport.serial.rxtx/src/main/java/org/openhab/core/io/transport/serial/rxtx/RxTxSerialPort.java
@@ -206,7 +206,7 @@ public class RxTxSerialPort implements SerialPort {
 
     @Override
     public int getFlowControlMode() {
-        return getFlowControlMode();
+        return sp.getFlowControlMode();
     }
 
     @Override
