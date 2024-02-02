6422ba122309e6aea14ced77727970f4b9420336
MV <mvcode00@gmail.com>
2024-02-02 02:08:38 +0100

    [jserialcomm-transport] First version of new serial transport based on jSerialComm
    Signed-off-by: MV <mvcode00@gmail.com>

---
+++ bundles/org.openhab.core.io.transport.serial.jserialcomm/src/main/java/org/openhab/core/io/transport/serial/internal/SerialPortEventImpl.java
@@ -0,0 +1,49 @@
+/**
+ * Copyright (c) 2010-2024 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.core.io.transport.serial.internal;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.openhab.core.io.transport.serial.SerialPortEvent;
+
+/**
+ * Specific serial port implementation using com.fazecast.jSerialComm.SerialPortEvent.
+ *
+ * @author Massimo Valla - Initial contribution
+ */
+@NonNullByDefault
+public class SerialPortEventImpl implements SerialPortEvent {
+
+    private final com.fazecast.jSerialComm.SerialPortEvent event;
+
+    /**
+     * Constructor.
+     *
+     * @param event the underlying event implementation
+     */
+    public SerialPortEventImpl(final com.fazecast.jSerialComm.SerialPortEvent event) {
+        this.event = event;
+    }
+
+    @Override
+    public int getEventType() {
+        // FIXME check event id mapping
+        return event.getEventType();
+    }
+
+    @Override
+    public boolean getNewValue() {
+        // FIXME placeholder implementation
+        return false;
+        // return event.getNewValue();
+    }
+}
---
+++ bundles/org.openhab.core.io.transport.serial.jserialcomm/src/main/java/org/openhab/core/io/transport/serial/internal/SerialPortIdentifierImpl.java
@@ -0,0 +1,76 @@
+/**
+ * Copyright (c) 2010-2024 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.core.io.transport.serial.internal;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.eclipse.jdt.annotation.Nullable;
+import org.openhab.core.io.transport.serial.PortInUseException;
+import org.openhab.core.io.transport.serial.SerialPort;
+import org.openhab.core.io.transport.serial.SerialPortIdentifier;
+import org.openhab.core.io.transport.serial.jserialcomm.JSerialCommSerialPort;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Specific serial port identifier implementation.
+ *
+ * @author Massimo Valla - Initial contribution
+ */
+@NonNullByDefault
+public class SerialPortIdentifierImpl implements SerialPortIdentifier {
+
+    private final Logger logger = LoggerFactory.getLogger(SerialPortIdentifierImpl.class);
+
+    final com.fazecast.jSerialComm.SerialPort sp;
+
+    /**
+     * Constructor.
+     *
+     * @param sp
+     */
+    public SerialPortIdentifierImpl(final com.fazecast.jSerialComm.SerialPort sp) {
+        this.sp = sp;
+    }
+
+    @Override
+    public String getName() {
+        final String name = sp.getSystemPortName();
+        return name != null ? name : "";
+    }
+
+    @Override
+    public SerialPort open(String owner, int timeout) throws PortInUseException {
+
+        logger.debug("jSerialComm --- SerialPort.getReadTimeout() = " + sp.getReadTimeout());
+        logger.debug("jSerialComm --- SerialPort.getPortDescription() = " + sp.getPortDescription());
+
+        boolean success = sp.openPort();
+        if (success) {
+            return new JSerialCommSerialPort(sp);
+        } else {
+            logger.error("jSerialComm --- Could not open port: " + sp.getSystemPortName());
+
+            throw new PortInUseException(new Exception("Could not open port: " + sp.getSystemPortName()));
+        }
+    }
+
+    @Override
+    public boolean isCurrentlyOwned() {
+        return false;
+    }
+
+    @Override
+    public @Nullable String getCurrentOwner() {
+        return "jserialcomm owner";
+    }
+}
