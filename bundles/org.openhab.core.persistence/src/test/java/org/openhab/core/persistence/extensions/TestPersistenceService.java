1b503afdbbc654e0d845dce7c713813cf652ead5
Mark Herwege <mherwege@users.noreply.github.com>
2024-05-02 11:47:12 +0200

    Return units in persistence extension commands and support future persisted states (#3736)
    * persistence extensions future and QuantityTypes
    
    Signed-off-by: Mark Herwege <mark.herwege@telenet.be>

--- bundles/org.openhab.core.persistence/src/test/java/org/openhab/core/persistence/extensions/TestPersistenceService.java
+++ bundles/org.openhab.core.persistence/src/test/java/org/openhab/core/persistence/extensions/TestPersistenceService.java
@@ -12,6 +12,7 @@
  */
 package org.openhab.core.persistence.extensions;
 
+import java.time.Duration;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.time.temporal.ChronoUnit;
@@ -21,6 +22,7 @@ import java.util.List;
 import java.util.Locale;
 import java.util.Objects;
 import java.util.Set;
+import java.util.stream.LongStream;
 import java.util.stream.Stream;
 
 import javax.measure.Unit;
@@ -45,11 +47,44 @@ import org.openhab.core.types.State;
  * A simple persistence service used for unit tests
  *
  * @author Kai Kreuzer - Initial contribution
+ * @author Mark Herwege - Allow future values
  */
 @NonNullByDefault
 public class TestPersistenceService implements QueryablePersistenceService {
 
-    public static final String ID = "test";
+    public static final String SERVICE_ID = "test";
+
+    static final int SWITCH_START = -15;
+    static final int SWITCH_ON_1 = -15;
+    static final int SWITCH_ON_INTERMEDIATE_1 = -12;
+    static final int SWITCH_OFF_1 = -10;
+    static final int SWITCH_OFF_INTERMEDIATE_1 = -6;
+    static final int SWITCH_ON_2 = -5;
+    static final int SWITCH_ON_INTERMEDIATE_21 = -1;
+    static final int SWITCH_ON_INTERMEDIATE_22 = +1;
+    static final int SWITCH_OFF_2 = +5;
+    static final int SWITCH_OFF_INTERMEDIATE_2 = +7;
+    static final int SWITCH_ON_3 = +10;
+    static final int SWITCH_ON_INTERMEDIATE_3 = +12;
+    static final int SWITCH_OFF_3 = +15;
+    static final int SWITCH_END = +15;
+    static final OnOffType SWITCH_STATE = OnOffType.ON;
+
+    static final int BEFORE_START = 1940;
+    static final int HISTORIC_START = 1950;
+    static final int HISTORIC_INTERMEDIATE_VALUE_1 = 2005;
+    static final int HISTORIC_INTERMEDIATE_VALUE_2 = 2011;
+    static final int HISTORIC_END = 2012;
+    static final int HISTORIC_INTERMEDIATE_NOVALUE_3 = 2019;
+    static final int HISTORIC_INTERMEDIATE_NOVALUE_4 = 2021;
+    static final int FUTURE_INTERMEDIATE_NOVALUE_1 = 2051;
+    static final int FUTURE_INTERMEDIATE_NOVALUE_2 = 2056;
+    static final int FUTURE_START = 2060;
+    static final int FUTURE_INTERMEDIATE_VALUE_3 = 2070;
+    static final int FUTURE_INTERMEDIATE_VALUE_4 = 2077;
+    static final int FUTURE_END = 2100;
+    static final int AFTER_END = 2110;
+    static final DecimalType STATE = new DecimalType(HISTORIC_END);
 
     private final ItemRegistry itemRegistry;
 
@@ -59,7 +94,7 @@ public class TestPersistenceService implements QueryablePersistenceService {
 
     @Override
     public String getId() {
-        return ID;
+        return SERVICE_ID;
     }
 
     @Override
@@ -73,18 +108,17 @@ public class TestPersistenceService implements QueryablePersistenceService {
     @Override
     public Iterable<HistoricItem> query(FilterCriteria filter) {
         if (PersistenceExtensionsTest.TEST_SWITCH.equals(filter.getItemName())) {
-            ZonedDateTime now = ZonedDateTime.now().truncatedTo(ChronoUnit.MINUTES),
-                    nowMinusFifteenHours = now.minusHours(15),
-                    beginDate = filter.getBeginDate() != null ? filter.getBeginDate() : nowMinusFifteenHours,
-                    endDate = filter.getEndDate() != null ? filter.getEndDate() : now;
-            if (endDate.isBefore(beginDate)) {
-                return List.of();
-            }
+            ZonedDateTime now = ZonedDateTime.now().truncatedTo(ChronoUnit.MINUTES);
+            ZonedDateTime nowMinusHours = now.plusHours(SWITCH_START);
+            ZonedDateTime endDate = filter.getEndDate();
+            endDate = endDate != null ? endDate : now;
+            ZonedDateTime beginDate = filter.getBeginDate();
+            beginDate = beginDate != null ? beginDate : endDate.isAfter(now) ? now : endDate.minusHours(1);
 
-            List<HistoricItem> results = new ArrayList<>(16);
-            for (int i = 0; i <= 15; i++) {
-                final int hours = i;
-                final ZonedDateTime theDate = nowMinusFifteenHours.plusHours(hours);
+            List<HistoricItem> results = new ArrayList<>(31);
+            for (int i = SWITCH_START; i <= SWITCH_END; i++) {
+                final int hour = i;
+                final ZonedDateTime theDate = nowMinusHours.plusHours(i - SWITCH_START);
                 if (!theDate.isBefore(beginDate) && !theDate.isAfter(endDate)) {
                     results.add(new HistoricItem() {
                         @Override
@@ -94,7 +128,8 @@ public class TestPersistenceService implements QueryablePersistenceService {
 
                         @Override
                         public State getState() {
-                            return OnOffType.from(hours < 5 || hours > 10);
+                            return OnOffType.from(hour < SWITCH_OFF_1 || (hour >= SWITCH_ON_2 && hour < SWITCH_OFF_2)
+                                    || hour >= SWITCH_ON_3);
                         }
 
                         @Override
@@ -117,22 +152,27 @@ public class TestPersistenceService implements QueryablePersistenceService {
             }
             return stream.toList();
         } else {
-            int startValue = 1950;
-            int endValue = 2012;
+            int startValue = HISTORIC_START;
+            int endValue = FUTURE_END;
 
-            if (filter.getBeginDate() != null) {
-                startValue = filter.getBeginDate().getYear();
+            ZonedDateTime beginDate = filter.getBeginDate();
+            if (beginDate != null && beginDate.getYear() >= startValue) {
+                startValue = beginDate.getYear();
             }
-            if (filter.getEndDate() != null) {
-                endValue = filter.getEndDate().getYear();
+            ZonedDateTime endDate = filter.getEndDate();
+            if (endDate != null && endDate.getYear() <= endValue) {
+                endValue = endDate.getYear();
             }
 
-            if (endValue <= startValue || startValue < 1950) {
+            if (endValue <= startValue) {
                 return List.of();
             }
 
             List<HistoricItem> results = new ArrayList<>(endValue - startValue);
             for (int i = startValue; i <= endValue; i++) {
+                if (i > HISTORIC_END && i < FUTURE_START) {
+                    continue;
+                }
                 final int year = i;
                 results.add(new HistoricItem() {
                     @Override
@@ -181,5 +221,47 @@ public class TestPersistenceService implements QueryablePersistenceService {
     @Override
     public List<PersistenceStrategy> getDefaultStrategies() {
         return List.of();
+    }
+
+    static OnOffType switchValue(int hour) {
+        return (hour >= SWITCH_ON_1 && hour < SWITCH_OFF_1) || (hour >= SWITCH_ON_2 && hour < SWITCH_OFF_2)
+                || (hour >= SWITCH_ON_3 && hour < SWITCH_OFF_3) ? OnOffType.ON : OnOffType.OFF;
+    }
+
+    static DecimalType value(long year) {
+        if (year < HISTORIC_START) {
+            return DecimalType.ZERO;
+        } else if (year <= HISTORIC_END) {
+            return new DecimalType(year);
+        } else if (year < FUTURE_START) {
+            return new DecimalType(HISTORIC_END);
+        } else if (year <= FUTURE_END) {
+            return new DecimalType(year);
+        } else {
+            return new DecimalType(FUTURE_END);
+        }
+    }
+
+    static double average(@Nullable Integer beginYear, @Nullable Integer endYear) {
+        ZonedDateTime now = ZonedDateTime.now();
+        ZonedDateTime beginDate = beginYear != null
+                ? ZonedDateTime.of(beginYear >= HISTORIC_START ? beginYear : HISTORIC_START, 1, 1, 0, 0, 0, 0,
+                        ZoneId.systemDefault())
+                : now;
+        ZonedDateTime endDate = endYear != null ? ZonedDateTime.of(endYear, 1, 1, 0, 0, 0, 0, ZoneId.systemDefault())
+                : now;
+        int begin = beginYear != null ? beginYear : now.getYear() + 1;
+        int end = endYear != null ? endYear : now.getYear();
+        long sum = LongStream.range(begin, end).map(y -> value(y).longValue() * Duration
+                .between(ZonedDateTime.of(Long.valueOf(y).intValue(), 1, 1, 0, 0, 0, 0, ZoneId.systemDefault()),
+                        ZonedDateTime.of(Long.valueOf(y + 1).intValue(), 1, 1, 0, 0, 0, 0, ZoneId.systemDefault()))
+                .toMillis()).sum();
+        sum += beginYear == null ? value(now.getYear()).longValue() * Duration
+                .between(now, ZonedDateTime.of(now.getYear() + 1, 1, 1, 0, 0, 0, 0, ZoneId.systemDefault())).toMillis()
+                : 0;
+        sum += endYear == null ? value(now.getYear()).longValue() * Duration
+                .between(ZonedDateTime.of(now.getYear(), 1, 1, 0, 0, 0, 0, ZoneId.systemDefault()), now).toMillis() : 0;
+        long duration = Duration.between(beginDate, endDate).toMillis();
+        return 1.0 * sum / duration;
     }
 }
