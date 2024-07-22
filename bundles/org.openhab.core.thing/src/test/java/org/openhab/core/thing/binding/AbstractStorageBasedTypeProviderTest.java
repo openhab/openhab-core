8557666f0a45a47f7a4dd5225c73c7a6e1d90cd3
J-N-K <github@klug.nrw>
2024-07-22 15:07:28 +0200

    Add test to ensure full mapping of types in AbstractStorageBasedProvider (#4318)
    Signed-off-by: Jan N. Klug <github@klug.nrw>

--- bundles/org.openhab.core.thing/src/test/java/org/openhab/core/thing/binding/AbstractStorageBasedTypeProviderTest.java
+++ bundles/org.openhab.core.thing/src/test/java/org/openhab/core/thing/binding/AbstractStorageBasedTypeProviderTest.java
@@ -19,9 +19,14 @@ import java.net.URI;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
+import java.util.stream.Stream;
 
 import org.eclipse.jdt.annotation.NonNullByDefault;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.openhab.core.internal.types.StateDescriptionFragmentImpl;
 import org.openhab.core.thing.ThingTypeUID;
 import org.openhab.core.thing.type.AutoUpdatePolicy;
 import org.openhab.core.thing.type.ChannelDefinition;
@@ -156,6 +161,37 @@ public class AbstractStorageBasedTypeProviderTest {
                     .orElse(null));
             assertChannelGroupDefinition(actualChannelGroupDefinition, expectedChannelGroupDefinition);
         }
+    }
+
+    @MethodSource
+    @ParameterizedTest
+    public void testMapToEntityIsComplete(Class<?> originalType, Class<?> mappedType, int allowedDelta) {
+        Class<?> clazz = originalType;
+        int originalTypeFieldCount = 0;
+        while (clazz != Object.class) {
+            originalTypeFieldCount += clazz.getDeclaredFields().length;
+            clazz = clazz.getSuperclass();
+        }
+
+        int mappedEntityFieldCount = mappedType.getDeclaredFields().length;
+
+        assertThat(originalType.getName() + " not properly mapped", mappedEntityFieldCount,
+                is(originalTypeFieldCount + allowedDelta));
+    }
+
+    private static Stream<Arguments> testMapToEntityIsComplete() {
+        return Stream.of( //
+                // isBridge is an extra field for storage and not present in ThingType
+                Arguments.of(ThingType.class, AbstractStorageBasedTypeProvider.ThingTypeEntity.class, 1),
+                Arguments.of(ChannelType.class, AbstractStorageBasedTypeProvider.ChannelTypeEntity.class, 0),
+                Arguments.of(ChannelDefinition.class, AbstractStorageBasedTypeProvider.ChannelDefinitionEntity.class,
+                        0),
+                // configDescriptionURI is not available for ChannelGroupType
+                Arguments.of(ChannelGroupType.class, AbstractStorageBasedTypeProvider.ChannelGroupTypeEntity.class, -1),
+                Arguments.of(ChannelGroupDefinition.class,
+                        AbstractStorageBasedTypeProvider.ChannelGroupDefinitionEntity.class, 0),
+                Arguments.of(StateDescriptionFragmentImpl.class,
+                        AbstractStorageBasedTypeProvider.StateDescriptionFragmentEntity.class, 0));
     }
 
     private void assertChannelDefinition(ChannelDefinition actual, ChannelDefinition expected) {
