a04ec40911338ad04acba63dd22559566591d1d4
Jimmy Tanagra <jcode@tanagra.id.au>
2024-09-09 12:16:29 +1000

    Insert context keys before compiling UI scripts
    Signed-off-by: Jimmy Tanagra <jcode@tanagra.id.au>

--- bundles/org.openhab.core.automation.module.script/src/main/java/org/openhab/core/automation/module/script/internal/handler/AbstractScriptModuleHandler.java
+++ bundles/org.openhab.core.automation.module.script/src/main/java/org/openhab/core/automation/module/script/internal/handler/AbstractScriptModuleHandler.java
@@ -105,6 +105,7 @@ public abstract class AbstractScriptModuleHandler<T extends Module> extends Base
             ScriptEngine scriptEngine = engine.get();
             if (scriptEngine instanceof Compilable) {
                 logger.debug("Pre-compiling script of rule with UID '{}'", ruleUID);
+                setExecutionContext(scriptEngine, Map.of()); // JRuby script engine needs this before compiling
                 compiledScript = Optional.ofNullable(((Compilable) scriptEngine).compile(script));
             }
         }
@@ -211,7 +212,7 @@ public abstract class AbstractScriptModuleHandler<T extends Module> extends Base
         try {
             if (compiledScript.isPresent()) {
                 logger.debug("Executing pre-compiled script of rule with UID '{}'", ruleUID);
-                return compiledScript.get().eval(engine.getContext());
+                return compiledScript.get().eval();
             }
             logger.debug("Executing script of rule with UID '{}'", ruleUID);
             return engine.eval(script);
