d992fa31a43a83c5c5d23dc1f45e1db6892e0ce6
Mark Herwege <mark.herwege@telenet.be>
2024-09-12 21:46:07 +0200

    author tag
    Signed-off-by: Mark Herwege <mark.herwege@telenet.be>

--- bundles/org.openhab.core.ui/src/main/java/org/openhab/core/ui/internal/components/UIComponentSitemapProvider.java
+++ bundles/org.openhab.core.ui/src/main/java/org/openhab/core/ui/internal/components/UIComponentSitemapProvider.java
@@ -87,6 +87,7 @@ import org.slf4j.LoggerFactory;
  * @author Laurent Garnier - Added support for new element Buttongrid
  * @author Laurent Garnier - Added icon field for mappings
  * @author Mark Herwege - Make UI provided sitemaps compatible with enhanced syntax in conditions
+ * @author Mark Herwege - Add support for Button element
  */
 @NonNullByDefault
 @Component(service = SitemapProvider.class)
