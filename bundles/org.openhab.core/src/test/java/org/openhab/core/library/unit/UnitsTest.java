8db6438a3fb396b386079ddb0678631c3cfcac5b
Gaël L'hopital <gael@lhopital.org>
2024-12-03 11:37:02 +0100

    Adding some units requested in open issues
    Signed-off-by: Gaël L'hopital <gael@lhopital.org>
    
    Signed-off-by: Gaël L'hopital <gael@lhopital.org>

--- bundles/org.openhab.core/src/test/java/org/openhab/core/library/unit/UnitsTest.java
+++ bundles/org.openhab.core/src/test/java/org/openhab/core/library/unit/UnitsTest.java
@@ -432,7 +432,6 @@ public class UnitsTest {
     }
 
     @Test
-<<<<<<< Upstream, based on origin/main
     public void testColorTemperatureAliases() {
         QuantityType<?> value;
         value = QuantityType.valueOf("20 mired");
@@ -441,7 +440,8 @@ public class UnitsTest {
         assertEquals(Units.MIRED, value.getUnit());
         value = QuantityType.valueOf("20 MK⁻¹");
         assertEquals(Units.MIRED, value.getUnit());
-=======
+    }
+
     public void testGrains() {
         assertThat(ImperialUnits.GRAIN.getSymbol(), is("gr"));
         QuantityType<?> oneHundredGrains = QuantityType.valueOf("100 gr");
@@ -458,7 +458,6 @@ public class UnitsTest {
         QuantityType<?> newspaper = QuantityType.valueOf("72 g/m²");
         QuantityType<?> converted = newspaper.toUnit(Units.KILOGRAM_PER_SQUARE_METRE);
         assertEquals(converted.doubleValue(), 0.072);
->>>>>>> df0bd5e Adding some units requested in open issues
     }
 
     private static class QuantityEquals extends IsEqual<Quantity<?>> {
