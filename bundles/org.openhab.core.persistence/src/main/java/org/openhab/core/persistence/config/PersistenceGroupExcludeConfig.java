a35041ac7b3885a26c4df353f1449c86538cda2a
Mark Herwege <mherwege@users.noreply.github.com>
2024-12-08 12:50:37 +0100

    [persistence] Exclude items and groups (#4468)
    Signed-off-by: Mark Herwege <mark.herwege@telenet.be>

---
+++ bundles/org.openhab.core.persistence/src/main/java/org/openhab/core/persistence/config/PersistenceGroupExcludeConfig.java
@@ -0,0 +1,39 @@
+/**
+ * Copyright (c) 2010-2024 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.core.persistence.config;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+
+/**
+ * This class represents the configuration that is used for excluding group items.
+ *
+ * @author Mark Herwege - Initial contribution
+ */
+@NonNullByDefault
+public class PersistenceGroupExcludeConfig extends PersistenceConfig {
+
+    private final String group;
+
+    public PersistenceGroupExcludeConfig(final String group) {
+        this.group = group;
+    }
+
+    public String getGroup() {
+        return group;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s [group=%s]", getClass().getSimpleName(), group);
+    }
+}
