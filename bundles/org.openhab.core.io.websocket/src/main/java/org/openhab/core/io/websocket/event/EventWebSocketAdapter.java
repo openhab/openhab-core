7d77058431e3e4a8133c3cac57d920631314eb9f
Florian Hotze <dev@florianhotze.com>
2025-01-04 21:48:05 +0100

    [websocket] Change EventWebSocketAdapter adapter id to `events`
    This is aligns the event WS name with the SSE events endpoint.
    This is a breaking change for clients that have explicitly used `/ws/event-subscriber` instead of the default `/ws`.
    
    Signed-off-by: Florian Hotze <dev@florianhotze.com>

--- bundles/org.openhab.core.io.websocket/src/main/java/org/openhab/core/io/websocket/event/EventWebSocketAdapter.java
+++ bundles/org.openhab.core.io.websocket/src/main/java/org/openhab/core/io/websocket/event/EventWebSocketAdapter.java
@@ -37,7 +37,7 @@ import com.google.gson.Gson;
 @NonNullByDefault
 @Component(immediate = true, service = { EventSubscriber.class, WebSocketAdapter.class })
 public class EventWebSocketAdapter implements EventSubscriber, WebSocketAdapter {
-    public static final String ADAPTER_ID = "event-subscriber";
+    public static final String ADAPTER_ID = "events";
     private final Gson gson = new Gson();
     private final EventPublisher eventPublisher;
 
