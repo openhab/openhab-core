195cc3d54d63906b66dc10573ef6429aa88763ad
Mark Herwege <mherwege@users.noreply.github.com>
2025-03-18 20:48:19 +0000

    revert iterating over history on restore (#4654)
    Signed-off-by: Mark Herwege <mark.herwege@telenet.be>

--- bundles/org.openhab.core.persistence/src/main/java/org/openhab/core/persistence/QueryablePersistenceService.java
+++ bundles/org.openhab.core.persistence/src/main/java/org/openhab/core/persistence/QueryablePersistenceService.java
@@ -101,9 +101,10 @@ public interface QueryablePersistenceService extends PersistenceService {
     /**
      * Returns a {@link PersistedItem} representing the persisted state, last update and change timestamps and previous
      * persisted state. This can be used to restore the full state of an item.
-     * The default implementation queries the service and iterates backward to find the last change and previous
-     * persisted state. Persistence services can override this default implementation with a more specific or efficient
-     * algorithm.
+     * The default implementation only queries the service for the last persisted state and does not attempt to look
+     * further back to find the last change timestamp and previous persisted state. This avoids potential performance
+     * issues if this method is not overriden in the specific persistence service. Persistence services should override
+     * this default implementation with a more complete and efficient algorithm.
      *
      * @param itemName name of item
      * @param alias alias of item
@@ -112,51 +113,21 @@ public interface QueryablePersistenceService extends PersistenceService {
      */
     default @Nullable PersistedItem persistedItem(String itemName, @Nullable String alias) {
         State currentState = UnDefType.NULL;
-        State previousState = null;
         ZonedDateTime lastUpdate = null;
-        ZonedDateTime lastChange = null;
 
-        int pageNumber = 0;
         FilterCriteria filter = new FilterCriteria().setItemName(itemName).setEndDate(ZonedDateTime.now())
-                .setOrdering(Ordering.DESCENDING).setPageSize(1000).setPageNumber(pageNumber);
-        Iterable<HistoricItem> items = query(filter, alias);
-        while (items != null) {
-            Iterator<HistoricItem> it = items.iterator();
-            int itemCount = 0;
-            if (UnDefType.NULL.equals(currentState) && it.hasNext()) {
-                HistoricItem historicItem = it.next();
-                itemCount++;
-                currentState = historicItem.getState();
-                lastUpdate = historicItem.getTimestamp();
-                lastChange = lastUpdate;
-            }
-            while (it.hasNext()) {
-                HistoricItem historicItem = it.next();
-                itemCount++;
-                if (!historicItem.getState().equals(currentState)) {
-                    previousState = historicItem.getState();
-                    items = null;
-                    break;
-                }
-                lastChange = historicItem.getTimestamp();
-            }
-            if (itemCount == filter.getPageSize()) {
-                filter.setPageNumber(++pageNumber);
-                items = query(filter);
-            } else {
-                items = null;
-            }
-        }
-
-        if (UnDefType.NULL.equals(currentState) || lastUpdate == null) {
+                .setOrdering(Ordering.DESCENDING).setPageSize(1).setPageNumber(0);
+        Iterator<HistoricItem> it = query(filter, alias).iterator();
+        if (it.hasNext()) {
+            HistoricItem historicItem = it.next();
+            currentState = historicItem.getState();
+            lastUpdate = historicItem.getTimestamp();
+        } else {
             return null;
         }
 
         final State state = currentState;
         final ZonedDateTime lastStateUpdate = lastUpdate;
-        final State lastState = previousState;
-        // if we don't find a previous state in persistence, we also don't know when it last changed
-        final ZonedDateTime lastStateChange = previousState != null ? lastChange : null;
 
         return new PersistedItem() {
 
@@ -177,12 +148,12 @@ public interface QueryablePersistenceService extends PersistenceService {
 
             @Override
             public @Nullable ZonedDateTime getLastStateChange() {
-                return lastStateChange;
+                return null;
             }
 
             @Override
             public @Nullable State getLastState() {
-                return lastState;
+                return null;
             }
         };
     }
