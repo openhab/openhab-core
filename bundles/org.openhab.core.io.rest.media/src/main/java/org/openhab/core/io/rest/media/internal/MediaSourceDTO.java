8b930fd93ec4d56f0e3c5214f0df7405955718e4
Laurent ARNAL <laurent@clae.net>
2025-03-31 11:59:10 +0200

    add rest media endpoint
    Signed-off-by: Laurent ARNAL <laurent@clae.net>

---
+++ bundles/org.openhab.core.io.rest.media/src/main/java/org/openhab/core/io/rest/media/internal/MediaSourceDTO.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2010-2025 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.core.io.rest.media.internal;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.eclipse.jdt.annotation.Nullable;
+
+/**
+ * A DTO that is used on the REST API to provide infos about {@link AudioSource} to UIs.
+ *
+ * @author Laurent Arnal - Initial contribution
+ */
+@NonNullByDefault
+public class MediaSourceDTO {
+    public String id;
+    public @Nullable String label;
+
+    public MediaSourceDTO(String id, @Nullable String label) {
+        this.id = id;
+        this.label = label;
+    }
+}
