961b68c6289962bd682df89d071f0d1b16b40941
Laurent Garnier <lg.hc@free.fr>
2025-04-04 13:24:57 +0200

    YAML configuration: add support for things
    Related to #3666
    
    Signed-off-by: Laurent Garnier <lg.hc@free.fr>

--- bundles/org.openhab.core.model.yaml/src/test/java/org/openhab/core/model/yaml/test/SecondTypeDTO.java
+++ bundles/org.openhab.core.model.yaml/src/test/java/org/openhab/core/model/yaml/test/SecondTypeDTO.java
@@ -12,9 +12,11 @@
  */
 package org.openhab.core.model.yaml.test;
 
+import java.util.List;
 import java.util.Objects;
 
 import org.eclipse.jdt.annotation.NonNull;
+import org.eclipse.jdt.annotation.Nullable;
 import org.openhab.core.model.yaml.YamlElement;
 import org.openhab.core.model.yaml.YamlElementName;
 
@@ -59,7 +61,7 @@ public class SecondTypeDTO implements YamlElement, Cloneable {
     }
 
     @Override
-    public boolean isValid() {
+    public boolean isValid(@Nullable List<@NonNull String> errors, @Nullable List<@NonNull String> warnings) {
         return id != null && !id.isBlank();
     }
 
