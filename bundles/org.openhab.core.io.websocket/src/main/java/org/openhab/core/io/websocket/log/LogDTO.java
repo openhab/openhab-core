f16f7cda3158c5733ae00043c63a4857dd7db2e4
Chris Jackson <chris@cd-jackson.com>
2025-04-21 08:15:39 +1200

    Add stack trace to logger DTO (#4732)
    Signed-off-by: Chris Jackson <chris@cd-jackson.com>

--- bundles/org.openhab.core.io.websocket/src/main/java/org/openhab/core/io/websocket/log/LogDTO.java
+++ bundles/org.openhab.core.io.websocket/src/main/java/org/openhab/core/io/websocket/log/LogDTO.java
@@ -30,15 +30,17 @@ public class LogDTO implements Comparable<LogDTO> {
     public Date timestamp;
     public long unixtime;
     public String message;
+    public String stackTrace;
     public long sequence;
 
-    public LogDTO(long sequence, String loggerName, LogLevel level, long unixtime, String message) {
+    public LogDTO(long sequence, String loggerName, LogLevel level, long unixtime, String message, String stackTrace) {
         this.sequence = sequence;
         this.loggerName = loggerName;
         this.level = level;
         this.timestamp = new Date(unixtime);
         this.unixtime = unixtime;
         this.message = message;
+        this.stackTrace = stackTrace;
     }
 
     @Override
