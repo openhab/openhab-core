2515b05ee3b61920546673a8a7b271ad79bdaf8e
Jimmy Tanagra <jcode@tanagra.id.au>
2025-04-27 22:43:14 +1000

    Upgrade Tool: Split upgrade commands into individual classes
    Signed-off-by: Jimmy Tanagra <jcode@tanagra.id.au>

--- tools/upgradetool/src/main/java/org/openhab/core/tools/internal/ItemUnitToMetadataUpgrader.java
+++ tools/upgradetool/src/main/java/org/openhab/core/tools/internal/ItemUnitToMetadataUpgrader.java
@@ -18,77 +18,57 @@ import static org.openhab.core.thing.DefaultSystemChannelTypeProvider.SYSTEM_CHA
 
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.time.ZonedDateTime;
 import java.util.List;
 import java.util.Objects;
 
 import javax.measure.Unit;
 
 import org.eclipse.jdt.annotation.NonNullByDefault;
-import org.openhab.core.config.core.Configuration;
 import org.openhab.core.items.ManagedItemProvider;
 import org.openhab.core.items.Metadata;
 import org.openhab.core.items.MetadataKey;
 import org.openhab.core.library.items.NumberItem;
 import org.openhab.core.storage.json.internal.JsonStorage;
 import org.openhab.core.thing.dto.ThingDTO;
-import org.openhab.core.thing.internal.link.ItemChannelLinkConfigDescriptionProvider;
 import org.openhab.core.thing.link.ItemChannelLink;
+import org.openhab.core.tools.Upgrader;
 import org.openhab.core.types.util.UnitUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * The {@link Upgrader} contains the implementation of the upgrade methods
+ * The {@link ItemUnitToMetadataUpgrader} copies the unit from the item to the metadata.
  *
  * @author Jan N. Klug - Initial contribution
- * @author Florian Hotze - Add script profile upgrade
+ * @author Jimmy Tanagra - Refactored into a separate class
  */
 @NonNullByDefault
-public class Upgrader {
-    public static final String ITEM_COPY_UNIT_TO_METADATA = "itemCopyUnitToMetadata";
-    public static final String LINK_UPGRADE_JS_PROFILE = "linkUpgradeJsProfile";
-    public static final String LINK_UPGRADE_SCRIPT_PROFILE = "linkUpgradeScriptProfile";
+public class ItemUnitToMetadataUpgrader implements Upgrader {
+    private final Logger logger = LoggerFactory.getLogger(ItemUnitToMetadataUpgrader.class);
 
-    private final Logger logger = LoggerFactory.getLogger(Upgrader.class);
-    private final String baseDir;
-    private final boolean force;
-    private final JsonStorage<UpgradeRecord> upgradeRecords;
-
-    public Upgrader(String baseDir, boolean force) {
-        this.baseDir = baseDir;
-        this.force = force;
-
-        Path upgradeJsonDatabasePath = Path.of(baseDir, "jsondb", "org.openhab.core.tools.UpgradeTool");
-
-        upgradeRecords = new JsonStorage<>(upgradeJsonDatabasePath.toFile(), null, 5, 0, 0, List.of());
+    @Override
+    public String getName() {
+        return "itemCopyUnitToMetadata"; // keep the old name for backwards compatibility
     }
 
-    private boolean checkUpgradeRecord(String key) {
-        UpgradeRecord upgradeRecord = upgradeRecords.get(key);
-        if (upgradeRecord != null && !force) {
-            logger.info("Already executed '{}' on {}. Use '--force'  to execute it again.", key,
-                    upgradeRecord.executionDate);
-            return false;
-        }
-        return true;
+    @Override
+    public String getDescription() {
+        return "Copy item unit from state description to metadata";
     }
 
-    public void itemCopyUnitToMetadata() {
+    @Override
+    public boolean execute(String userdataDir, String confDir) {
         boolean noLink;
 
-        if (!checkUpgradeRecord(ITEM_COPY_UNIT_TO_METADATA)) {
-            return;
-        }
-        Path itemJsonDatabasePath = Path.of(baseDir, "jsondb", "org.openhab.core.items.Item.json");
-        Path metadataJsonDatabasePath = Path.of(baseDir, "jsondb", "org.openhab.core.items.Metadata.json");
-        Path linkJsonDatabasePath = Path.of(baseDir, "jsondb", "org.openhab.core.thing.link.ItemChannelLink.json");
-        Path thingJsonDatabasePath = Path.of(baseDir, "jsondb", "org.openhab.core.thing.Thing.json");
+        Path itemJsonDatabasePath = Path.of(userdataDir, "jsondb", "org.openhab.core.items.Item.json");
+        Path metadataJsonDatabasePath = Path.of(userdataDir, "jsondb", "org.openhab.core.items.Metadata.json");
+        Path linkJsonDatabasePath = Path.of(userdataDir, "jsondb", "org.openhab.core.thing.link.ItemChannelLink.json");
+        Path thingJsonDatabasePath = Path.of(userdataDir, "jsondb", "org.openhab.core.thing.Thing.json");
         logger.info("Copying item unit from state description to metadata in database '{}'", itemJsonDatabasePath);
 
         if (!Files.isReadable(itemJsonDatabasePath)) {
             logger.error("Cannot access item database '{}', check path and access rights.", itemJsonDatabasePath);
-            return;
+            return false;
         }
 
         if (!Files.isReadable(linkJsonDatabasePath) || !Files.isReadable(thingJsonDatabasePath)) {
@@ -102,7 +82,7 @@ public class Upgrader {
         if (!Files.isWritable(metadataJsonDatabasePath) && Files.exists(metadataJsonDatabasePath)) {
             logger.error("Cannot access metadata database '{}', check path and access rights.",
                     metadataJsonDatabasePath);
-            return;
+            return false;
         }
 
         JsonStorage<ManagedItemProvider.PersistedItem> itemStorage = new JsonStorage<>(itemJsonDatabasePath.toFile(),
@@ -121,7 +101,7 @@ public class Upgrader {
                     logger.debug("{}: Already contains a 'unit' metadata, skipping it", itemName);
                 } else {
                     String unit = null;
-                    if (!noLink) {
+                    if (linkStorage != null && thingStorage != null) {
                         List<ItemChannelLink> links = linkStorage.getValues().stream().map(Objects::requireNonNull)
                                 .filter(link -> itemName.equals(link.getItemName())).toList();
                         // check if we can find the channel for these links
@@ -192,98 +172,7 @@ public class Upgrader {
         });
 
         metadataStorage.flush();
-        upgradeRecords.put(ITEM_COPY_UNIT_TO_METADATA, new UpgradeRecord(ZonedDateTime.now()));
-        upgradeRecords.flush();
-    }
 
-    public void linkUpgradeJsProfile() {
-        if (!checkUpgradeRecord(LINK_UPGRADE_JS_PROFILE)) {
-            return;
-        }
-
-        Path linkJsonDatabasePath = Path.of(baseDir, "jsondb", "org.openhab.core.thing.link.ItemChannelLink.json");
-        logger.info("Upgrading JS profile configuration in database '{}'", linkJsonDatabasePath);
-
-        if (!Files.isWritable(linkJsonDatabasePath)) {
-            logger.error("Cannot access link database '{}', check path and access rights.", linkJsonDatabasePath);
-            return;
-        }
-        JsonStorage<ItemChannelLink> linkStorage = new JsonStorage<>(linkJsonDatabasePath.toFile(), null, 5, 0, 0,
-                List.of());
-
-        List.copyOf(linkStorage.getKeys()).forEach(linkUid -> {
-            ItemChannelLink link = Objects.requireNonNull(linkStorage.get(linkUid));
-            Configuration configuration = link.getConfiguration();
-            String profileName = (String) configuration.get(ItemChannelLinkConfigDescriptionProvider.PARAM_PROFILE);
-            if ("transform:JS".equals(profileName)) {
-                String function = (String) configuration.get("function");
-                if (function != null) {
-                    configuration.put("toItemScript", function);
-                    configuration.put("toHandlerScript", "|input");
-                    configuration.remove("function");
-                    configuration.remove("sourceFormat");
-
-                    linkStorage.put(linkUid, link);
-                    logger.info("{}: rewrote JS profile link to new format", linkUid);
-                } else {
-                    logger.info("{}: link already has correct configuration", linkUid);
-                }
-            }
-        });
-
-        linkStorage.flush();
-        upgradeRecords.put(LINK_UPGRADE_JS_PROFILE, new UpgradeRecord(ZonedDateTime.now()));
-        upgradeRecords.flush();
-    }
-
-    /**
-     * Upgrades the ItemChannelLink database for the separation of {@code toHandlerScript} into
-     * {@code commandFromItemScript} and {@code stateFromItemScript}.
-     * See <a href="https://github.com/openhab/openhab-core/pull/4058">openhab/openhab-core#4058</a>.
-     */
-    public void linkUpgradeScriptProfile() {
-        if (!checkUpgradeRecord(LINK_UPGRADE_SCRIPT_PROFILE)) {
-            return;
-        }
-
-        Path linkJsonDatabasePath = Path.of(baseDir, "jsondb", "org.openhab.core.thing.link.ItemChannelLink.json");
-        logger.info("Upgrading script profile configuration in database '{}'", linkJsonDatabasePath);
-
-        if (!Files.isWritable(linkJsonDatabasePath)) {
-            logger.error("Cannot access link database '{}', check path and access rights.", linkJsonDatabasePath);
-            return;
-        }
-        JsonStorage<ItemChannelLink> linkStorage = new JsonStorage<>(linkJsonDatabasePath.toFile(), null, 5, 0, 0,
-                List.of());
-
-        List.copyOf(linkStorage.getKeys()).forEach(linkUid -> {
-            ItemChannelLink link = Objects.requireNonNull(linkStorage.get(linkUid));
-            Configuration configuration = link.getConfiguration();
-            String profileName = (String) configuration.get(ItemChannelLinkConfigDescriptionProvider.PARAM_PROFILE);
-            if (profileName != null && profileName.startsWith("transform:")) {
-                String toHandlerScript = (String) configuration.get("toHandlerScript");
-                if (toHandlerScript != null) {
-                    configuration.put("commandFromItemScript", toHandlerScript);
-                    configuration.remove("toHandlerScript");
-
-                    linkStorage.put(linkUid, link);
-                    logger.info("{}: rewrote script profile link to new format", linkUid);
-                } else {
-                    logger.info("{}: link already has correct configuration", linkUid);
-                }
-            }
-        });
-
-        linkStorage.flush();
-        upgradeRecords.put(LINK_UPGRADE_SCRIPT_PROFILE, new UpgradeRecord(ZonedDateTime.now()));
-        upgradeRecords.flush();
-    }
-
-    private static class UpgradeRecord {
-        public final String executionDate;
-
-        public UpgradeRecord(ZonedDateTime executionDate) {
-            this.executionDate = executionDate.toString();
-        }
+        return true;
     }
 }
