de871d9507e103adc55b219423d2f7193dea597a
Laurent Garnier <lg.hc@free.fr>
2025-05-08 22:40:06 +0200

    YAML config: add "description" field to channel for things
    Signed-off-by: Laurent Garnier <lg.hc@free.fr>

--- bundles/org.openhab.core.model.yaml/src/main/java/org/openhab/core/model/yaml/internal/things/YamlThingProvider.java
+++ bundles/org.openhab.core.model.yaml/src/main/java/org/openhab/core/model/yaml/internal/things/YamlThingProvider.java
@@ -364,6 +364,7 @@ public class YamlThingProvider extends AbstractProvider<Thing>
             ChannelKind kind = channelDto.getKind();
             String itemType = channelDto.getItemType();
             String label = channelDto.label;
+            String description = channelDto.description;
             AutoUpdatePolicy autoUpdatePolicy = null;
             Configuration configuration = new Configuration(channelDto.config);
             if (channelDto.type != null) {
@@ -376,6 +377,9 @@ public class YamlThingProvider extends AbstractProvider<Thing>
                     if (label == null) {
                         label = channelType.getLabel();
                     }
+                    if (description == null) {
+                        description = channelType.getDescription();
+                    }
                     autoUpdatePolicy = channelType.getAutoUpdatePolicy();
                     URI descUriO = channelType.getConfigDescriptionURI();
                     if (descUriO != null) {
@@ -387,9 +391,15 @@ public class YamlThingProvider extends AbstractProvider<Thing>
                 }
             }
 
-            Channel channel = ChannelBuilder.create(new ChannelUID(thingUID, channelId), itemType).withKind(kind)
-                    .withConfiguration(configuration).withType(channelTypeUID).withLabel(label)
-                    .withAutoUpdatePolicy(autoUpdatePolicy).build();
+            ChannelBuilder builder = ChannelBuilder.create(new ChannelUID(thingUID, channelId), itemType).withKind(kind)
+                    .withConfiguration(configuration).withType(channelTypeUID).withAutoUpdatePolicy(autoUpdatePolicy);
+            if (label != null) {
+                builder.withLabel(label);
+            }
+            if (description != null) {
+                builder.withDescription(description);
+            }
+            Channel channel = builder.build();
             channels.add(channel);
             addedChannelIds.add(channelId);
         });
