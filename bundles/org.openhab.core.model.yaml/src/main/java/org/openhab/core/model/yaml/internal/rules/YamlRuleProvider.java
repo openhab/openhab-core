520b53383880a815ad461176d3620deaeb01dd6b
Ravi Nadahar <nadahar@rediffmail.com>
2025-05-18 05:42:56 +0200

    Create YamlConfigDescriptionParameterDTO
    YamlConfigDescriptionParameterDTO is separated from the other rule-related classes because it can also be used by other element types.
    
    Signed-off-by: Ravi Nadahar <nadahar@rediffmail.com>

--- bundles/org.openhab.core.model.yaml/src/main/java/org/openhab/core/model/yaml/internal/rules/YamlRuleProvider.java
+++ bundles/org.openhab.core.model.yaml/src/main/java/org/openhab/core/model/yaml/internal/rules/YamlRuleProvider.java
@@ -28,10 +28,10 @@ import org.openhab.core.automation.Rule;
 import org.openhab.core.automation.RuleProvider;
 import org.openhab.core.automation.Trigger;
 import org.openhab.core.automation.util.RuleBuilder;
-import org.openhab.core.config.core.ConfigDescriptionParameter;
 import org.openhab.core.config.core.Configuration;
 import org.openhab.core.io.dto.SerializationException;
 import org.openhab.core.model.yaml.YamlModelListener;
+import org.openhab.core.model.yaml.internal.config.YamlConfigDescriptionParameterDTO;
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Deactivate;
@@ -158,9 +158,10 @@ public class YamlRuleProvider extends AbstractYamlRuleProvider<Rule>
         if (configuration != null) {
             ruleBuilder.withConfiguration(new Configuration(configuration));
         }
-        List<ConfigDescriptionParameter> configurationDescriptions = ruleDto.configDescriptions;
-        if (configurationDescriptions != null) {
-            ruleBuilder.withConfigurationDescriptions(configurationDescriptions);
+        List<YamlConfigDescriptionParameterDTO> configDescriptionDtos = ruleDto.configDescriptions;
+        if (configDescriptionDtos != null) {
+            ruleBuilder.withConfigurationDescriptions(
+                    YamlConfigDescriptionParameterDTO.mapConfigDescriptions(configDescriptionDtos));
         }
         List<YamlModuleDTO> triggerDTOs = ruleDto.triggers;
         List<YamlConditionDTO> conditionDTOs = ruleDto.conditions;
