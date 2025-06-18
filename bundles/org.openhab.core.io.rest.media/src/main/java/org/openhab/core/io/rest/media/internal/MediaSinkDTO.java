ab81a29f2aace96280a6ffa2b55a18d8a3412acc
Laurent ARNAL <laurent@clae.net>
2025-06-18 19:31:32 +0200

    start adding MediaSink support to enable selecting output rendering device directly from oh-player-controls
    Signed-off-by: Laurent ARNAL <laurent@clae.net>

---
+++ bundles/org.openhab.core.io.rest.media/src/main/java/org/openhab/core/io/rest/media/internal/MediaSinkDTO.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) 2010-2025 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.core.io.rest.media.internal;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+
+/**
+ * A DTO that is used on the REST API to provide infos about {@link AudioSource} to UIs.
+ *
+ * @author Laurent Arnal - Initial contribution
+ */
+@NonNullByDefault
+public class MediaSinkDTO {
+    private String id;
+    private String name;
+    private String type;
+
+    public MediaSinkDTO(String id, String name, String type) {
+        this.id = id;
+        this.name = name;
+        this.type = type;
+    }
+
+    public String getId() {
+        return this.id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getType() {
+        return this.type;
+    }
+}
