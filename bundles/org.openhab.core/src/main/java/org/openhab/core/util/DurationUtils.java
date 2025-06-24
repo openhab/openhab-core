635176362a8ebf786214779461056f23e15a1e98
Jimmy Tanagra <jcode@tanagra.id.au>
2025-06-25 00:36:01 +1000

    Support `ms` in Duration parser
    Signed-off-by: Jimmy Tanagra <jcode@tanagra.id.au>

--- bundles/org.openhab.core/src/main/java/org/openhab/core/util/DurationUtils.java
+++ bundles/org.openhab.core/src/main/java/org/openhab/core/util/DurationUtils.java
@@ -13,6 +13,7 @@
 package org.openhab.core.util;
 
 import java.time.Duration;
+import java.time.temporal.ChronoUnit;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -26,13 +27,20 @@ import org.eclipse.jdt.annotation.NonNullByDefault;
 @NonNullByDefault
 public class DurationUtils {
 
-    private static final Pattern DURATION_PATTERN = Pattern
-            .compile("(?:([0-9]+)D)?\\s*(?:([0-9]+)H)?\\s*(?:([0-9]+)M)?\\s*(?:([0-9]+)S)?", Pattern.CASE_INSENSITIVE);
+    private static final Pattern DURATION_PATTERN = Pattern.compile(
+            "(?:([0-9]+)D)?\\s*(?:([0-9]+)H)?\\s*(?:([0-9]+)M)?\\s*(?:([0-9]+)S)?\\s*(?:([0-9]+)MS)?",
+            Pattern.CASE_INSENSITIVE);
+    private static final ChronoUnit[] DURATION_UNITS = { ChronoUnit.DAYS, ChronoUnit.HOURS, ChronoUnit.MINUTES,
+            ChronoUnit.SECONDS, ChronoUnit.MILLIS };
 
     /**
      * Parses a duration string in ISO-8601 format or a custom format like
-     * "1d 1h 15m 30s" where 'd' stands for days, 'h' for hours,
-     * 'm' for minutes, and 's' for seconds.
+     * "1d 1h 15m 30s 500ms" where
+     * 'd' stands for days,
+     * 'h' for hours,
+     * 'm' for minutes,
+     * 's' for seconds, and
+     * 'ms' for milliseconds.
      *
      * @param durationString the string representation of the duration
      * @return a Duration object representing the parsed duration
@@ -46,23 +54,19 @@ public class DurationUtils {
         }
 
         Matcher m = DURATION_PATTERN.matcher(durationString);
-        if (!m.matches() || (m.group(1) == null && m.group(2) == null && m.group(3) == null && m.group(4) == null)) {
-            throw new IllegalArgumentException(
-                    "Invalid duration: " + durationString + ". Expected something like: '1d 1h 15m 30s'");
+        if (!m.matches() || (m.group(1) == null && m.group(2) == null && m.group(3) == null && m.group(4) == null
+                && m.group(5) == null)) {
+            throw new IllegalArgumentException("Invalid duration: " + durationString
+                    + ". Expected an ISO8601 duration or something like: '1d 1h 15m 30s 300ms'");
         }
 
         Duration duration = Duration.ZERO;
-        if (m.group(1) != null) {
-            duration = duration.plus(Duration.ofDays(Long.parseLong(m.group(1))));
-        }
-        if (m.group(2) != null) {
-            duration = duration.plus(Duration.ofHours(Long.parseLong(m.group(2))));
-        }
-        if (m.group(3) != null) {
-            duration = duration.plus(Duration.ofMinutes(Long.parseLong(m.group(3))));
-        }
-        if (m.group(4) != null) {
-            duration = duration.plus(Duration.ofSeconds(Long.parseLong(m.group(4))));
+        for (int i = 0; i < DURATION_UNITS.length; i++) {
+            String value = m.group(i + 1);
+            if (value != null) {
+                long amount = Long.parseLong(value);
+                duration = duration.plus(amount, DURATION_UNITS[i]);
+            }
         }
         return duration;
     }
