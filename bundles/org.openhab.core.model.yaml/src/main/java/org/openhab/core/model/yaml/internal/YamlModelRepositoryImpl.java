2c3174366c3187491d8f167289d7f2e37d59bc66
Jimmy Tanagra <jcode@tanagra.id.au>
2025-06-29 20:52:00 +1000

    adapt to changes in main branch
    Signed-off-by: Jimmy Tanagra <jcode@tanagra.id.au>

--- bundles/org.openhab.core.model.yaml/src/main/java/org/openhab/core/model/yaml/internal/YamlModelRepositoryImpl.java
+++ bundles/org.openhab.core.model.yaml/src/main/java/org/openhab/core/model/yaml/internal/YamlModelRepositoryImpl.java
@@ -175,8 +175,8 @@ public class YamlModelRepositoryImpl implements WatchService.WatchEventListener,
                 logger.warn("Could not list YAML files in '{}', models might be missing: {}", watchPath,
                         e.getMessage());
             }
-        initializing = false;
         });
+        initializing = false;
     }
 
     @Deactivate
@@ -201,9 +201,9 @@ public class YamlModelRepositoryImpl implements WatchService.WatchEventListener,
             return;
         }
 
-        if ((!modelName.endsWith(".yaml") && !modelName.endsWith(".yml")) || modelName.endsWith(".inc.yaml") ||
-                modelName.endsWith(".inc.yml")) {
-                                logger.trace("Ignored {}", fullPath);
+        if ((!modelName.endsWith(".yaml") && !modelName.endsWith(".yml")) || modelName.endsWith(".inc.yaml")
+                || modelName.endsWith(".inc.yml")) {
+            logger.trace("Ignored {}", fullPath);
             return;
         }
 
@@ -359,7 +359,7 @@ public class YamlModelRepositoryImpl implements WatchService.WatchEventListener,
         });
 
         dependingModels.forEach(modelName -> {
-            Path modelPath = watchPath.resolve(modelName);
+            Path modelPath = mainWatchPath.resolve(modelName);
             try {
                 // reprocess the model that depends on this include file
                 processWatchEvent(WatchService.Kind.MODIFY, modelPath);
