openhab/openhab-core

commit b48e1632aa668509a2e32e652f9b78df405d78d5

WARN log when an object with same ID is added to a registry by a seco… (#4880)
* WARN log when an object with same ID is added to a registry by a second provider

Log was existing but at DEBUG level, meaning the general user was not informed that he defined the same object (thing, item, ...) at different places (providers) like for example an item in Main UI and an item in config file with same name.

Fix #4810

Signed-off-by: Laurent Garnier <lg.hc@free.fr>

bundles/org.openhab.core/src/main/java/org/openhab/core/common/registry/AbstractRegistry.java
@@ -192,7 +192,7 @@ public abstract class AbstractRegistry<@NonNull E extends Identifiable<K>, @NonN
         E existingElement = identifierToElement.get(uid);
         if (existingElement != null) {
             Provider<E> existingElementProvider = elementToProvider.get(existingElement);
-            logger.debug(
+            logger.warn(
                     "Cannot add \"{}\" with key \"{}\". It exists already from provider \"{}\"! Failed to add a second with the same UID from provider \"{}\"!",
                     element.getClass().getSimpleName(), uid,
                     existingElementProvider != null ? existingElementProvider.getClass().getSimpleName() : null,
@@ -249,7 +249,7 @@ public abstract class AbstractRegistry<@NonNull E extends Identifiable<K>, @NonN
             }
             Provider<E> elementProvider = elementToProvider.get(existingElement);
             if (elementProvider != null && !elementProvider.equals(provider)) {
-                logger.error(
+                logger.warn(
                         "Provider '{}' is not allowed to remove element '{}' with key '{}' from the registry because it was added by provider '{}'.",
                         provider.getClass().getSimpleName(), element.getClass().getSimpleName(), uid,
                         elementProvider.getClass().getSimpleName());

commit c9a9266bd3d0e300fbe2e7846960878775be9c21

Registry: Avoid warn log when adding ActionType/Metadata several times
Fix openhab/openhab-addons#18945
Regression from #4880

For Metadata, it was reported on the community forum, it occured when an item is linked to several channels.

Signed-off-by: Laurent Garnier <lg.hc@free.fr>

bundles/org.openhab.core/src/main/java/org/openhab/core/common/registry/AbstractRegistry.java
@@ -192,11 +192,20 @@ public abstract class AbstractRegistry<@NonNull E extends Identifiable<K>, @NonN
         E existingElement = identifierToElement.get(uid);
         if (existingElement != null) {
             Provider<E> existingElementProvider = elementToProvider.get(existingElement);
-            logger.warn(
-                    "Cannot add \"{}\" with key \"{}\". It exists already from provider \"{}\"! Failed to add a second with the same UID from provider \"{}\"!",
-                    element.getClass().getSimpleName(), uid,
-                    existingElementProvider != null ? existingElementProvider.getClass().getSimpleName() : null,
-                    provider.getClass().getSimpleName());
+            String elementClassName = element.getClass().getSimpleName();
+            if ("ActionType".equals(elementClassName) || "Metadata".equals(elementClassName)) {
+                logger.debug(
+                        "Cannot add \"{}\" with key \"{}\". It exists already from provider \"{}\"! Failed to add a second with the same UID from provider \"{}\"!",
+                        elementClassName, uid,
+                        existingElementProvider != null ? existingElementProvider.getClass().getSimpleName() : null,
+                        provider.getClass().getSimpleName());
+            } else {
+                logger.warn(
+                        "Cannot add \"{}\" with key \"{}\". It exists already from provider \"{}\"! Failed to add a second with the same UID from provider \"{}\"!",
+                        elementClassName, uid,
+                        existingElementProvider != null ? existingElementProvider.getClass().getSimpleName() : null,
+                        provider.getClass().getSimpleName());
+            }
             return false;
         }
         try {