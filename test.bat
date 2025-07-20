d9755bbe7fab6a7095297f81410963144dc805ae
Andrew Fiddian-Green <software@whitebear.ch>
2025-07-20 17:08:05 +0100

    remove bat file
    Signed-off-by: Andrew Fiddian-Green <software@whitebear.ch>

--- test.bat
+++
@@ -1,5 +0,0 @@
-call mvn clean install -T 18 -DskipTests -DskipChecks
-echo on
-call mvn clean install -pl :org.openhab.core.model.item.tests
-echo on
-call mvn clean install -pl :org.openhab.core.model.thing.tests
