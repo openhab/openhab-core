c94bfa8dff2574d086075bbc294c50719bb22e1a
Andrew Fiddian-Green <software@whitebear.ch>
2025-07-21 14:04:34 +0100

    work in progress
    Signed-off-by: Andrew Fiddian-Green <software@whitebear.ch>

---
+++ itests/org.openhab.core.thing.tests/src/main/java/org/openhab/core/thing/link/UseLinkedChannelDefaultTagsTest.java
@@ -0,0 +1,153 @@
+/*
+ * Copyright (c) 2010-2025 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.core.thing.link;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.ByteArrayInputStream;
+import java.util.Optional;
+import java.util.Set;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openhab.core.config.core.Configuration;
+import org.openhab.core.items.Item;
+import org.openhab.core.items.ItemRegistry;
+import org.openhab.core.model.core.ModelRepository;
+import org.openhab.core.test.java.JavaOSGiTest;
+import org.openhab.core.thing.Channel;
+import org.openhab.core.thing.ChannelUID;
+import org.openhab.core.thing.ThingRegistry;
+import org.openhab.core.thing.ThingTypeUID;
+import org.openhab.core.thing.ThingUID;
+import org.openhab.core.thing.binding.builder.ChannelBuilder;
+import org.openhab.core.thing.binding.builder.ThingBuilder;
+
+/**
+ * Tests for {@link ItemChannelLinkRegistry} assignment of default tags.
+ *
+ * @author Andrew Fiddian-Green - Initial contribution
+ */
+@NonNullByDefault
+public class UseLinkedChannelDefaultTagsTest extends JavaOSGiTest {
+
+    private static final String TEST_ITEM_NAME = "testItem";
+    private static final String TEST_MODEL_NAME = "testModel.items";
+    private static final String ITEMS_MODEL_TYPE = "items";
+
+    private @NonNullByDefault({}) ItemRegistry itemRegistry;
+    private @NonNullByDefault({}) ModelRepository modelRepository;
+    private @NonNullByDefault({}) ThingRegistry thingRegistry;
+    private @NonNullByDefault({}) ItemChannelLinkRegistry itemChannelLinkRegistry;
+
+    @BeforeEach
+    public void setUp() {
+        registerVolatileStorageService();
+
+        itemRegistry = getService(ItemRegistry.class);
+        assertNotNull(itemRegistry);
+        assertEquals(0, itemRegistry.getAll().size());
+
+        modelRepository = getService(ModelRepository.class);
+        assertNotNull(modelRepository);
+        assertFalse(modelRepository.getAllModelNamesOfType(ITEMS_MODEL_TYPE).iterator().hasNext());
+
+        thingRegistry = getService(ThingRegistry.class);
+        assertNotNull(thingRegistry);
+
+        itemChannelLinkRegistry = getService(ItemChannelLinkRegistry.class);
+        assertNotNull(itemChannelLinkRegistry);
+    }
+
+    @Test
+    public void assertItemChannelLinkRegistryIsUpdated() {
+        String input = "String %s { channel=\"test:test:test:test\" [ boolVal=true, foo=\"bar\" ] }"
+                .formatted(TEST_ITEM_NAME);
+
+        modelRepository.addOrRefreshModel(TEST_MODEL_NAME, new ByteArrayInputStream(input.getBytes()));
+
+        Item item = itemRegistry.get(TEST_ITEM_NAME);
+        assertNotNull(item);
+        assertEquals(1, itemChannelLinkRegistry.getLinks(TEST_ITEM_NAME).size());
+        Optional<ItemChannelLink> optional = itemChannelLinkRegistry.getLinks(TEST_ITEM_NAME).stream().findFirst();
+        assertTrue(optional.isPresent());
+        ItemChannelLink link = optional.get();
+        assertEquals(new ChannelUID("test:test:test:test"), link.getLinkedUID());
+        Configuration conf = link.getConfiguration();
+        assertNotNull(conf);
+        assertEquals(true, conf.get("boolVal"));
+        assertEquals("bar", conf.get("foo"));
+    }
+
+    @Test
+    public void assertItemAssignedDefaultTags() {
+        ThingTypeUID thingTypeUID = new ThingTypeUID("test", "test");
+        ThingUID thingUID = new ThingUID(thingTypeUID, "test");
+        Channel Channel = ChannelBuilder.create(new ChannelUID(thingUID, "test")).withDefaultTags(Set.of("foo", "bar"))
+                .build();
+        ThingBuilder thingBuilder = ThingBuilder.create(thingTypeUID, thingUID).withChannel(Channel);
+        thingRegistry.add(thingBuilder.build());
+
+        String input = "String %s { channel=\"test:test:test:test\" [ %s=true ] }".formatted(TEST_ITEM_NAME,
+                ItemChannelLinkRegistry.USE_TAGS);
+
+        modelRepository.addOrRefreshModel(TEST_MODEL_NAME, new ByteArrayInputStream(input.getBytes()));
+
+        Item item = itemRegistry.get(TEST_ITEM_NAME);
+        assertNotNull(item);
+        assertEquals(2, item.getTags().size());
+        assertTrue(item.getTags().contains("foo"));
+        assertTrue(item.getTags().contains("bar"));
+    }
+
+    @Test
+    public void assertItemDidNotAssignDefaultTags() {
+        ThingTypeUID thingTypeUID = new ThingTypeUID("test", "test");
+        ThingUID thingUID = new ThingUID(thingTypeUID, "test");
+        Channel Channel = ChannelBuilder.create(new ChannelUID(thingUID, "test")).withDefaultTags(Set.of("foo", "bar"))
+                .build();
+        ThingBuilder thingBuilder = ThingBuilder.create(thingTypeUID, thingUID).withChannel(Channel);
+        thingRegistry.add(thingBuilder.build());
+
+        String input = "String %s { channel=\"test:test:test:test\" [ %s=false ] }".formatted(TEST_ITEM_NAME,
+                ItemChannelLinkRegistry.USE_TAGS);
+
+        modelRepository.addOrRefreshModel(TEST_MODEL_NAME, new ByteArrayInputStream(input.getBytes()));
+
+        Item item = itemRegistry.get(TEST_ITEM_NAME);
+        assertNotNull(item);
+        assertEquals(0, item.getTags().size());
+    }
+
+    @Test
+    public void assertItemAssignedOwnTags() {
+        ThingTypeUID thingTypeUID = new ThingTypeUID("test", "test");
+        ThingUID thingUID = new ThingUID(thingTypeUID, "test");
+        Channel Channel = ChannelBuilder.create(new ChannelUID(thingUID, "test")).withDefaultTags(Set.of("foo", "bar"))
+                .build();
+        ThingBuilder thingBuilder = ThingBuilder.create(thingTypeUID, thingUID).withChannel(Channel);
+        thingRegistry.add(thingBuilder.build());
+
+        String input = "String %s [tag1, tag2] { channel=\"test:test:test:test\" [ %s=true ] }"
+                .formatted(TEST_ITEM_NAME, ItemChannelLinkRegistry.USE_TAGS);
+
+        modelRepository.addOrRefreshModel(TEST_MODEL_NAME, new ByteArrayInputStream(input.getBytes()));
+
+        Item item = itemRegistry.get(TEST_ITEM_NAME);
+        assertNotNull(item);
+        assertEquals(2, item.getTags().size());
+        assertTrue(item.getTags().contains("tag1"));
+        assertTrue(item.getTags().contains("tag2"));
+    }
+}
