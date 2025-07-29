11cc97c8dd9afeb6c88696ac4bc4332ad7350320
Mark Herwege <mark.herwege@telenet.be>
2025-07-29 16:38:18 +0200

    Adjustments based on AI review
    Signed-off-by: Mark Herwege <mark.herwege@telenet.be>

--- bundles/org.openhab.core.persistence/src/main/java/org/openhab/core/persistence/dto/PersistenceServiceConfigurationDTO.java
+++ bundles/org.openhab.core.persistence/src/main/java/org/openhab/core/persistence/dto/PersistenceServiceConfigurationDTO.java
@@ -30,7 +30,13 @@ public class PersistenceServiceConfigurationDTO {
     public String serviceId = "";
     public Collection<PersistenceItemConfigurationDTO> configs = List.of();
     public Map<String, String> aliases = Map.of();
-    public @Nullable Collection<String> defaults; // Kept to be able to migrate previous version storage
+    /**
+     * @deprecated This field is kept to enable migration from previous version storage.
+     *             It should not be removed as this would make automatic upgrading persistence configurations from an
+     *             older version impossible.
+     */
+    @Deprecated
+    public @Nullable Collection<String> defaults;
     public Collection<PersistenceCronStrategyDTO> cronStrategies = List.of();
     public Collection<PersistenceFilterDTO> thresholdFilters = List.of();
     public Collection<PersistenceFilterDTO> timeFilters = List.of();
