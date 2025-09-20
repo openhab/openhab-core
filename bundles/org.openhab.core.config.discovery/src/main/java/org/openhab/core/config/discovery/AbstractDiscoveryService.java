c78ccda53e365ba5216613c27a740ebae24528fe
Ravi Nadahar <nadahar@rediffmail.com>
2025-09-20 03:58:22 +0200

    Isolate discovery result registrations from binding threads
    By using the discovery thread pool to do the actual registrations in AbstractDiscoveryService, binding threads won't have to wait for the registration to complete, which can be slow.
    
    In addition, some thread-safety fixes have been done in AbstractDiscoveryService and DiscoveryServiceRegistryImpl, both to avoid contention and to ensure isolation of mutable objects.
    
    Signed-off-by: Ravi Nadahar <nadahar@rediffmail.com>

--- bundles/org.openhab.core.config.discovery/src/main/java/org/openhab/core/config/discovery/AbstractDiscoveryService.java
+++ bundles/org.openhab.core.config.discovery/src/main/java/org/openhab/core/config/discovery/AbstractDiscoveryService.java
@@ -16,6 +16,7 @@ import java.time.Instant;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.CancellationException;
@@ -61,20 +62,26 @@ public abstract class AbstractDiscoveryService implements DiscoveryService {
     protected final ScheduledExecutorService scheduler = ThreadPoolManager.getScheduledPool(DISCOVERY_THREADPOOL_NAME);
 
     private final Set<DiscoveryListener> discoveryListeners = new CopyOnWriteArraySet<>();
-    protected @Nullable ScanListener scanListener = null;
 
-    private boolean backgroundDiscoveryEnabled;
+    // All access must be guarded by "this"
+    protected @Nullable ScanListener scanListener;
 
-    private @Nullable String scanInputLabel;
-    private @Nullable String scanInputDescription;
+    private volatile boolean backgroundDiscoveryEnabled;
 
+    private final @Nullable String scanInputLabel;
+    private final @Nullable String scanInputDescription;
+
+    // All access must be guarded by "cachedResults"
     private final Map<ThingUID, DiscoveryResult> cachedResults = new HashMap<>();
 
+    // This set is immutable and can safely be shared between threads
     private final Set<ThingTypeUID> supportedThingTypes;
     private final int timeout;
 
+    // All access must be guarded by "this"
     private Instant timestampOfLastScan = Instant.MIN;
 
+    // All access must be guarded by "this"
     private @Nullable ScheduledFuture<?> scheduledStop;
 
     protected @NonNullByDefault({}) TranslationProvider i18nProvider;
@@ -198,12 +205,14 @@ public abstract class AbstractDiscoveryService implements DiscoveryService {
         if (listener == null) {
             return;
         }
-        synchronized (cachedResults) {
-            for (DiscoveryResult cachedResult : cachedResults.values()) {
-                listener.thingDiscovered(this, cachedResult);
-            }
-        }
         discoveryListeners.add(listener);
+        Map<ThingUID, DiscoveryResult> existingResults;
+        synchronized (cachedResults) {
+            existingResults = Map.copyOf(cachedResults);
+        }
+        for (DiscoveryResult existingResult : existingResults.values()) {
+            listener.thingDiscovered(this, existingResult);
+        }
     }
 
     @Override
@@ -221,11 +230,10 @@ public abstract class AbstractDiscoveryService implements DiscoveryService {
         startScanInternal(input, listener);
     }
 
-    private synchronized void startScanInternal(@Nullable String input, @Nullable ScanListener listener) {
+    private void startScanInternal(@Nullable String input, @Nullable ScanListener listener) {
+        // we first stop any currently running scan and its scheduled stop call
+        stopScan();
         synchronized (this) {
-            // we first stop any currently running scan and its scheduled stop
-            // call
-            stopScan();
             ScheduledFuture<?> scheduledStop = this.scheduledStop;
             if (scheduledStop != null) {
                 scheduledStop.cancel(false);
@@ -245,39 +253,41 @@ public abstract class AbstractDiscoveryService implements DiscoveryService {
                 }, getScanTimeout(), TimeUnit.SECONDS);
             }
             timestampOfLastScan = Instant.now();
-
-            try {
-                if (isScanInputSupported() && input != null) {
-                    startScan(input);
-                } else {
-                    startScan();
-                }
-            } catch (Exception ex) {
-                scheduledStop = this.scheduledStop;
+        }
+        try {
+            if (isScanInputSupported() && input != null) {
+                startScan(input);
+            } else {
+                startScan();
+            }
+        } catch (Exception ex) {
+            synchronized (this) {
+                ScheduledFuture<?> scheduledStop = this.scheduledStop;
                 if (scheduledStop != null) {
                     scheduledStop.cancel(false);
                     this.scheduledStop = null;
                 }
                 scanListener = null;
-                throw ex;
             }
+            throw ex;
         }
     }
 
     @Override
-    public synchronized void abortScan() {
+    public void abortScan() {
+        ScanListener scanListener = null;
         synchronized (this) {
             ScheduledFuture<?> scheduledStop = this.scheduledStop;
             if (scheduledStop != null) {
-                scheduledStop.cancel(false);
+                scheduledStop.cancel(true);
                 this.scheduledStop = null;
             }
-            final ScanListener scanListener = this.scanListener;
-            if (scanListener != null) {
-                Exception e = new CancellationException("Scan has been aborted.");
-                scanListener.onErrorOccurred(e);
-                this.scanListener = null;
-            }
+            scanListener = this.scanListener;
+            this.scanListener = null;
+        }
+        if (scanListener != null) {
+            Exception e = new CancellationException("Scan has been aborted.");
+            scanListener.onErrorOccurred(e);
         }
     }
 
@@ -297,11 +307,14 @@ public abstract class AbstractDiscoveryService implements DiscoveryService {
     /**
      * This method cleans up after a scan, i.e. it removes listeners and other required operations.
      */
-    protected synchronized void stopScan() {
-        ScanListener scanListener = this.scanListener;
+    protected void stopScan() {
+        ScanListener scanListener = null;
+        synchronized (this) {
+            scanListener = this.scanListener;
+            this.scanListener = null;
+        }
         if (scanListener != null) {
             scanListener.onFinished();
-            this.scanListener = null;
         }
     }
 
@@ -314,12 +327,14 @@ public abstract class AbstractDiscoveryService implements DiscoveryService {
         final DiscoveryResult discoveryResultNew = getLocalizedDiscoveryResult(discoveryResult,
                 FrameworkUtil.getBundle(this.getClass()));
         for (DiscoveryListener discoveryListener : discoveryListeners) {
-            try {
-                discoveryListener.thingDiscovered(this, discoveryResultNew);
-            } catch (Exception e) {
-                logger.error("An error occurred while calling the discovery listener {}.",
-                        discoveryListener.getClass().getName(), e);
-            }
+            scheduler.execute(() -> {
+                try {
+                    discoveryListener.thingDiscovered(this, discoveryResultNew);
+                } catch (Exception e) {
+                    logger.error("An error occurred while calling the discovery listener {}.",
+                            discoveryListener.getClass().getName(), e);
+                }
+            });
         }
         synchronized (cachedResults) {
             cachedResults.put(discoveryResultNew.getThingUID(), discoveryResultNew);
@@ -384,18 +399,22 @@ public abstract class AbstractDiscoveryService implements DiscoveryService {
      */
     protected void removeOlderResults(Instant timestamp, @Nullable Collection<ThingTypeUID> thingTypeUIDs,
             @Nullable ThingUID bridgeUID) {
-        Collection<ThingUID> removedThings = null;
+        Set<ThingUID> removedThings = new HashSet<>();
 
+        Collection<ThingUID> removed;
         Collection<ThingTypeUID> toBeRemoved = thingTypeUIDs != null ? thingTypeUIDs : getSupportedThingTypes();
         for (DiscoveryListener discoveryListener : discoveryListeners) {
             try {
-                removedThings = discoveryListener.removeOlderResults(this, timestamp, toBeRemoved, bridgeUID);
+                removed = discoveryListener.removeOlderResults(this, timestamp, toBeRemoved, bridgeUID);
+                if (removed != null) {
+                    removedThings.addAll(removed);
+                }
             } catch (Exception e) {
                 logger.error("An error occurred while calling the discovery listener {}.",
                         discoveryListener.getClass().getName(), e);
             }
         }
-        if (removedThings != null) {
+        if (!removedThings.isEmpty()) {
             synchronized (cachedResults) {
                 for (ThingUID uid : removedThings) {
                     cachedResults.remove(uid);
@@ -486,7 +505,7 @@ public abstract class AbstractDiscoveryService implements DiscoveryService {
      *
      * @return timestamp as {@link Instant}
      */
-    protected Instant getTimestampOfLastScan() {
+    protected synchronized Instant getTimestampOfLastScan() {
         return timestampOfLastScan;
     }
 
@@ -496,6 +515,8 @@ public abstract class AbstractDiscoveryService implements DiscoveryService {
 
     protected DiscoveryResult getLocalizedDiscoveryResult(final DiscoveryResult discoveryResult,
             @Nullable Bundle bundle) {
+        TranslationProvider i18nProvider = this.i18nProvider;
+        LocaleProvider localeProvider = this.localeProvider;
         if (i18nProvider != null && localeProvider != null) {
             String currentLabel = discoveryResult.getLabel();
 
