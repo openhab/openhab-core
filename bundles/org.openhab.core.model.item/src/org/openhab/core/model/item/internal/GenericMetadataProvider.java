openhab/openhab-core

commit 00b6a476a72ef38b64cd25dea9e26bfd985c965b

[REST] New API for conversion between file format and JSON (#4793)
* [REST] New API for conversion between file format and JSON

Related to #4585

This PR supports conversion of things and items.
It supports DSL and YAML file formats.

A first new API (POST /file-format/create) allows to create a file format (DSL or YAML) from a JSON object.
A second new API (POST /file-format/parse) allows to parse a file format (DSL or YAML) to a JSON object.

These 2 APIs should help Main UI displaying DSL and YAML file formats for things.

Signed-off-by: Laurent Garnier <lg.hc@free.fr>

bundles/org.openhab.core.model.item/src/org/openhab/core/model/item/internal/GenericMetadataProvider.java
@@ -13,10 +13,13 @@
 package org.openhab.core.model.item.internal;
 
 import static java.util.stream.Collectors.toSet;
+import static org.openhab.core.model.core.ModelCoreConstants.isIsolatedModel;
 
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Set;
 import java.util.concurrent.locks.ReadWriteLock;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
@@ -38,32 +41,37 @@ import org.osgi.service.component.annotations.Component;
  * methods.
  *
  * @author Kai Kreuzer - Initial contribution
+ * @author Laurent Garnier - Store metadata per model + do not notify the registry for isolated models
  */
 @Component(service = { MetadataProvider.class, GenericMetadataProvider.class })
 @NonNullByDefault
 public class GenericMetadataProvider extends AbstractProvider<Metadata> implements MetadataProvider {
 
-    private final Set<Metadata> metadata = new HashSet<>();
+    private final Map<String, Set<Metadata>> metadata = new HashMap<>();
     private final ReadWriteLock lock = new ReentrantReadWriteLock(true);
 
     /**
      * Adds metadata to this provider
      *
+     * @param modelName the model name
      * @param bindingType
      * @param itemName
      * @param configuration
      */
-    public void addMetadata(String bindingType, String itemName, String value,
+    public void addMetadata(String modelName, String bindingType, String itemName, String value,
             @Nullable Map<String, Object> configuration) {
         MetadataKey key = new MetadataKey(bindingType, itemName);
         Metadata md = new Metadata(key, value, configuration);
         try {
             lock.writeLock().lock();
-            metadata.add(md);
+            Set<Metadata> mdSet = Objects.requireNonNull(metadata.computeIfAbsent(modelName, k -> new HashSet<>()));
+            mdSet.add(md);
         } finally {
             lock.writeLock().unlock();
         }
-        notifyListenersAboutAddedElement(md);
+        if (!isIsolatedModel(modelName)) {
+            notifyListenersAboutAddedElement(md);
+        }
     }
 
     /**
@@ -72,41 +80,75 @@ public class GenericMetadataProvider extends AbstractProvider<Metadata> implemen
      * @param namespace the namespace
      */
     public void removeMetadataByNamespace(String namespace) {
-        Set<Metadata> toBeRemoved;
+        Map<String, Set<Metadata>> toBeNotified;
         try {
             lock.writeLock().lock();
-            toBeRemoved = metadata.stream().filter(MetadataPredicates.hasNamespace(namespace)).collect(toSet());
-            metadata.removeAll(toBeRemoved);
+            toBeNotified = new HashMap<>();
+            for (Map.Entry<String, Set<Metadata>> entry : metadata.entrySet()) {
+                String modelName = entry.getKey();
+                Set<Metadata> mdSet = entry.getValue();
+                Set<Metadata> toBeRemoved = mdSet.stream().filter(MetadataPredicates.hasNamespace(namespace))
+                        .collect(toSet());
+                mdSet.removeAll(toBeRemoved);
+                if (mdSet.isEmpty()) {
+                    metadata.remove(modelName);
+                }
+                if (!isIsolatedModel(modelName) && !toBeRemoved.isEmpty()) {
+                    toBeNotified.put(modelName, toBeRemoved);
+                }
+            }
         } finally {
             lock.writeLock().unlock();
         }
-        toBeRemoved.forEach(this::notifyListenersAboutRemovedElement);
+        toBeNotified.values().forEach((set) -> {
+            set.forEach(this::notifyListenersAboutRemovedElement);
+        });
     }
 
     /**
      * Removes all meta-data for a given item
      *
+     * @param modelName the model name
      * @param itemName the item name
      */
-    public void removeMetadataByItemName(String itemName) {
-        Set<Metadata> toBeRemoved;
+    public void removeMetadataByItemName(String modelName, String itemName) {
+        Set<Metadata> toBeNotified;
         try {
             lock.writeLock().lock();
-            toBeRemoved = metadata.stream().filter(MetadataPredicates.ofItem(itemName)).collect(toSet());
-            metadata.removeAll(toBeRemoved);
+            toBeNotified = new HashSet<>();
+            Set<Metadata> mdSet = metadata.getOrDefault(modelName, new HashSet<>());
+            Set<Metadata> toBeRemoved = mdSet.stream().filter(MetadataPredicates.ofItem(itemName)).collect(toSet());
+            mdSet.removeAll(toBeRemoved);
+            if (mdSet.isEmpty()) {
+                metadata.remove(modelName);
+            }
+            if (!isIsolatedModel(modelName) && !toBeRemoved.isEmpty()) {
+                toBeNotified.addAll(toBeRemoved);
+            }
         } finally {
             lock.writeLock().unlock();
         }
-        for (Metadata m : toBeRemoved) {
-            notifyListenersAboutRemovedElement(m);
-        }
+        toBeNotified.forEach(this::notifyListenersAboutRemovedElement);
     }
 
     @Override
     public Collection<Metadata> getAll() {
         try {
             lock.readLock().lock();
-            return Set.copyOf(metadata);
+            // Ignore isolated models
+            Set<Metadata> set = metadata.keySet().stream().filter(name -> !isIsolatedModel(name))
+                    .map(name -> metadata.getOrDefault(name, Set.of())).flatMap(s -> s.stream()).collect(toSet());
+            return Set.copyOf(set);
+        } finally {
+            lock.readLock().unlock();
+        }
+    }
+
+    public Collection<Metadata> getAllFromModel(String modelName) {
+        try {
+            lock.readLock().lock();
+            Set<Metadata> set = metadata.getOrDefault(modelName, Set.of());
+            return Set.copyOf(set);
         } finally {
             lock.readLock().unlock();
         }

bundles/org.openhab.core.io.rest.core/src/main/java/org/openhab/core/io/rest/core/fileformat/ExtendedFileFormatDTO.java
@@ -1,0 +1,26 @@
+/*
+ * Copyright (c) 2010-2025 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.core.io.rest.core.fileformat;
+
+import java.util.List;
+
+/**
+ * This is a data transfer object to serialize the different components that can be contained
+ * in a file format (items, things, ...) including an optional list of warnings.
+ *
+ * @author Laurent Garnier - Initial contribution
+ */
+public class ExtendedFileFormatDTO extends FileFormatDTO {
+
+    public List<String> warnings;
+}

bundles/org.openhab.core.io.rest.core/src/main/java/org/openhab/core/io/rest/core/fileformat/FileFormatChannelLinkDTO.java
@@ -1,0 +1,31 @@
+/*
+ * Copyright (c) 2010-2025 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.core.io.rest.core.fileformat;
+
+import java.util.Map;
+
+/**
+ * This is a data transfer object to serialize a channel link for an item contained in a file format.
+ *
+ * @author Laurent Garnier - Initial contribution
+ */
+public class FileFormatChannelLinkDTO {
+
+    public String channelUID;
+    public Map<String, Object> configuration;
+
+    public FileFormatChannelLinkDTO(String channelUID, Map<String, Object> configuration) {
+        this.channelUID = channelUID;
+        this.configuration = configuration;
+    }
+}

bundles/org.openhab.core.io.rest.core/src/main/java/org/openhab/core/io/rest/core/fileformat/FileFormatDTO.java
@@ -1,0 +1,29 @@
+/*
+ * Copyright (c) 2010-2025 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.core.io.rest.core.fileformat;
+
+import java.util.List;
+
+import org.openhab.core.thing.dto.ThingDTO;
+
+/**
+ * This is a data transfer object to serialize the different components that can be contained
+ * in a file format (items, things, ...).
+ *
+ * @author Laurent Garnier - Initial contribution
+ */
+public class FileFormatDTO {
+
+    public List<FileFormatItemDTO> items;
+    public List<ThingDTO> things;
+}

bundles/org.openhab.core.io.rest.core/src/main/java/org/openhab/core/io/rest/core/fileformat/FileFormatItemDTO.java
@@ -1,0 +1,48 @@
+/*
+ * Copyright (c) 2010-2025 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.core.io.rest.core.fileformat;
+
+import java.util.List;
+import java.util.Map;
+
+import org.openhab.core.items.dto.GroupFunctionDTO;
+import org.openhab.core.items.dto.GroupItemDTO;
+import org.openhab.core.items.dto.ItemDTO;
+import org.openhab.core.items.dto.MetadataDTO;
+
+/**
+ * This is a data transfer object to serialize an item contained in a file format.
+ *
+ * @author Laurent Garnier - Initial contribution
+ */
+public class FileFormatItemDTO extends ItemDTO {
+
+    public String groupType;
+    public GroupFunctionDTO function;
+    public String format;
+    public Map<String, MetadataDTO> metadata;
+    public List<FileFormatChannelLinkDTO> channelLinks;
+
+    public FileFormatItemDTO(ItemDTO itemDTO, boolean isGroup) {
+        this.type = itemDTO.type;
+        this.name = itemDTO.name;
+        this.label = itemDTO.label;
+        this.category = itemDTO.category;
+        this.tags = itemDTO.tags;
+        this.groupNames = itemDTO.groupNames;
+        if (isGroup) {
+            this.groupType = ((GroupItemDTO) itemDTO).groupType;
+            this.function = ((GroupItemDTO) itemDTO).function;
+        }
+    }
+}

bundles/org.openhab.core.io.rest.core/src/main/java/org/openhab/core/io/rest/core/fileformat/FileFormatItemDTOMapper.java
@@ -1,0 +1,135 @@
+/*
+ * Copyright (c) 2010-2025 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.core.io.rest.core.fileformat;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.eclipse.jdt.annotation.Nullable;
+import org.openhab.core.items.GroupItem;
+import org.openhab.core.items.Item;
+import org.openhab.core.items.ItemBuilderFactory;
+import org.openhab.core.items.Metadata;
+import org.openhab.core.items.MetadataKey;
+import org.openhab.core.items.dto.GroupItemDTO;
+import org.openhab.core.items.dto.ItemDTO;
+import org.openhab.core.items.dto.ItemDTOMapper;
+import org.openhab.core.items.dto.MetadataDTO;
+import org.openhab.core.thing.link.ItemChannelLink;
+
+/**
+ * The {@link FileFormatItemDTOMapper} is a utility class to map items into file format item data transfer objects
+ * (DTOs).
+ *
+ * @author Laurent Garnier - Initial contribution
+ */
+@NonNullByDefault
+public class FileFormatItemDTOMapper {
+
+    /**
+     * Maps item into file format item DTO object.
+     *
+     * @param item the item
+     * @param metadata some metadata
+     * @param format the format to be used to format the item state, can be NULL
+     * @param channelLinks some items channel links
+     * @return file format item DTO object
+     */
+    public static FileFormatItemDTO map(Item item, Collection<Metadata> metadata, @Nullable String format,
+            Collection<ItemChannelLink> channelLinks) {
+        ItemDTO itemDto = ItemDTOMapper.map(item);
+        FileFormatItemDTO dto = new FileFormatItemDTO(itemDto, itemDto instanceof GroupItemDTO);
+
+        dto.format = format;
+
+        Map<String, MetadataDTO> metadataDTO = new LinkedHashMap<>();
+        metadata.forEach(md -> {
+            if (item.getName().equals(md.getUID().getItemName())) {
+                MetadataDTO mdDTO = new MetadataDTO();
+                mdDTO.value = md.getValue();
+                mdDTO.config = md.getConfiguration().isEmpty() ? null : md.getConfiguration();
+                metadataDTO.put(md.getUID().getNamespace(), mdDTO);
+            }
+        });
+        if (!metadataDTO.isEmpty()) {
+            dto.metadata = metadataDTO;
+        }
+
+        List<FileFormatChannelLinkDTO> channelLinksDTO = new ArrayList<>();
+        channelLinks.forEach(link -> {
+            if (item.getName().equals(link.getItemName())) {
+                channelLinksDTO.add(new FileFormatChannelLinkDTO(link.getLinkedUID().getAsString(),
+                        link.getConfiguration().getProperties().isEmpty() ? null
+                                : link.getConfiguration().getProperties()));
+            }
+        });
+        if (!channelLinksDTO.isEmpty()) {
+            dto.channelLinks = channelLinksDTO;
+        }
+
+        return dto;
+    }
+
+    /**
+     * Maps file format item DTO object into item.
+     *
+     * @param dto the file format item DTO object
+     * @param itemBuilderFactory the item builder factory
+     * @return item
+     */
+    public static @Nullable Item map(FileFormatItemDTO dto, ItemBuilderFactory itemBuilderFactory) {
+        if (GroupItem.TYPE.equals(dto.type)) {
+            GroupItemDTO groupDto = new GroupItemDTO();
+            groupDto.type = dto.type;
+            groupDto.name = dto.name;
+            groupDto.label = dto.label;
+            groupDto.category = dto.category;
+            groupDto.tags = dto.tags;
+            groupDto.groupNames = dto.groupNames;
+            groupDto.groupType = dto.groupType;
+            groupDto.function = dto.function;
+            return ItemDTOMapper.map(groupDto, itemBuilderFactory);
+        }
+        return ItemDTOMapper.map(dto, itemBuilderFactory);
+    }
+
+    /**
+     * Maps file format item DTO object into a collection of metadata including channels links
+     * provided through the "channel" namespace.
+     *
+     * @param dto the file format item DTO object
+     * @return the collection of metadata
+     */
+    public static Collection<Metadata> mapMetadata(FileFormatItemDTO dto) {
+        String name = dto.name;
+        Collection<Metadata> metadata = new ArrayList<>();
+        if (dto.channelLinks != null) {
+            for (FileFormatChannelLinkDTO link : dto.channelLinks) {
+                MetadataKey key = new MetadataKey("channel", name);
+                metadata.add(new Metadata(key, link.channelUID, link.configuration));
+            }
+        }
+        if (dto.metadata != null) {
+            for (Map.Entry<String, MetadataDTO> md : dto.metadata.entrySet()) {
+                MetadataKey key = new MetadataKey(md.getKey(), name);
+                metadata.add(new Metadata(key, Objects.requireNonNull(md.getValue().value), md.getValue().config));
+            }
+        }
+        return metadata;
+    }
+}

bundles/org.openhab.core.model.core/src/main/java/org/openhab/core/model/core/ModelRepository.java
@@ -14,6 +14,7 @@ package org.openhab.core.model.core;
 
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.List;
 import java.util.Set;
 
 import org.eclipse.emf.ecore.EObject;
@@ -27,7 +28,7 @@ import org.eclipse.jdt.annotation.Nullable;
  * come from.
  *
  * @author Kai Kreuzer - Initial contribution
- * @author Laurent Garnier - Added method generateSyntaxFromModel
+ * @author Laurent Garnier - Added methods generateFileFormat and createIsolatedModel
  */
 @NonNullByDefault
 public interface ModelRepository {
@@ -96,11 +97,25 @@ public interface ModelRepository {
     void removeModelRepositoryChangeListener(ModelRepositoryChangeListener listener);
 
     /**
-     * Generate the syntax from a provided model content.
+     * Creates an isolated model in the repository
+     *
+     * An isolated model is a temporary model loaded without impacting any object registry.
+     *
+     * @param modelType the model type
+     * @param inputStream an input stream with the model's content
+     * @param errors the list to be used to fill the errors
+     * @param warnings the list to be used to fill the warnings
+     * @return the created model name if it was successfully processed, null otherwise
+     */
+    @Nullable
+    String createIsolatedModel(String modelType, InputStream inputStream, List<String> errors, List<String> warnings);
+
+    /**
+     * Generate the DSL file format from a provided model type and model content.
      *
      * @param out the output stream to write the generated syntax to
      * @param modelType the model type
      * @param modelContent the content of the model
      */
-    void generateSyntaxFromModel(OutputStream out, String modelType, EObject modelContent);
+    void generateFileFormat(OutputStream out, String modelType, EObject modelContent);
 }

commit 6c41d7a3c65e62db3a0336ae84b6a4d8e00a35a3

Fix ConcurrentModificationException during format conversion
Regression from #4793

Signed-off-by: Holger Friedrich <mail@holger-friedrich.de>

bundles/org.openhab.core.model.item/src/org/openhab/core/model/item/internal/GenericMetadataProvider.java
@@ -84,6 +84,7 @@ public class GenericMetadataProvider extends AbstractProvider<Metadata> implemen
         try {
             lock.writeLock().lock();
             toBeNotified = new HashMap<>();
+            Set<String> modelsToRemove = new HashSet<>();
             for (Map.Entry<String, Set<Metadata>> entry : metadata.entrySet()) {
                 String modelName = entry.getKey();
                 Set<Metadata> mdSet = entry.getValue();
@@ -91,12 +92,14 @@ public class GenericMetadataProvider extends AbstractProvider<Metadata> implemen
                         .collect(toSet());
                 mdSet.removeAll(toBeRemoved);
                 if (mdSet.isEmpty()) {
-                    metadata.remove(modelName);
+                    modelsToRemove.add(modelName);
                 }
                 if (!isIsolatedModel(modelName) && !toBeRemoved.isEmpty()) {
                     toBeNotified.put(modelName, toBeRemoved);
                 }
             }
+            // Remove empty model entries after iteration to avoid ConcurrentModificationException
+            modelsToRemove.forEach(metadata::remove);
         } finally {
             lock.writeLock().unlock();
         }