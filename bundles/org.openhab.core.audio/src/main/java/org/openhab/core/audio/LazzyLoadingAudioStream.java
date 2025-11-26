457ba8d8a94d8dd52f3a698ee20546a57c9a0f74
Laurent ARNAL <laurent@clae.net>
2025-11-26 14:52:43 +0100

    WIP : experiment how we can add missing metadata information from streaming mpd stream Urls
    Signed-off-by: Laurent ARNAL <laurent@clae.net>

--- bundles/org.openhab.core.audio/src/main/java/org/openhab/core/audio/LazzyLoadingAudioStream.java
+++ bundles/org.openhab.core.audio/src/main/java/org/openhab/core/audio/LazzyLoadingAudioStream.java
@@ -12,18 +12,19 @@
  */
 package org.openhab.core.audio;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.PipedInputStream;
 import java.io.PipedOutputStream;
 import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
-
-import org.eclipse.jdt.annotation.NonNullByDefault;
-import org.eclipse.jdt.annotation.Nullable;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import java.util.Map;
 
 /**
  * This is an AudioStream from a URL. Note that some sinks, like Sonos, can directly handle URL
@@ -33,25 +34,61 @@ import org.slf4j.LoggerFactory;
  * @author Kai Kreuzer - Refactored to not require a source
  * @author Christoph Weitkamp - Refactored use of filename extension
  */
-@NonNullByDefault
 public class LazzyLoadingAudioStream extends InputStream {
-    private final Logger logger = LoggerFactory.getLogger(LazzyLoadingAudioStream.class);
-
     private final Iterator<URL> urls;
-    @Nullable
     private InputStream current;
+    private boolean first = true;
+    private String artist;
+    private String title;
 
-    public LazzyLoadingAudioStream(List<URL> urls) {
+    public LazzyLoadingAudioStream(List<URL> urls, String artist, String title) {
         this.urls = urls.iterator();
+        this.artist = artist;
+        this.title = title;
+    }
+
+    private InputStream downloadAndInject(URL url) throws IOException {
+        // 1) Télécharge dans un buffer
+        byte[] rawInit = url.openStream().readAllBytes();
+
+        // 2) Modifie le MP4 (init segment)
+        ByteArrayInputStream in = new ByteArrayInputStream(rawInit);
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+        // Appel à ton injecteur :
+        Map<String, String> tags = new HashMap<>();
+        tags.put("title", title);
+        tags.put("artist", artist);
+        tags.put("album", "Greatest Hits");
+        tags.put("genre", "Rock");
+        tags.put("year", "2023");
+        tags.put("track", "1");
+        tags.put("trackTotal", "12");
+        tags.put("albumArtist", "John Doe");
+
+        byte[] cover = Files.readAllBytes(
+                Paths.get("C:/eclipse/openhab-main/git/openhab-distro/launch/app/runtime/conf/html/Deezer.png"));
+
+        Mp4UdtaInjector.inject(in, out, tags, cover);
+
+        byte[] modified = out.toByteArray();
+
+        // 3) Retourne un InputStream lisant le MP4 modifié
+        return new ByteArrayInputStream(modified);
     }
 
     private InputStream startDownload(URL url) throws IOException {
+        if (first) {
+            first = false;
+            return downloadAndInject(url);
+        }
+
         PipedInputStream in = new PipedInputStream(64 * 1024);
         PipedOutputStream out = new PipedOutputStream(in);
 
         new Thread(() -> {
             try (InputStream src = url.openStream()) {
-                logger.info("Download URL:" + url.toString());
+                System.out.println("Download URL:" + url.toString());
 
                 src.transferTo(out);
             } catch (IOException ignored) {
