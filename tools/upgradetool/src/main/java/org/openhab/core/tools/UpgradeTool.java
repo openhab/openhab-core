9c3907da2ce2f92df12cc99700c7e8c909b04666
Kai Kreuzer <kai@openhab.org>
2025-12-14 18:23:49 +0100

    Add PersistenceUpgrader (#5190)
    This accidentially got lost in https://github.com/openhab/openhab-core/commit/cea82896242c290941a62c519c618d4b702b260f
    
    Signed-off-by: Kai Kreuzer <kai@openhab.org>

--- tools/upgradetool/src/main/java/org/openhab/core/tools/UpgradeTool.java
+++ tools/upgradetool/src/main/java/org/openhab/core/tools/UpgradeTool.java
@@ -57,7 +57,8 @@ public class UpgradeTool {
             new ScriptProfileUpgrader(), //
             new YamlConfigurationV1TagsUpgrader(), // Added in 5.0
             new HomeAssistantAddonUpgrader(), // Added in 5.1
-            new HomieAddonUpgrader() // Added in 5.1
+            new HomieAddonUpgrader(), // Added in 5.1
+            new PersistenceUpgrader() // Added in 5.1
     );
 
     private static final Logger LOGGER = LoggerFactory.getLogger(UpgradeTool.class);
