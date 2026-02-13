185830994be963355b51fee24248b82697205c10
Jimmy Tanagra <jcode@tanagra.id.au>
2026-02-14 03:20:50 +1000

    improve log visibility of validation issues
    Signed-off-by: Jimmy Tanagra <jcode@tanagra.id.au>

--- bundles/org.openhab.core.model.core/src/main/java/org/openhab/core/model/core/internal/ModelRepositoryImpl.java
+++ bundles/org.openhab.core.model.core/src/main/java/org/openhab/core/model/core/internal/ModelRepositoryImpl.java
@@ -131,7 +131,7 @@ public class ModelRepositoryImpl implements ModelRepository {
                 return false;
             }
             if (!newWarnings.isEmpty()) {
-                logger.info("Validation issues found in DSL model '{}', using it anyway:\n{}", name,
+                logger.warn("Validation issues found in DSL model '{}', using it anyway:\n{}", name,
                         String.join("\n", newWarnings));
             }
         } catch (IOException e) {
