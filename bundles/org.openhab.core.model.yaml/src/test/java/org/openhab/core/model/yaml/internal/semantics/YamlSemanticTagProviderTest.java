630fd07ffcbce65b0dfbf34b8c935a29a9cdd614
Laurent Garnier <lg.hc@free.fr>
2026-02-13 08:07:08 +0100

    Enhance tests in YamlSemanticTagProviderTest
    Signed-off-by: Laurent Garnier <lg.hc@free.fr>

--- bundles/org.openhab.core.model.yaml/src/test/java/org/openhab/core/model/yaml/internal/semantics/YamlSemanticTagProviderTest.java
+++ bundles/org.openhab.core.model.yaml/src/test/java/org/openhab/core/model/yaml/internal/semantics/YamlSemanticTagProviderTest.java
@@ -14,30 +14,32 @@ package org.openhab.core.model.yaml.internal.semantics;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
-import static org.hamcrest.Matchers.contains;
-import static org.mockito.ArgumentMatchers.*;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.when;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
-import org.eclipse.jdt.annotation.NonNull;
 import org.eclipse.jdt.annotation.NonNullByDefault;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.api.io.TempDir;
-import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.mockito.junit.jupiter.MockitoSettings;
 import org.mockito.quality.Strictness;
-import org.openhab.core.model.yaml.YamlModelListener;
+import org.openhab.core.common.registry.Provider;
+import org.openhab.core.common.registry.ProviderChangeListener;
+import org.openhab.core.model.yaml.YamlElement;
 import org.openhab.core.model.yaml.internal.YamlModelRepositoryImpl;
+import org.openhab.core.semantics.SemanticTag;
 import org.openhab.core.service.WatchService;
 
 /**
@@ -56,10 +58,11 @@ public class YamlSemanticTagProviderTest {
     private @Mock @NonNullByDefault({}) WatchService watchServiceMock;
 
     private @NonNullByDefault({}) YamlModelRepositoryImpl modelRepository;
+    private @NonNullByDefault({}) YamlSemanticTagProvider semanticTagProvider;
+    private @NonNullByDefault({}) TestSemanticTagChangeListener semanticTagListener;
 
     private @TempDir @NonNullByDefault({}) Path watchPath;
     private @NonNullByDefault({}) Path fullModelPath;
-    private @NonNullByDefault({}) YamlModelListener<@NonNull YamlSemanticTagDTO> tagListener;
 
     @BeforeEach
     @SuppressWarnings({ "null", "unchecked" })
@@ -67,26 +70,30 @@ public class YamlSemanticTagProviderTest {
         fullModelPath = watchPath.resolve(MODEL_PATH);
         when(watchServiceMock.getWatchPath()).thenReturn(watchPath);
 
+        semanticTagListener = new TestSemanticTagChangeListener();
+
+        semanticTagProvider = new YamlSemanticTagProvider();
+        semanticTagProvider.addProviderChangeListener(semanticTagListener);
+
         modelRepository = new YamlModelRepositoryImpl(watchServiceMock);
-
-        tagListener = mock(YamlModelListener.class);
-        when(tagListener.getElementClass()).thenReturn(YamlSemanticTagDTO.class);
-        when(tagListener.isVersionSupported(anyInt())).thenReturn(true);
-        when(tagListener.isDeprecated()).thenReturn(false);
-
-        modelRepository.addYamlModelListener(tagListener);
+        modelRepository.addYamlModelListener(semanticTagProvider);
     }
 
     @Test
     public void testShortFormTagsLoadingAndGeneration() throws IOException {
         processYamlResource("tagWithShortFormSyntax.yaml");
-        Collection<YamlSemanticTagDTO> tags = captureAddedTags();
 
+        assertThat(semanticTagListener.semanticTags, is(aMapWithSize(1)));
+        assertThat(semanticTagListener.semanticTags, hasKey("Tag_uid"));
+        Collection<SemanticTag> tags = semanticTagProvider.getAll();
         assertThat(tags, hasSize(1));
-        YamlSemanticTagDTO tag = tags.iterator().next();
-        assertThat(tag.getId(), is("Tag_uid"));
-        assertThat(tag.label, is("TagLabel"));
-        assertThat(tag.description, is(nullValue()));
+        SemanticTag tag = tags.iterator().next();
+        assertThat(tag.getUID(), is("Tag_uid"));
+        assertThat(tag.getName(), is("uid"));
+        assertThat(tag.getParentUID(), is("Tag"));
+        assertThat(tag.getLabel(), is("TagLabel"));
+        assertThat(tag.getDescription(), is(""));
+        assertThat(tag.getSynonyms(), hasSize(0));
 
         // Verify YAML output contains short form
         String outYaml = generateYamlFromTags(tags);
@@ -96,14 +103,18 @@ public class YamlSemanticTagProviderTest {
     @Test
     public void testMapFormTagsLoadingAndGeneration() throws IOException {
         processYamlResource("tagWithMapForm.yaml");
-        Collection<YamlSemanticTagDTO> tags = captureAddedTags();
 
+        assertThat(semanticTagListener.semanticTags, is(aMapWithSize(1)));
+        assertThat(semanticTagListener.semanticTags, hasKey("Tag_uid"));
+        Collection<SemanticTag> tags = semanticTagProvider.getAll();
         assertThat(tags, hasSize(1));
-        YamlSemanticTagDTO tag = tags.iterator().next();
-        assertThat(tag.getId(), is("Tag_uid"));
-        assertThat(tag.label, is("TagLabel"));
-        assertThat(tag.description, is("Some description"));
-        assertThat(tag.synonyms, contains("syn1", "syn2"));
+        SemanticTag tag = tags.iterator().next();
+        assertThat(tag.getUID(), is("Tag_uid"));
+        assertThat(tag.getName(), is("uid"));
+        assertThat(tag.getParentUID(), is("Tag"));
+        assertThat(tag.getLabel(), is("TagLabel"));
+        assertThat(tag.getDescription(), is("Some description"));
+        assertThat(tag.getSynonyms(), contains("syn1", "syn2"));
 
         // Verify YAML output contains map form
         String outYaml = generateYamlFromTags(tags);
@@ -116,28 +127,26 @@ public class YamlSemanticTagProviderTest {
     @Test
     public void testShortFormTagWithNullLabelDeserializesAndSerializes() throws IOException {
         processYamlResource("tagWithShortFormSyntaxWithNullLabel.yaml");
-        Collection<YamlSemanticTagDTO> tags = captureAddedTags();
 
+        assertThat(semanticTagListener.semanticTags, is(aMapWithSize(1)));
+        assertThat(semanticTagListener.semanticTags, hasKey("Tag_null"));
+        Collection<SemanticTag> tags = semanticTagProvider.getAll();
         assertThat(tags, hasSize(1));
-        YamlSemanticTagDTO tag = tags.iterator().next();
-        assertThat(tag.getId(), is("Tag_null"));
+        SemanticTag tag = tags.iterator().next();
         // DTO currently represents an explicit null label as null
-        assertThat(tag.label, is(nullValue()));
-        assertThat(tag.description, is(nullValue()));
-        String outYaml = generateYamlFromTags(tags);
+        assertThat(tag.getUID(), is("Tag_null"));
+        assertThat(tag.getName(), is("null"));
+        assertThat(tag.getParentUID(), is("Tag"));
+        assertThat(tag.getLabel(), is(""));
+        assertThat(tag.getDescription(), is(""));
+        assertThat(tag.getSynonyms(), hasSize(0));
 
         // Generation should contain the tag key but should not emit label/description/synonyms
+        String outYaml = generateYamlFromTags(tags);
         assertThat(outYaml, containsString("Tag_null:"));
         assertThat(outYaml, not(containsString("label:")));
         assertThat(outYaml, not(containsString("description:")));
         assertThat(outYaml, not(containsString("synonyms:")));
-    }
-
-    @SuppressWarnings("unchecked")
-    private Collection<YamlSemanticTagDTO> captureAddedTags() {
-        ArgumentCaptor<Collection<YamlSemanticTagDTO>> captor = ArgumentCaptor.forClass(Collection.class);
-        verify(tagListener).addedModel(eq(MODEL_NAME), captor.capture());
-        return captor.getValue();
     }
 
     private void processYamlResource(String resourceName) throws IOException {
@@ -145,10 +154,39 @@ public class YamlSemanticTagProviderTest {
         modelRepository.processWatchEvent(WatchService.Kind.CREATE, fullModelPath);
     }
 
-    private String generateYamlFromTags(Collection<YamlSemanticTagDTO> tags) {
-        modelRepository.addElementsToBeGenerated("tags", List.copyOf(tags));
+    private String generateYamlFromTags(Collection<SemanticTag> tags) {
+        List<YamlElement> elts = new ArrayList<>();
+        tags.forEach(tag -> {
+            YamlSemanticTagDTO dto = new YamlSemanticTagDTO();
+            dto.uid = tag.getUID();
+            dto.label = tag.getLabel();
+            dto.description = tag.getDescription();
+            dto.synonyms = tag.getSynonyms();
+            elts.add(dto);
+        });
+        modelRepository.addElementsToBeGenerated("tags", elts);
         ByteArrayOutputStream out = new ByteArrayOutputStream();
         modelRepository.generateFileFormat("tags", out);
         return out.toString();
+    }
+
+    private static class TestSemanticTagChangeListener implements ProviderChangeListener<SemanticTag> {
+
+        public final Map<String, SemanticTag> semanticTags = new HashMap<>();
+
+        @Override
+        public void added(Provider<SemanticTag> provider, SemanticTag element) {
+            semanticTags.put(element.getUID(), element);
+        }
+
+        @Override
+        public void removed(Provider<SemanticTag> provider, SemanticTag element) {
+            semanticTags.remove(element.getUID());
+        }
+
+        @Override
+        public void updated(Provider<SemanticTag> provider, SemanticTag oldelement, SemanticTag element) {
+            semanticTags.put(element.getUID(), element);
+        }
     }
 }
