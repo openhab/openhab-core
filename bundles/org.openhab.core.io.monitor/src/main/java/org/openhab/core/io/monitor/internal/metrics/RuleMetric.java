b5814c1ab6daf0e8f7c68eea37735d086f94d646
Robert Delbrueck <querdenker2k@gmx.de>
2026-02-14 16:41:00 +0100

    add rule execution duration
    Signed-off-by: querdenker2k <querdenker2k@gmx.de>

--- bundles/org.openhab.core.io.monitor/src/main/java/org/openhab/core/io/monitor/internal/metrics/RuleMetric.java
+++ bundles/org.openhab.core.io.monitor/src/main/java/org/openhab/core/io/monitor/internal/metrics/RuleMetric.java
@@ -12,16 +12,19 @@
  */
 package org.openhab.core.io.monitor.internal.metrics;
 
+import java.time.Duration;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
 
+import org.eclipse.jdt.annotation.NonNull;
 import org.eclipse.jdt.annotation.NonNullByDefault;
 import org.eclipse.jdt.annotation.Nullable;
 import org.openhab.core.automation.Rule;
 import org.openhab.core.automation.RuleRegistry;
 import org.openhab.core.automation.RuleStatus;
 import org.openhab.core.automation.events.RuleStatusInfoEvent;
+import org.openhab.core.cache.ExpiringCacheMap;
 import org.openhab.core.events.Event;
 import org.openhab.core.events.EventSubscriber;
 import org.osgi.framework.BundleContext;
@@ -32,6 +35,7 @@ import org.slf4j.LoggerFactory;
 import io.micrometer.core.instrument.Meter;
 import io.micrometer.core.instrument.MeterRegistry;
 import io.micrometer.core.instrument.Tag;
+import io.micrometer.core.instrument.Timer;
 
 /**
  * The {@link RuleMetric} class implements a gauge metric for rules RUNNING events (per rule)
@@ -42,6 +46,7 @@ import io.micrometer.core.instrument.Tag;
 public class RuleMetric implements OpenhabCoreMeterBinder, EventSubscriber {
 
     public static final String METRIC_NAME = "openhab.rule.runs";
+    public static final String METRIC_DURATION_NAME = "openhab.rule.duration";
     public static final String RULES_TOPIC_PREFIX = "openhab/rules/";
     public static final String RULES_TOPIC_SUFFIX = "/state";
     private final Logger logger = LoggerFactory.getLogger(RuleMetric.class);
@@ -53,6 +58,7 @@ public class RuleMetric implements OpenhabCoreMeterBinder, EventSubscriber {
     private @Nullable ServiceRegistration<?> eventSubscriberRegistration;
     private BundleContext bundleContext;
     private RuleRegistry ruleRegistry;
+    private final ExpiringCacheMap<String, Timer.Sample> cache = new ExpiringCacheMap<>(Duration.ofMinutes(5));
 
     public RuleMetric(BundleContext bundleContext, Collection<Tag> tags, RuleRegistry ruleRegistry) {
         this.tags.addAll(tags);
@@ -104,19 +110,35 @@ public class RuleMetric implements OpenhabCoreMeterBinder, EventSubscriber {
 
         String topic = event.getTopic();
         String ruleId = topic.substring(RULES_TOPIC_PREFIX.length(), topic.lastIndexOf(RULES_TOPIC_SUFFIX));
-        if (!event.getPayload().contains(RuleStatus.RUNNING.name())) {
-            logger.trace("Skipping rule status info with status other than RUNNING {}", event.getPayload());
-            return;
-        }
+        String ruleStatus = event.getPayload();
 
-        logger.debug("Rule {} RUNNING - updating metric.", ruleId);
+        Set<Tag> tagsWithRule = createTags(ruleId);
+
+        if (ruleStatus.contains(RuleStatus.RUNNING.name())) {
+            logger.debug("Rule {} RUNNING - updating metric.", ruleId);
+            meterRegistry.counter(METRIC_NAME, tagsWithRule).increment();
+            cache.put(topic, () -> Timer.start(meterRegistry));
+        } else if (ruleStatus.contains(RuleStatus.IDLE.name())) {
+            Timer.Sample sample = cache.get(topic);
+            if (sample != null) {
+                logger.debug("Rule {} Finished - updating metric.", ruleId);
+                sample.stop(meterRegistry.timer(METRIC_DURATION_NAME, tagsWithRule));
+            } else {
+                logger.debug("Rule {} Finished - but running state missed.", ruleId);
+            }
+        } else {
+            logger.trace("Skipping rule status info with status {}", ruleStatus);
+        }
+    }
+
+    private @NonNull Set<Tag> createTags(String ruleId) {
         Set<Tag> tagsWithRule = new HashSet<>(tags);
         tagsWithRule.add(Tag.of(RULE_ID_TAG_NAME, ruleId));
         String ruleName = getRuleName(ruleId);
         if (ruleName != null) {
             tagsWithRule.add(Tag.of(RULE_NAME_TAG_NAME, ruleName));
         }
-        meterRegistry.counter(METRIC_NAME, tagsWithRule).increment();
+        return tagsWithRule;
     }
 
     private @Nullable String getRuleName(String ruleId) {
