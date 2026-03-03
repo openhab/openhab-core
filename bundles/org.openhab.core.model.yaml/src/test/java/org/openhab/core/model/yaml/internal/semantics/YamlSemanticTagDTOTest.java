677b1497dac336ac308109f3ff13fe01b6eb290e
Holger Friedrich <mail@holger-friedrich.de>
2026-03-03 11:25:24 +0100

    Upgrade jackson to 3.1.0
    * Upgrade jackson from 2.21.1 to 3.1.0
    * Upgrade jollyday-jackson from 1.8.1 to 2.2.0
    * Code adaptation due to changed interfaces
    
    Signed-off-by: Holger Friedrich <mail@holger-friedrich.de>

--- bundles/org.openhab.core.model.yaml/src/test/java/org/openhab/core/model/yaml/internal/semantics/YamlSemanticTagDTOTest.java
+++ bundles/org.openhab.core.model.yaml/src/test/java/org/openhab/core/model/yaml/internal/semantics/YamlSemanticTagDTOTest.java
@@ -21,7 +21,7 @@ import java.util.Map;
 
 import org.junit.jupiter.api.Test;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import tools.jackson.databind.ObjectMapper;
 
 /**
  * Test class for {@link YamlSemanticTagDTO} to verify correct serialization and deserialization
