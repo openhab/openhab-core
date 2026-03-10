ce6cef6a715d5d464fbb437e822841ee7cac99a1
Laurent ARNAL <laurent@clae.net>
2026-03-10 18:59:41 +0100

    try to resolve unit test failure !
    Signed-off-by: Laurent ARNAL <laurent@clae.net>

--- bundles/org.openhab.core.thing/src/main/java/org/openhab/core/thing/internal/ThingImpl.java
+++ bundles/org.openhab.core.thing/src/main/java/org/openhab/core/thing/internal/ThingImpl.java
@@ -209,8 +209,8 @@ public class ThingImpl implements Thing {
     }
 
     @Override
-    public @Nullable String setProperty(String name, @Nullable String value) {
-        if (name.isEmpty()) {
+    public @Nullable String setProperty(@Nullable String name, @Nullable String value) {
+        if (name == null || name.isEmpty()) {
             throw new IllegalArgumentException("Property name must not be null or empty");
         }
         synchronized (this) {
