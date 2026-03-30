7bbd9eaccec065e6819199f4cc80fbd778e34fea
Laurent Garnier <lg.hc@free.fr>
2026-03-30 23:36:32 +0200

    Review comment: change type of withTags parameter
    Signed-off-by: Laurent Garnier <lg.hc@free.fr>

--- bundles/org.openhab.core.model.rule.runtime/src/org/openhab/core/model/rule/runtime/internal/DSLRuleProvider.java
+++ bundles/org.openhab.core.model.rule.runtime/src/org/openhab/core/model/rule/runtime/internal/DSLRuleProvider.java
@@ -14,12 +14,10 @@ package org.openhab.core.model.rule.runtime.internal;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
-import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.stream.Collectors;
 
@@ -293,9 +291,8 @@ public class DSLRuleProvider
         List<Action> actions = List.of(ActionBuilder.create().withId("script").withTypeUID(ScriptActionHandler.TYPE_ID)
                 .withConfiguration(cfg).build());
 
-        Set<String> tags = new HashSet<>(rule.getTags());
-        return RuleBuilder.create(uid).withTags(tags).withName(name).withTriggers(triggers).withActions(actions)
-                .build();
+        return RuleBuilder.create(uid).withTags(rule.getTags()).withName(name).withTriggers(triggers)
+                .withActions(actions).build();
     }
 
     private String removeIndentation(String script) {
