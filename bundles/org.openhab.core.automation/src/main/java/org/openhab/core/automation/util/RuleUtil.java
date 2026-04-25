b29536ce3d3a7a9e2eef4c0eb15db42e767ca0ec
Ravi Nadahar <nadahar@rediffmail.com>
2026-04-25 17:35:07 +0200

    Apply my preferred alternative
    Signed-off-by: Ravi Nadahar <nadahar@rediffmail.com>

--- bundles/org.openhab.core.automation/src/main/java/org/openhab/core/automation/util/RuleUtil.java
+++ bundles/org.openhab.core.automation/src/main/java/org/openhab/core/automation/util/RuleUtil.java
@@ -27,51 +27,35 @@ import org.openhab.core.automation.Rule;
 @NonNullByDefault
 public class RuleUtil {
 
-    private static final Pattern RULE_UID_PATTERN_STRICT = Pattern.compile("[\\w-]+(:[\\w-]+)*");
-    private static final Pattern RULE_UID_PATTERN_STRICT_UNICODE = Pattern.compile("[\\w-]+(:[\\w-]+)*", Pattern.UNICODE_CHARACTER_CLASS);
-    private static final Pattern RULE_UID_PATTERN_LENIENT = Pattern.compile("[^\\s/\\\\]+", Pattern.UNICODE_CHARACTER_CLASS);
-    private static final Pattern RULE_UID_PATTERN_VERY_LENIENT = Pattern.compile("\\S(?:[^/\\\\]*\\S)?", Pattern.UNICODE_CHARACTER_CLASS);
+    private static final Pattern RULE_UID_PATTERN = Pattern.compile("\\S(?:[^/\\\\]*\\S)?",
+            Pattern.UNICODE_CHARACTER_CLASS);
 
     /**
-     * Returns {@code true} if the specified uid is a valid rule UID, otherwise {@code false}.
+     * Returns {@code true} if the specified UID is a valid rule UID, otherwise {@code false}.
      * <p>
-     * A valid rule UID must contain one or several segments separated by the character ":".
-     * Each segment must <i>only</i> consists of the following characters:
-     * <ul>
-     * <li>a-z</li>
-     * <li>A-Z</li>
-     * <li>0..9</li>
-     * <li>_ (underscore)</li>
-     * <li>- (hyphen)</li>
-     * </ul>
+     * A valid rule UID is any string that doesn't contain {@code /}, {@code \} and has no leading or trailing
+     * whitespace.
      *
-     * @param uid the UID of the rule to be checked
-     * @return true if the specified uid is a valid rule UID, otherwise false
+     * @param uid the UID of the rule to be checked.
+     * @return {@code true} if the specified UID is a valid rule UID, {@code false} otherwise.
      */
-    public static boolean isValidRuleUID(final String uid) { // TODO: Update Javadocs to reflect final decision
-        return RULE_UID_PATTERN_STRICT_UNICODE.matcher(uid).matches();
+    public static boolean isValidRuleUID(final String uid) {
+        return RULE_UID_PATTERN.matcher(uid).matches();
     }
 
     /**
-     * Ensures that the specified UID of the rule is valid.
+     * Ensures that the specified rule UID of is valid.
      * <p>
-     * If the UID of the rule is invalid an {@link IllegalArgumentException} is thrown, otherwise this method returns
+     * If the rule UID is invalid an {@link IllegalArgumentException} is thrown, otherwise this method returns
      * silently.
      * <p>
-     * A valid rule UID must contain one or several segments separated by the character ":".
-     * Each segment must <i>only</i> consists of the following characters:
-     * <ul>
-     * <li>a-z</li>
-     * <li>A-Z</li>
-     * <li>0..9</li>
-     * <li>_ (underscore)</li>
-     * <li>- (hyphen)</li>
-     * </ul>
+     * A valid rule UID is any string that doesn't contain {@code /}, {@code \} and has no leading or trailing
+     * whitespace.
      *
-     * @param uid the UID of the rule to be checked
-     * @throws IllegalArgumentException if the UID of the rule is invalid
+     * @param uid the UID of the rule to be checked.
+     * @throws IllegalArgumentException If the specified rule UID is invalid.
      */
-    public static void assertValidRuleUID(String uid) throws IllegalArgumentException { // TODO: Update Javadocs to reflect final decision
+    public static void assertValidRuleUID(String uid) throws IllegalArgumentException {
         if (!isValidRuleUID(uid)) {
             throw new IllegalArgumentException("The specified UID of rule '" + uid + "' is invalid!");
         }
