07bc486d6d5ed8933cc7b502183d562e21fe5e1e
Florian Hotze <dev@florianhotze.com>
2026-04-25 11:41:38 +0200

    Extract displayState calculation from SseItemStatesEventBuilder to utility class
    Signed-off-by: Florian Hotze <dev@florianhotze.com>

---
+++ bundles/org.openhab.core.transform/src/main/java/org/openhab/core/transform/util/ItemDisplayStateUtil.java
@@ -0,0 +1,169 @@
+/*
+ * Copyright (c) 2010-2026 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.core.transform.util;
+
+import java.time.ZoneId;
+import java.util.IllegalFormatException;
+import java.util.Locale;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import javax.measure.Unit;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.eclipse.jdt.annotation.Nullable;
+import org.openhab.core.items.Item;
+import org.openhab.core.library.types.DateTimeType;
+import org.openhab.core.library.types.QuantityType;
+import org.openhab.core.transform.TransformationException;
+import org.openhab.core.transform.TransformationHelper;
+import org.openhab.core.transform.TransformationService;
+import org.openhab.core.types.State;
+import org.openhab.core.types.StateDescription;
+import org.openhab.core.types.StateOption;
+import org.openhab.core.types.UnDefType;
+import org.openhab.core.types.util.UnitUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Utility class for getting the display state of an Item from its {@link StateDescription}.
+ *
+ * @author Florian Hotze - Initial contribution (extracted from SseItemStatesEventBuilder)
+ */
+@NonNullByDefault
+public class ItemDisplayStateUtil {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ItemDisplayStateUtil.class);
+
+    private static final Pattern EXTRACT_TRANSFORM_FUNCTION_PATTERN = Pattern.compile("(.*?)\\((.*)\\):(.*)");
+
+    /**
+     * Get the display state of an item.
+     *
+     * @param item the item
+     * @param locale the locale
+     * @param zoneId the timezone id
+     * @return the display state
+     */
+    public static @Nullable String getDisplayState(Item item, Locale locale, ZoneId zoneId) {
+        return getDisplayState(item, item.getState(), locale, zoneId);
+    }
+
+    /**
+     * Get the display state of an item for a given state.
+     *
+     * @param item the item
+     * @param state the state
+     * @param locale the locale
+     * @param zoneId the timezone id
+     * @return the display state
+     */
+    public static @Nullable String getDisplayState(Item item, State state, Locale locale, ZoneId zoneId) {
+        StateDescription stateDescription = item.getStateDescription(locale);
+        String displayState = state.toString();
+
+        if (stateDescription != null) {
+            String pattern = stateDescription.getPattern();
+            // First check if the pattern is a transformation
+            Matcher matcher;
+            if (pattern != null && (matcher = EXTRACT_TRANSFORM_FUNCTION_PATTERN.matcher(pattern)).find()) {
+                try {
+                    String type = matcher.group(1);
+                    String function = matcher.group(2);
+                    String value = matcher.group(3);
+                    TransformationService transformation = TransformationHelper.getTransformationService(type);
+                    if (transformation != null) {
+                        String format = state instanceof UnDefType ? "%s" : value;
+                        try {
+                            displayState = transformation.transform(function, state.format(format));
+                            if (displayState == null) {
+                                displayState = state.toString();
+                            }
+                        } catch (IllegalArgumentException e) {
+                            throw new TransformationException(
+                                    "Cannot format state '" + state + "' to format '" + format + "'", e);
+                        } catch (RuntimeException e) {
+                            throw new TransformationException("Transformation service of type '" + type
+                                    + "' threw an exception: " + e.getMessage(), e);
+                        }
+                    } else {
+                        throw new TransformationException(
+                                "Transformation service of type '" + type + "' is not available.");
+                    }
+                } catch (TransformationException e) {
+                    LOGGER.warn("Failed transforming the state '{}' on item '{}' with pattern '{}': {}", state,
+                            item.getName(), pattern, e.getMessage());
+                }
+            }
+            // If no transformation, NULL/UNDEF state is returned as "NULL"/"UNDEF" without considering anything else
+            else if (!(state instanceof UnDefType)) {
+                boolean optionMatched = false;
+                if (!stateDescription.getOptions().isEmpty()) {
+                    // Look for a state option with a value corresponding to the state
+                    for (StateOption option : stateDescription.getOptions()) {
+                        String label = option.getLabel();
+                        if (option.getValue().equals(state.toString()) && label != null) {
+                            optionMatched = true;
+                            try {
+                                displayState = pattern == null ? label : String.format(pattern, label);
+                            } catch (IllegalFormatException e) {
+                                LOGGER.debug(
+                                        "Unable to format option label '{}' of item {} using format pattern '{}': {}, displaying option label",
+                                        label, item.getName(), pattern, e.getMessage());
+                                displayState = label;
+                            }
+                            break;
+                        }
+                    }
+                }
+                if (pattern != null && !optionMatched) {
+                    // if it's not a transformation pattern and there is no matching state option,
+                    // then it must be a format string
+                    if (state instanceof QuantityType quantityState) {
+                        // sanity convert current state to the item state description unit in case it was
+                        // updated in the meantime. The item state is still in the "original" unit while the
+                        // state description will display the new unit:
+                        Unit<?> patternUnit = UnitUtils.parseUnit(pattern);
+                        if (patternUnit != null && !quantityState.getUnit().equals(patternUnit)) {
+                            quantityState = quantityState.toInvertibleUnit(patternUnit);
+                        }
+
+                        if (quantityState != null) {
+                            state = quantityState;
+                        }
+                    }
+
+                    // The following exception handling has been added to work around a Java bug with formatting
+                    // numbers. See http://bugs.sun.com/view_bug.do?bug_id=6476425
+                    // This also handles IllegalFormatConversionException, which is a subclass of
+                    // IllegalArgument.
+                    try {
+                        if (state instanceof DateTimeType dateTimeState) {
+                            displayState = dateTimeState.format(pattern, zoneId);
+                        } else {
+                            displayState = state.format(pattern);
+                        }
+                    } catch (IllegalArgumentException e) {
+                        LOGGER.debug(
+                                "Unable to format value '{}' of item {} using format pattern '{}': {}, displaying raw state",
+                                state, item.getName(), pattern, e.getMessage());
+                        displayState = state.toString();
+                    }
+                }
+            }
+        }
+
+        return displayState;
+    }
+}
