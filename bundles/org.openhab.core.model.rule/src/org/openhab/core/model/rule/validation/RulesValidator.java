055259bd2e69d124ce222ef39eaeef6e13b7a0ca
Nadahar <Nadahar@users.noreply.github.com>
2026-05-05 20:45:04 +0200

    Rule UID syntax validation (#5467)
    * Add a validity check for the rule UID
    
    A valid rule UID must consist of one or several segments separated by a colon, each segment must only contain alphanumeric, underscore or hyphen, and must not contain any other symbols.
    
    Signed-off-by: Laurent Garnier <lg.hc@free.fr>
    
    * Apply UID validation to YAML rules
    
    Signed-off-by: Ravi Nadahar <nadahar@rediffmail.com>
    
    * Alternatives
    
    Signed-off-by: Ravi Nadahar <nadahar@rediffmail.com>
    
    * Implement RulesValidator in Java instead
    
    Signed-off-by: Ravi Nadahar <nadahar@rediffmail.com>
    
    * Workaround to let specific errors fail rule parsing while others are just a warning
    
    Signed-off-by: Ravi Nadahar <nadahar@rediffmail.com>
    
    * Alternative way to express model validation expression
    
    Signed-off-by: Ravi Nadahar <nadahar@rediffmail.com>
    
    * Enforce rule UID in RuleRegistry
    
    Signed-off-by: Ravi Nadahar <nadahar@rediffmail.com>
    
    * Apply my preferred alternative
    
    Signed-off-by: Ravi Nadahar <nadahar@rediffmail.com>
    
    * Address review feedback
    
    Signed-off-by: Ravi Nadahar <nadahar@rediffmail.com>
    
    * Add tests for RuleUtil
    
    Signed-off-by: Ravi Nadahar <nadahar@rediffmail.com>
    
    * Address review comments
    
    Signed-off-by: Ravi Nadahar <nadahar@rediffmail.com>
    
    ---------
    
    Signed-off-by: Laurent Garnier <lg.hc@free.fr>
    Signed-off-by: Ravi Nadahar <nadahar@rediffmail.com>
    Co-authored-by: Laurent Garnier <lg.hc@free.fr>
    Co-authored-by: Ravi Nadahar <nadahar@rediffmail.com>

---
+++ bundles/org.openhab.core.model.rule/src/org/openhab/core/model/rule/validation/RulesValidator.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright (c) 2010-2026 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.core.model.rule.validation;
+
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.eclipse.jdt.annotation.Nullable;
+import org.eclipse.xtext.nodemodel.ICompositeNode;
+import org.eclipse.xtext.nodemodel.util.NodeModelUtils;
+import org.eclipse.xtext.validation.Check;
+import org.openhab.core.automation.util.RuleUtil;
+import org.openhab.core.model.rule.rules.Rule;
+import org.openhab.core.model.rule.rules.RulesPackage;
+
+/**
+ * This class contains custom validation rules.
+ *
+ * See https://www.eclipse.org/Xtext/documentation/303_runtime_concepts.html#validation
+ *
+ * @author Ravi Nadahar - Initial contribution
+ */
+@NonNullByDefault
+public class RulesValidator extends AbstractRulesValidator {
+
+    @Check
+    public void checkRuleUID(@Nullable Rule rule) {
+        String uid;
+        if (rule == null || (uid = rule.getUid()) == null) {
+            return;
+        }
+        if (!RuleUtil.isValidRuleUID(uid)) {
+            error(buildMsgWithLineNb(rule, "Rule UID '" + uid
+                    + "' is invalid. A rule UID can't contain '/', '\\' or have leading or trailing whitespace."), rule,
+                    RulesPackage.Literals.RULE__UID, "uid");
+        }
+    }
+
+    private static String buildMsgWithLineNb(EObject object, String msg) {
+        ICompositeNode node = NodeModelUtils.getNode(object);
+        if (node == null) {
+            return msg;
+        }
+        int startLine = node.getStartLine();
+        int endLine = node.getEndLine();
+        return (startLine == endLine) ? "Line " + startLine + ": " + msg
+                : "Line " + startLine + "-" + endLine + ": " + msg;
+    }
+}
