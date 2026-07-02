e9848dc68a0ebbc251110591bbba63167a3b2c91
Florian Hotze <dev@florianhotze.com>
2026-07-02 17:06:38 +0200

    [voice] Refactor LLMToolCall to a standard class to allow inheritance (#5689)
    Signed-off-by: Florian Hotze <dev@florianhotze.com>

--- bundles/org.openhab.core.voice/src/main/java/org/openhab/core/voice/text/interpreter/llm/LLMToolCall.java
+++ bundles/org.openhab.core.voice/src/main/java/org/openhab/core/voice/text/interpreter/llm/LLMToolCall.java
@@ -13,23 +13,36 @@
 package org.openhab.core.voice.text.interpreter.llm;
 
 import java.util.Map;
+import java.util.Objects;
 
 import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.eclipse.jdt.annotation.Nullable;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonSyntaxException;
 
 /**
  * A DTO to store information about a tool call.
- * 
- * @param tool the UID of the {@link LLMTool}
- * @param params the params of the tool call
  *
  * @author Florian Hotze - Initial contribution
  */
 @NonNullByDefault
-public record LLMToolCall(String tool, Map<String, Object> params) {
-    private static final Gson GSON = new Gson();
+public class LLMToolCall {
+    protected static final Gson GSON = new Gson();
+
+    /**
+     * the UID of the {@link LLMTool}
+     */
+    public final String tool;
+    /**
+     * the params of the tool call
+     */
+    public final Map<String, Object> params;
+
+    public LLMToolCall(String tool, Map<String, Object> params) {
+        this.tool = Objects.requireNonNull(tool, "tool must not be null");
+        this.params = Objects.requireNonNull(params, "params must not be null");
+    }
 
     public static LLMToolCall map(LLMTool tool, Map<String, Object> params) {
         return new LLMToolCall(tool.getUID(), params);
@@ -56,6 +69,27 @@ public record LLMToolCall(String tool, Map<String, Object> params) {
         if (call == null) {
             throw new JsonSyntaxException("Deserialized LLMToolCall is null.");
         }
+        if (call.tool == null || call.params == null) {
+            throw new JsonSyntaxException("Deserialized LLMToolCall has null tool or params.");
+        }
         return call;
+    }
+
+    @Override
+    public boolean equals(@Nullable Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        LLMToolCall call = (LLMToolCall) o;
+        return tool.equals(call.tool) && params.equals(call.params);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(tool, params);
     }
 }
